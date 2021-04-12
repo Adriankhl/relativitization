e221cf0e9ba851a324594c6ff2ab1ab5ba2c71f0
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-04-12 18:59:03 +0800

    Add buildSrc

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,16 @@
+package com.relativitization.build
+
+object Versions {
+    const val appName = "Relativization"
+    const val appVersion = "0.1"
+
+    const val kotlinVersion = "1.4.32"
+    const val kotlinxCoroutineVersion = "1.4.3"
+    const val kotlinxSerializationVersion = "1.1.0"
+    const val ktorVersion = "1.5.3"
+
+    const val gdxVersion = "1.9.14"
+    const val log4jVersion = "2.14.1"
+    const val junitVersion = "5.7.1"
+    const val javafakerVersion = "1.0.2"
+}
