457ae54cf3d590edf665f1898e20e0ec2d814194
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-04-20 19:51:13 +0800

    Complete coordinate2DFrom3D

--- universe/src/main/kotlin/relativitization/universe/maths/grid/Projections.kt
+++ universe/src/main/kotlin/relativitization/universe/maths/grid/Projections.kt
@@ -1,11 +1,23 @@
 package relativitization.universe.maths.grid
 
 import relativitization.universe.data.physics.Int2D
+import kotlin.math.max
 
 object Projections {
 
     /**
+     * Project a 3D coordinate to 2D pixel plane
      *
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param z z coordinate
+     * @param zDim the dimension of the z coordinate
+     * @param gridWidth width of a grid in pixel
+     * @param gridHeight height of a grid in pixel
+     * @param xSpace x separation (in number of grid) between projected grid in z dimension
+     * @param ySpace y separation (in number of grid) between projected grid in z dimension
+     * @param xExtra extra spaces in the x-axis after all projected grid, in number of grid
+     * @param yExtra extra spaces in the y-axis after all projected grid, in number of grid
      */
     fun coordinate2DFrom3D(
         x: Int,
@@ -21,16 +33,24 @@ object Projections {
     ): Int2D {
 
         val xSeparation = if (xSpace != ySpace) {
-            (zDim - 1 + xExtra) * (xSpace + 1) * gridWidth
+            (zDim + xExtra) * (xSpace + 1) * gridWidth
         } else {
-            (zDim - 1 + xExtra) * 2 * (xSpace + 1) * gridWidth
+            (zDim + xExtra) * 2 * (xSpace + 1) * gridWidth
         }
 
         val ySeparation = if (xSpace != ySpace) {
-            (zDim - 1 + yExtra) * (ySpace + 1) * gridWidth
+            (zDim + yExtra) * (ySpace + 1) * gridHeight
         } else {
-            (zDim - 1 + xExtra) * 2 * (xSpace + 1) * gridWidth
+            (zDim + yExtra) * 2 * (ySpace + 1) * gridHeight
         }
+
+        val separation = max(xSeparation, ySeparation)
+
+        val xPos = x * separation + z * (xSpace + 1) * gridWidth
+
+        val yPos = y * separation + z * (ySpace + 1) * gridHeight
+
+        return Int2D(xPos, yPos)
     }
 }
 
