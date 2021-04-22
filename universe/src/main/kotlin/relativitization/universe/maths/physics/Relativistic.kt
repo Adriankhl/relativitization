439b3d0233d8c1803046852e08b5c38e2036d606
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-04-22 18:54:43 +0800

    Update energy instruction

--- universe/src/main/kotlin/relativitization/universe/maths/physics/Relativistic.kt
+++ universe/src/main/kotlin/relativitization/universe/maths/physics/Relativistic.kt
@@ -17,4 +17,14 @@ object Relativistic {
         val v2 = (1.0 - gamaInv * gamaInv) * (speedOfLight * speedOfLight).toDouble()
         return sqrt(v2)
     }
+
+    // E = m * c^2
+    // Unit of energy change when c != 1
+    // if energy value data is stored as if c=1, scaling is required when c != 1
+    // Consider the amount of energy required to accelerate something to 0.5c
+    // c = 2 needs 4 times more energy then c = 1, but this the amount of energy
+    // should be the same regardless of c, so the energy value should be amplified by 4 times
+    fun energyUnit(speedOfLight: Int): Int {
+        return speedOfLight * speedOfLight
+    }
 }
