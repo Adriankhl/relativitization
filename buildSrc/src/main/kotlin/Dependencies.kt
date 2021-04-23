d3d08a88a1bc856d4279d739a5b4e1c5a606959b
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-04-23 08:18:50 +0800

    Update libgdx to 1.10.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val kotlinxSerializationVersion = "1.1.0"
     const val ktorVersion = "1.5.3"
 
-    const val gdxVersion = "1.9.14"
+    const val gdxVersion = "1.10.0"
     const val log4jVersion = "2.14.1"
     const val junitVersion = "5.7.1"
     const val javafakerVersion = "1.0.2"
