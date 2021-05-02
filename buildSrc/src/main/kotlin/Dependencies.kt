ccefe1dbb1a25dccf126cbd0768793fa48a907e1
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-05-02 22:22:59 +0800

    Update dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -5,7 +5,7 @@ object Versions {
     const val kotlinVersion = "1.5.0-RC"
     const val kotlinxCoroutineVersion = "1.5.0-RC"
     const val kotlinxSerializationVersion = "1.2.0"
-    const val ktorVersion = "1.5.3"
+    const val ktorVersion = "1.5.4"
 
     const val gdxVersion = "1.10.0"
     const val log4jVersion = "2.14.1"
