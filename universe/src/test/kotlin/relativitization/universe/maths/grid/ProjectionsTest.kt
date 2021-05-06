9dfb3dc1d65c5c1abd32a84a12697fe6d105a562
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-05-06 21:29:13 +0800

    Add test

--- universe/src/test/kotlin/relativitization/universe/maths/grid/ProjectionsTest.kt
+++ universe/src/test/kotlin/relativitization/universe/maths/grid/ProjectionsTest.kt
@@ -4,12 +4,37 @@ import kotlin.test.Test
 import relativitization.universe.data.physics.Int2D
 import relativitization.universe.maths.grid.Projections.coordinate2DFrom3D
 import relativitization.universe.maths.grid.Projections.indexToRectangleFunction
+import relativitization.universe.maths.grid.Projections.positionToIndexFunction
 
 internal class ProjectionsTest {
     @Test
     fun indexToRectangleTest() {
         val iFunc = indexToRectangleFunction(
-            
+            numRequiredRectangle = 10,
+            groupHeight = 4096,
+            groupWidth = 4096,
+            imageHeight = 512,
+            imageWidth = 512,
+            xOffSet = 100,
+            yOffSet = 200
         )
+
+        assert(iFunc(2) == IntRectangle(xPos=2148, yPos=200, width=1024, height=1024))
+        assert(iFunc(5) == IntRectangle(xPos=1124, yPos=1224, width=1024, height=1024))
+    }
+
+    @Test
+    fun positionToIndexTest() {
+        val pFunc = positionToIndexFunction(
+            numRequiredRectangle = 10,
+            groupHeight = 4096,
+            groupWidth = 4096,
+            imageHeight = 512,
+            imageWidth = 512,
+            xOffSet = 100,
+            yOffSet = 200
+        )
+
+        assert(pFunc(2148, 1300) == 6)
     }
 }
