26af9f18c26c25a38695b6d2c2457834190e9000
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-05-12 21:28:21 +0800

    Add background

--- gdx-core/src/main/kotlin/relativitization/game/MainMenuScreen.kt
+++ gdx-core/src/main/kotlin/relativitization/game/MainMenuScreen.kt
@@ -3,9 +3,15 @@ package relativitization.game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import relativitization.game.utils.Assets
 import relativitization.game.utils.BaseScreen
 
 class MainMenuScreen(assets: Assets) : BaseScreen(assets) {
+    val background: Image = assets.getImage("background/universe-background")
+
+    init {
+        stage.addActor(background)
+    }
 }
--- gdx-core/src/main/kotlin/relativitization/game/utils/Assets.kt
+++ gdx-core/src/main/kotlin/relativitization/game/utils/Assets.kt
@@ -3,10 +3,15 @@ package relativitization.game.utils
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.assets.loaders.SkinLoader
 import com.badlogic.gdx.audio.Music
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 
 class Assets {
     private val manager: AssetManager = AssetManager()
+
+    private val textureMap: MutableMap<String, AtlasRegion> = mutableMapOf()
 
     fun loadAll() {
         val flatUISkin = SkinLoader.SkinParameter("skin/flatearth/flatearthui/flat-earth-ui.atlas")
@@ -14,10 +19,18 @@ class Assets {
 
         manager.load("music/Alexander Ehlers - Warped.mp3", Music::class.java)
 
+        manager.load("relativitization-asset.atlas", TextureAtlas::class.java)
+
         manager.finishLoading()
     }
 
     fun getSkin(): Skin = manager.get("skin/flatearth/flatearthui/flat-earth-ui.json")
 
     fun getBackgroundMusic(): Music = manager.get("music/Alexander Ehlers - Warped.mp3")
+
+    fun getImage(name: String): Image {
+        val textureAtLas: TextureAtlas = manager.get("relativitization-asset.atlas")
+        val region = textureMap.getOrPut(name) { textureAtLas.findRegion(name) }
+        return Image(region)
+    }
 }
