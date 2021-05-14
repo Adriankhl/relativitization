babe639e77406707dbf5cf1eca6c728967cdd8fc
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-05-14 14:24:34 +0800

    Add create textfield

--- gdx-core/src/main/kotlin/relativitization/game/utils/TableScreen.kt
+++ gdx-core/src/main/kotlin/relativitization/game/utils/TableScreen.kt
@@ -6,10 +6,18 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.*
-import com.badlogic.gdx.scenes.scene2d.ui.SelectBox.SelectBoxStyle
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox.SelectBoxStyle
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle
+import com.badlogic.gdx.scenes.scene2d.ui.TextField
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldListener
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
 import com.badlogic.gdx.utils.Array
 import com.badlogic.gdx.utils.viewport.ScreenViewport
@@ -67,7 +75,7 @@ open class TableScreen(val assets: Assets) : ScreenAdapter() {
 
         val button = TextButton(text, style)
 
-        button.addListener( object : ChangeListener() {
+        button.addListener(object : ChangeListener() {
             override fun changed(event: ChangeEvent, actor: Actor) {
                 function()
             }
@@ -127,5 +135,28 @@ open class TableScreen(val assets: Assets) : ScreenAdapter() {
             }
         })
         return selectBox
+    }
+
+
+    /**
+     * Create textfield
+     *
+     * @param default the default value of the text
+     * @param fontSize the font size of the text
+     * @param function the function acted after the text field has changed
+     */
+    fun createTextField(default: String, fontSize: Int = 16, function: (String) -> Unit = {}): TextField {
+        val style = skin.get(TextFieldStyle::class.java)
+        style.font = assets.getFont(fontSize)
+
+        val textField = TextField(default, style)
+
+        textField.setTextFieldListener( object : TextFieldListener {
+            override fun keyTyped(textField: TextField?, c: Char) {
+                function(textField?.text ?: "")
+            }
+        })
+
+        return textField
     }
 }
