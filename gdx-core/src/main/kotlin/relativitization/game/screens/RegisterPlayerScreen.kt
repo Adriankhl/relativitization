763c926603929f7057f381fb9fc821d95e43a0bb
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-05-15 15:53:56 +0800

    Add pick player id

--- gdx-core/src/main/kotlin/relativitization/game/screens/RegisterPlayerScreen.kt
+++ gdx-core/src/main/kotlin/relativitization/game/screens/RegisterPlayerScreen.kt
@@ -1,5 +1,7 @@
 package relativitization.game.screens
 
+import com.badlogic.gdx.utils.Array
+import kotlinx.coroutines.runBlocking
 import relativitization.game.RelativitizationGame
 import relativitization.game.utils.TableScreen
 
@@ -11,6 +13,51 @@ class RegisterPlayerScreen(val game: RelativitizationGame) : TableScreen(game.as
 
         var idList: List<Int> = listOf()
 
-        root.add(createLabel("Register Settings :", gdxSetting.hugeFontSIze))
+        root.add(createLabel("Type of available players: ", gdxSetting.normalFontSize))
+        val getPlayerTypeSelectBox = createSelectBox(
+            listOf("All", "Human only"),
+            "All",
+            gdxSetting.normalFontSize
+        )
+        root.add(getPlayerTypeSelectBox)
+
+        root.row().space(10f)
+
+        // Define before update button but show after update button
+        val playerIdSelectBox = createSelectBox(
+            idList,
+            idList.getOrElse(0) { -1 },
+            gdxSetting.normalFontSize
+        )
+
+        val updateButton = createTextButton("Update") {
+            when (getPlayerTypeSelectBox.selected) {
+                "All" -> {
+                    runBlocking {
+                        idList = game.universeClient.httpGetAvailableIdList()
+                    }
+                }
+                "Human only" -> {
+                    runBlocking {
+                        idList = game.universeClient.httpGetAvailableHumanIdList()
+                    }
+                }
+                else -> {
+                    runBlocking {
+                        idList = game.universeClient.httpGetAvailableIdList()
+                    }
+                }
+            }
+
+            playerIdSelectBox.items = Array(idList.toTypedArray<Int>())
+        }
+        root.add(updateButton).colspan(2)
+
+        root.row().space(10f)
+
+        root.add(createLabel("Pick your player id: ", gdxSetting.normalFontSize))
+        root.add(playerIdSelectBox)
+
+        root.row().space(10f)
     }
 }
