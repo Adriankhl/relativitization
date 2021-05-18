99676c7ecfe2f235b66884faefa7ae6619ecb467
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-05-18 20:45:40 +0800

    Improve getPlayerImage

--- gdx-core/src/main/kotlin/relativitization/game/utils/PlayerImage.kt
+++ gdx-core/src/main/kotlin/relativitization/game/utils/PlayerImage.kt
@@ -2,6 +2,7 @@ package relativitization.game.utils
 
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import relativitization.universe.data.PlayerData
+import relativitization.universe.data.PlayerType
 import relativitization.universe.data.UniverseData3DAtPlayer
 import relativitization.universe.data.popsystems.CarrierType
 
@@ -14,21 +15,27 @@ object PlayerImage {
         width: Float,
         height: Float,
     ): List<Image> {
-        val playerId = playerData.id
 
-        val playerShipImage: Image = assets.getImage(playerId, "system/ship1", xPos, yPos, width, height)
+        val imageList: MutableList<Image> = mutableListOf()
+        
+        val playerId = playerData.id
 
         val hasStellarSystem: Boolean = playerData.playerInternalData.popSystemicData.carrier.map {
             it.carrierType == CarrierType.STELLAR
         }.contains(true)
 
-        return if (hasStellarSystem) {
+        if (hasStellarSystem)  {
             val stellarImage: Image = assets.getImage("system/sun")
             stellarImage.setPosition(xPos, yPos)
             stellarImage.setSize(width, height)
-            listOf(stellarImage, playerShipImage)
-        } else {
-            listOf(playerShipImage)
+            imageList.add(stellarImage)
         }
+
+        if (playerData.playerType != PlayerType.NONE) {
+            val playerShipImage: Image = assets.getImage(playerId, "system/ship1", xPos, yPos, width, height)
+            imageList.add(playerShipImage)
+        }
+
+        return imageList
     }
 }
