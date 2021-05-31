035f151384d9f0cd1dbcf043ccd7f9b443f8f198
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-05-31 22:35:33 +0800

    Clear unused import

--- gdx-core/src/main/kotlin/relativitization/game/components/info/PhysicsInfo.kt
+++ gdx-core/src/main/kotlin/relativitization/game/components/info/PhysicsInfo.kt
@@ -3,8 +3,6 @@ package relativitization.game.components.info
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import relativitization.game.RelativitizationGame
-import relativitization.game.components.GameScreenInfo
-import relativitization.game.components.GameScreenWorldMap
 import relativitization.game.utils.ScreenComponent
 
 class PhysicsInfo(
