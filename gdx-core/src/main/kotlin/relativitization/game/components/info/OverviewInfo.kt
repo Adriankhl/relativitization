64b46dc8b66ef240b1d7991b7889f8f181031461
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-01 13:56:09 +0800

    Complete overview info

--- gdx-core/src/main/kotlin/relativitization/game/components/info/OverviewInfo.kt
+++ gdx-core/src/main/kotlin/relativitization/game/components/info/OverviewInfo.kt
@@ -3,7 +3,9 @@ package relativitization.game.components.info
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import relativitization.game.RelativitizationGame
+import relativitization.game.utils.PlayerImage
 import relativitization.game.utils.ScreenComponent
+import relativitization.universe.data.PlayerData
 
 class OverviewInfo(val game: RelativitizationGame) : ScreenComponent<ScrollPane>(game.assets) {
 
@@ -12,37 +14,54 @@ class OverviewInfo(val game: RelativitizationGame) : ScreenComponent<ScrollPane>
     private val table: Table = Table()
     private val scrollPane: ScrollPane = createScrollPane(table)
 
-    private var id: Int = -1
+    private var playerData: PlayerData = PlayerData(-1)
 
     init {
         table.background = assets.getBackgroundColor(0.2f, 0.3f, 0.5f, 1.0f)
+        updatePlayerData()
+        updateTable()
     }
 
     override fun getScreenComponent(): ScrollPane {
         return scrollPane
     }
 
+    override fun onUniverseData3DChange() {
+        updatePlayerData()
+        updateTable()
+    }
+
     override fun onPrimarySelectedPlayerIdChange() {
-        updateId()
+        updatePlayerData()
+        updateTable()
     }
 
 
-    private fun updateId() {
-        id = if (game.universeClient.isPrimarySelectedPlayerIdValid()) {
-            game.universeClient.primarySelectedPlayerId
+    private fun updatePlayerData() {
+        playerData = if (game.universeClient.isPrimarySelectedPlayerIdValid()) {
+            game.universeClient.getUniverseData3D().get(game.universeClient.primarySelectedPlayerId)
         } else {
-            game.universeClient.getUniverseData3D().id
+            game.universeClient.getUniverseData3D().get(game.universeClient.getUniverseData3D().id)
         }
     }
 
     private fun updateTable() {
         table.clear()
 
-        val playerData = game.universeClient.getUniverseData3D().get(id)
         val headerLabel = createLabel("Overview: ${playerData.id}", gdxSettings.normalFontSize)
 
         table.add(headerLabel)
 
         table.row().space(10f)
+
+        val playerImageStack = PlayerImage.getPlayerImageStack(
+            playerData,
+            assets,
+            128f,
+            128f,
+            gdxSettings.soundEffectsVolume
+        )
+
+        table.add(playerImageStack).size(128f * gdxSettings.imageScale, 128f * gdxSettings.imageScale)
     }
 }
