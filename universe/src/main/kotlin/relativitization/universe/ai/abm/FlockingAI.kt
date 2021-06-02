01a1bf386a47e5a69b96188bd6f693228e80ec1d
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-02 16:37:00 +0800

    Complete alignment in FlockingAI

--- universe/src/main/kotlin/relativitization/universe/ai/abm/FlockingAI.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/abm/FlockingAI.kt
@@ -6,7 +6,6 @@ import relativitization.universe.data.PlayerData
 import relativitization.universe.data.UniverseData3DAtPlayer
 import relativitization.universe.data.commands.Command
 import relativitization.universe.data.physics.Double3D
-import relativitization.universe.data.physics.Velocity
 import relativitization.universe.maths.physics.Intervals.distance
 
 class FlockingAI : AI() {
@@ -15,18 +14,24 @@ class FlockingAI : AI() {
 
         val nearByRadius: Double = 5.0
 
+        val desiredSeparation: Double = 1.0
+
+        val cohesionDouble3D = cohesion(universeData3DAtPlayer, nearByRadius)
+
+        val alignmentDouble3D = alignment(universeData3DAtPlayer, nearByRadius)
+
         return listOf()
     }
 
-    private fun cohesion(universeData3DAtPlayer: UniverseData3DAtPlayer, radius: Double): Velocity {
+    private fun cohesion(universeData3DAtPlayer: UniverseData3DAtPlayer, radius: Double): Double3D {
         val selfDouble4D = universeData3DAtPlayer.getCurrentPlayerData().playerInternalData.physicsData.double4D
         val nearByPlayerData: List<PlayerData> = universeData3DAtPlayer.playerDataMap.values.filter {
             val otherDouble4D = it.playerInternalData.physicsData.double4D
-            distance(selfDouble4D, otherDouble4D) < radius
+            distance(selfDouble4D, otherDouble4D) < radius && (it.id != universeData3DAtPlayer.getCurrentPlayerData().id)
         }
 
         return if (nearByPlayerData.isEmpty()) {
-            Velocity(0.0, 0.0, 0.0)
+            Double3D(0.0, 0.0, 0.0)
         } else {
             val avgX: Double = nearByPlayerData.fold(0.0) { acc, playerData ->
                 acc + playerData.playerInternalData.physicsData.double4D.x
@@ -34,13 +39,40 @@ class FlockingAI : AI() {
 
             val avgY: Double = nearByPlayerData.fold(0.0) { acc, playerData ->
                 acc + playerData.playerInternalData.physicsData.double4D.y
-            }/ nearByPlayerData.size.toDouble()
+            } / nearByPlayerData.size.toDouble()
 
             val avgZ: Double = nearByPlayerData.fold(0.0) { acc, playerData ->
                 acc + playerData.playerInternalData.physicsData.double4D.z
-            }/ nearByPlayerData.size.toDouble()
+            } / nearByPlayerData.size.toDouble()
 
-            Velocity(avgX - selfDouble4D.x, avgY - selfDouble4D.y, avgZ - selfDouble4D.z).scaleVelocity(0.5)
+            Double3D(avgX - selfDouble4D.x, avgY - selfDouble4D.y, avgZ - selfDouble4D.z)
+        }
+    }
+
+    fun alignment(universeData3DAtPlayer: UniverseData3DAtPlayer, radius: Double): Double3D {
+        val selfDouble4D = universeData3DAtPlayer.getCurrentPlayerData().playerInternalData.physicsData.double4D
+        val nearByPlayerData: List<PlayerData> = universeData3DAtPlayer.playerDataMap.values.filter {
+            val otherDouble4D = it.playerInternalData.physicsData.double4D
+            distance(selfDouble4D, otherDouble4D) < radius && (it.id != universeData3DAtPlayer.getCurrentPlayerData().id)
+        }
+
+
+        return if (nearByPlayerData.isEmpty()) {
+            Double3D(0.0, 0.0, 0.0)
+        } else {
+            val avgX: Double = nearByPlayerData.fold(0.0) { acc, playerData ->
+                acc + playerData.playerInternalData.physicsData.velocity.vx
+            } / nearByPlayerData.size.toDouble()
+
+            val avgY: Double = nearByPlayerData.fold(0.0) { acc, playerData ->
+                acc + playerData.playerInternalData.physicsData.velocity.vy
+            } / nearByPlayerData.size.toDouble()
+
+            val avgZ: Double = nearByPlayerData.fold(0.0) { acc, playerData ->
+                acc + playerData.playerInternalData.physicsData.velocity.vz
+            } / nearByPlayerData.size.toDouble()
+
+            Double3D(avgX, avgY, avgZ)
         }
     }
 
