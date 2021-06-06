6441e4680811d088bcaaa5fefdc8b66111b7adc0
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-06 11:13:29 +0800

    Add ProcessEvents Mechanism

---
+++ universe/src/main/kotlin/relativitization/universe/mechanisms/events/ProcessEvents.kt
@@ -0,0 +1,17 @@
+package relativitization.universe.mechanisms.events
+
+import relativitization.universe.data.MutablePlayerData
+import relativitization.universe.data.UniverseData
+import relativitization.universe.data.UniverseData3DAtPlayer
+import relativitization.universe.data.commands.Command
+import relativitization.universe.mechanisms.Mechanism
+
+object ProcessEvents : Mechanism() {
+    override fun process(
+        mutablePlayerData: MutablePlayerData,
+        universeData3DAtPlayer: UniverseData3DAtPlayer,
+        universeData: UniverseData
+    ): List<Command> {
+        return listOf()
+    }
+}
