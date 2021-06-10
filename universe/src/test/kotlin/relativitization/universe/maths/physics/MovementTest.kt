f385c2305d00869fe6e8dfe3606cc9929f6b1c52
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-10 15:57:55 +0800

    Add target velocity test for sameDirectionTest

--- universe/src/test/kotlin/relativitization/universe/maths/physics/MovementTest.kt
+++ universe/src/test/kotlin/relativitization/universe/maths/physics/MovementTest.kt
@@ -3,6 +3,7 @@ package relativitization.universe.maths.physics
 import relativitization.universe.data.physics.Double3D
 import relativitization.universe.data.physics.Velocity
 import relativitization.universe.maths.physics.Movement.isSameDirection
+import relativitization.universe.maths.physics.Relativistic.targetVelocityByPhotonRocket
 import kotlin.test.Test
 
 internal class MovementTest {
@@ -10,5 +11,17 @@ internal class MovementTest {
     fun sameDirectionTest() {
         val b1: Boolean = isSameDirection(Velocity(0.3, 0.3, 0.3), Double3D(0.1, 0.1, 0.1))
         assert(b1)
+
+
+        val v2 = targetVelocityByPhotonRocket(
+            initialRestMass = 1.0,
+            maxDeltaRestMass = 0.5,
+            initialVelocity = Velocity(vx = 0.2, vy = 0.1, vz = 0.3),
+            targetVelocity = Velocity(vx = 0.234, vy = -0.182, vz = -0.411),
+            speedOfLight = 1.0
+        )
+
+        val b2 = isSameDirection(v2.newVelocity, Double3D(0.234, -0.182, -0.411))
+        assert(b2)
     }
 }
