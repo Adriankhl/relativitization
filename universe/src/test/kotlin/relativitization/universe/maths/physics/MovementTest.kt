b149d823fec75ef8ff474cee530d9e1909ef2b9f
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-10 19:52:02 +0800

    Add stoppingDistance test

--- universe/src/test/kotlin/relativitization/universe/maths/physics/MovementTest.kt
+++ universe/src/test/kotlin/relativitization/universe/maths/physics/MovementTest.kt
@@ -1,8 +1,10 @@
 package relativitization.universe.maths.physics
 
+import org.junit.jupiter.api.TestFactory
 import relativitization.universe.data.physics.Double3D
 import relativitization.universe.data.physics.Velocity
 import relativitization.universe.maths.physics.Movement.isSameDirection
+import relativitization.universe.maths.physics.Movement.stoppingDistanceByPhotonRocket
 import relativitization.universe.maths.physics.Relativistic.targetVelocityByPhotonRocket
 import kotlin.test.Test
 
@@ -23,5 +25,17 @@ internal class MovementTest {
 
         val b2 = isSameDirection(v2.newVelocity, Double3D(0.234, -0.182, -0.411))
         assert(b2)
+    }
+
+    @Test
+    fun stoppingDistanceTest() {
+        val d1: Double = stoppingDistanceByPhotonRocket(
+            initialRestMass = 1.0,
+            maxDeltaRestMass = 0.01,
+            initialVelocity = Velocity(vx = 0.2, vy = 0.1, vz = 0.3),
+            speedOfLight = 1.0
+        )
+
+        assert(d1 > 6.4 && d1 < 6.5)
     }
 }
