ae463eb68ecfe9beb53e66d4cbf8b7a5b8109739
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-10 22:20:04 +0800

    Complete targetDouble3DByPhotonRocket

--- universe/src/main/kotlin/relativitization/universe/maths/physics/Intervals.kt
+++ universe/src/main/kotlin/relativitization/universe/maths/physics/Intervals.kt
@@ -1,5 +1,6 @@
 package relativitization.universe.maths.physics
 
+import relativitization.universe.data.physics.Double3D
 import relativitization.universe.data.physics.Double4D
 import relativitization.universe.data.physics.Int3D
 import relativitization.universe.data.physics.Int4D
@@ -13,6 +14,9 @@ object Intervals {
         sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1))
 
     fun distance(d1: Double4D, d2: Double4D): Double =
+        distance(d1.x, d1.y, d1.z, d2.z, d2.y, d2.z)
+
+    fun distance(d1: Double3D, d2: Double3D): Double =
         distance(d1.x, d1.y, d1.z, d2.z, d2.y, d2.z)
 
     fun distance(d1: MutableDouble4D, d2: MutableDouble4D): Double =
--- universe/src/main/kotlin/relativitization/universe/maths/physics/Movement.kt
+++ universe/src/main/kotlin/relativitization/universe/maths/physics/Movement.kt
@@ -3,9 +3,11 @@ package relativitization.universe.maths.physics
 import org.apache.logging.log4j.LogManager
 import relativitization.universe.data.physics.Double3D
 import relativitization.universe.data.physics.Velocity
+import relativitization.universe.maths.physics.Intervals.distance
 import relativitization.universe.maths.physics.Relativistic.decelerateByPhotonRocket
 import relativitization.universe.maths.physics.Relativistic.targetVelocityAtDirectionPhotonRocket
 import relativitization.universe.maths.physics.Relativistic.targetVelocityByPhotonRocket
+import kotlin.math.min
 
 object Movement {
     private val logger = LogManager.getLogger()
@@ -28,10 +30,19 @@ object Movement {
     }
 
     /**
-     * Whether the velocity and double3D is pointing at the same direction
+     * Whether the velocity and double3D are pointing at the same direction
      */
     fun isSameDirection(velocity: Velocity, double3D: Double3D): Boolean {
         val dotProduct: Double = velocity.scaleVelocity(1.0).dot(double3D.normalize())
+        return (dotProduct > 0.999999) && (dotProduct < 1.000001)
+    }
+
+
+    /**
+     * Whether the velocities are pointing at the same direction
+     */
+    fun isSameDirection(v1: Velocity, v2: Velocity): Boolean {
+        val dotProduct: Double = v1.scaleVelocity(1.0).dotUnitVelocity(v2)
         return (dotProduct > 0.999999) && (dotProduct < 1.000001)
     }
 
@@ -96,92 +107,61 @@ object Movement {
         return intervalMin
     }
 
-    /**
-     * Given that the initialVelocity and the double3D pointing from initialPosition to targetPosition are pointing
-     * at the same direction, whether the object should decelerate now to stop at target position
-     */
-    fun shouldDecelerate(
-        initialRestMass: Double,
-        maxDeltaRestMass: Double,
-        initialVelocity: Velocity,
-        initialPosition: Double3D,
-        targetPosition: Double3D,
-        speedOfLight: Double,
-    ) {
-        val originalDirection: Double3D = (targetPosition - initialPosition).normalize()
 
-        var currentRestMass: Double = initialRestMass
-        var currentPosition: Double3D = initialPosition
-        var currentDirection: Double3D = (targetPosition - currentPosition).normalize()
-        var currentVelocity: Velocity = initialVelocity
-
-        // Maintain this velocity for one turn, then decelerate
-        currentPosition += currentVelocity.displacement(1)
-        currentDirection = (targetPosition - currentPosition).normalize()
-
-        while (isSameDirection(currentVelocity, currentDirection) && (currentVelocity.squareMag() > 0)) {
-            val targetVelocity: Velocity = displacementToVelocity(currentPosition, targetPosition, speedOfLight)
-
-            val requiredDeltaMass: Double = Relativistic.deltaMassByPhotonRocket(
-                initialRestMass = currentRestMass,
-                initialVelocity = currentVelocity,
-                targetVelocity = targetVelocity,
-                speedOfLight = speedOfLight,
-            )
-
-            if (targetVelocity.mag() > 0.000001) {
-                if (requiredDeltaMass <= maxDeltaRestMass) {
-                    currentVelocity = targetVelocity
-                    currentPosition += currentVelocity.displacement(1)
-                    currentRestMass -= requiredDeltaMass
-                    currentDirection = (targetPosition - currentPosition).normalize()
-                } else {
-                    val targetVelocityData: TargetVelocityData = targetVelocityAtDirectionPhotonRocket(
-                        initialRestMass = currentRestMass,
-                        deltaRestMass = maxDeltaRestMass,
-                        initialVelocity = currentVelocity,
-                        targetDirection = targetVelocity,
-                        accelerate = false,
-                        speedOfLight = speedOfLight,
-                    )
-
-                    if (targetVelocityData.targetVelocityType == TargetVelocityType.FAIL) {
-                        logger.error("Something wrong in shouldDecelerate when computing targetVelocityData")
-                    }
-                }
-            }
-        }
-    }
-
-    fun targetPositionByPhotonRocket(
+    fun targetDouble3DByPhotonRocket(
         initialRestMass: Double,
         maxDeltaRestMass: Double,
         initialVelocity: Velocity,
         maxSpeed: Double,
-        initialPosition: Double3D,
-        targetPosition: Double3D,
+        initialDouble3D: Double3D,
+        targetDouble3D: Double3D,
         speedOfLight: Double,
     ): TargetVelocityData {
-        val originalTargetVelocity: Velocity = displacementToVelocity(
-            from = initialPosition,
-            to = targetPosition,
-            speedOfLight = 0.0,
-        )
 
-        val targetVelocity: Velocity = if (originalTargetVelocity.mag() > maxSpeed) {
-            originalTargetVelocity.scaleVelocity(maxSpeed)
+        val distance: Double = distance(initialDouble3D, targetDouble3D)
+        val originalTargetVelocity: Velocity = displacementToVelocity(initialDouble3D, targetDouble3D, speedOfLight)
+        val maxSpeedByDistance: Double = min(maxSpeed, maxSpeedToStopByPhotonRocket(
+            initialRestMass = initialRestMass,
+            maxDeltaRestMass = maxDeltaRestMass,
+            distance = distance,
+            speedOfLight = speedOfLight,
+        ))
+
+        val targetVelocity: Velocity = if (originalTargetVelocity.mag() > maxSpeedByDistance) {
+            originalTargetVelocity.scaleVelocity(maxSpeedByDistance)
         } else {
             originalTargetVelocity
         }
 
-        val targetVelocityData: TargetVelocityData = targetVelocityByPhotonRocket(
-            initialRestMass = initialRestMass,
-            maxDeltaRestMass = maxDeltaRestMass,
-            initialVelocity = initialVelocity,
-            targetVelocity = targetVelocity,
-            speedOfLight = 0.0
-        )
+        return if (initialDouble3D == targetDouble3D) {
+            TargetVelocityData(
+                TargetVelocityType.SUCCESS,
+                Velocity(0.0, 0.0, 0.0),
+                0.0
+            )
+        } else {
+            val requiredDeltaMass: Double = Relativistic.deltaMassByPhotonRocket(
+                initialRestMass = initialRestMass,
+                initialVelocity = initialVelocity,
+                targetVelocity = targetVelocity,
+                speedOfLight = speedOfLight,
+            )
 
-        return targetVelocityData
+            if ((requiredDeltaMass <= maxDeltaRestMass) && (targetVelocity == originalTargetVelocity)) {
+                TargetVelocityData(
+                    TargetVelocityType.SUCCESS,
+                    targetVelocity,
+                    requiredDeltaMass
+                )
+            } else {
+                targetVelocityByPhotonRocket(
+                    initialRestMass = initialRestMass,
+                    maxDeltaRestMass = maxDeltaRestMass,
+                    initialVelocity = initialVelocity,
+                    targetVelocity = targetVelocity,
+                    speedOfLight = speedOfLight
+                )
+            }
+        }
     }
 }
