2376fefad52b0dba8ee262aba12f08fc0dde4487
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-13 12:01:53 +0800

    Add copyAndroidNatives

--- gdx-android/build.gradle.kts
+++ gdx-android/build.gradle.kts
@@ -73,3 +73,42 @@ android {
 dependencies {
     implementation("com.android.tools.build:gradle:${Versions.androidGradlePluginVersion}")
 }
+
+
+// called every time gradle gets executed, takes the native dependencies of
+// the natives configuration, and extracts them to the proper libs/ folders
+// so they get packed with the APK.
+task("copyAndroidNatives") {
+    val natives: Configuration by configurations
+
+    doFirst {
+        file("libs/armeabi/").mkdirs()
+        file("libs/armeabi-v7a/").mkdirs()
+        file("libs/arm64-v8a/").mkdirs()
+        file("libs/x86_64/").mkdirs()
+        file("libs/x86/").mkdirs()
+        natives.forEach { jar ->
+            val outputDir: File? = when {
+                jar.name.endsWith("natives-arm64-v8a.jar") -> file("libs/arm64-v8a")
+                jar.name.endsWith("natives-armeabi-v7a.jar") -> file("libs/armeabi-v7a")
+                jar.name.endsWith("natives-armeabi.jar") -> file("libs/armeabi")
+                jar.name.endsWith("natives-x86_64.jar") -> file("libs/x86_64")
+                jar.name.endsWith("natives-x86.jar") -> file("libs/x86")
+                else -> null
+            }
+            outputDir?.let {
+                copy {
+                    from(zipTree(jar))
+                    into(outputDir)
+                    include("*.so")
+                }
+            }
+        }
+    }
+}
+
+tasks.whenTaskAdded {
+    if ("package" in name) {
+        dependsOn("copyAndroidNatives")
+    }
+}
