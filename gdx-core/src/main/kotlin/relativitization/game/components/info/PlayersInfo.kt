be4506b1e6edc8bda32828e8bbb3b3ecaac44a32
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-15 22:12:13 +0800

    Add selected player in PlayersInfo

--- gdx-core/src/main/kotlin/relativitization/game/components/info/PlayersInfo.kt
+++ gdx-core/src/main/kotlin/relativitization/game/components/info/PlayersInfo.kt
@@ -3,11 +3,14 @@ package relativitization.game.components.info
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import relativitization.game.RelativitizationGame
 import relativitization.game.utils.ScreenComponent
+import relativitization.universe.data.PlayerData
 
 class PlayersInfo(val game: RelativitizationGame) : ScreenComponent<Table>(game.assets) {
     private val gdxSettings = game.gdxSettings
 
     private var table: Table = Table()
+
+    private var playerData: PlayerData = PlayerData(-1)
 
     init {
         table.background = assets.getBackgroundColor(0.2f, 0.2f, 0.2f, 1.0f)
@@ -15,5 +18,24 @@ class PlayersInfo(val game: RelativitizationGame) : ScreenComponent<Table>(game.
 
     override fun getScreenComponent(): Table {
         return table
+    }
+
+    private fun updatePlayerData() {
+        playerData = if (game.universeClient.isPrimarySelectedPlayerIdValid()) {
+            game.universeClient.getUniverseData3D().get(game.universeClient.primarySelectedPlayerId)
+        } else {
+            game.universeClient.getUniverseData3D().get(game.universeClient.getUniverseData3D().id)
+        }
+    }
+
+
+    private fun updateTable() {
+        table.clear()
+
+        val headerLabel = createLabel("Selected: player ${playerData.id}", gdxSettings.bigFontSize)
+
+        table.add(headerLabel)
+
+        table.row().space(20f)
     }
 }
