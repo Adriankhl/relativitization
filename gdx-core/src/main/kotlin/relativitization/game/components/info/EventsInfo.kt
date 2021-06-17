ba9009d4d33efe2e578e3ba57a858b1720ad94f8
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-17 15:49:32 +0800

    Add functions for EventInfo

--- gdx-core/src/main/kotlin/relativitization/game/components/info/EventsInfo.kt
+++ gdx-core/src/main/kotlin/relativitization/game/components/info/EventsInfo.kt
@@ -3,13 +3,42 @@ package relativitization.game.components.info
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import relativitization.game.RelativitizationGame
 import relativitization.game.utils.ScreenComponent
+import relativitization.universe.data.PlayerData
 
 class EventsInfo(val game: RelativitizationGame) : ScreenComponent<Table>(game.assets) {
     private val gdxSettings = game.gdxSettings
 
     private var table: Table = Table()
 
+    private var playerData: PlayerData = PlayerData(-1)
+
     override fun getScreenComponent(): Table {
         return table
+    }
+
+    override fun onUniverseData3DChange() {
+        updatePlayerData()
+        updateTable()
+    }
+
+    override fun onPrimarySelectedPlayerIdChange() {
+        updatePlayerData()
+        updateTable()
+    }
+
+    private fun updatePlayerData() {
+        playerData = if (game.universeClient.isPrimarySelectedPlayerIdValid()) {
+            game.universeClient.getUniverseData3D().get(game.universeClient.primarySelectedPlayerId)
+        } else {
+            game.universeClient.getUniverseData3D().getCurrentPlayerData()
+        }
+    }
+
+    private fun updateTable() {
+        table.clear()
+
+        val headerLabel = createLabel("Event list: player ${playerData.id}", gdxSettings.bigFontSize)
+
+        table.add(headerLabel)
     }
 }
