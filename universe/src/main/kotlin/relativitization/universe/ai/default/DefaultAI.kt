9cdd6d0557a9e26fa44e641ade98aff5df7e8a77
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-22 22:04:58 +0800

    Add root reasoner

--- universe/src/main/kotlin/relativitization/universe/ai/default/DefaultAI.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/default/DefaultAI.kt
@@ -1,6 +1,9 @@
 package relativitization.universe.ai.default
 
 import relativitization.universe.ai.AI
+import relativitization.universe.ai.default.utils.DecisionData
+import relativitization.universe.ai.default.utils.Option
+import relativitization.universe.ai.default.utils.SequenceReasoner
 import relativitization.universe.data.UniverseData3DAtPlayer
 import relativitization.universe.data.commands.Command
 import relativitization.universe.utils.RelativitizationLogManager
@@ -10,6 +13,13 @@ object DefaultAI : AI() {
 
     override fun compute(universeData3DAtPlayer: UniverseData3DAtPlayer): List<Command> {
         logger.debug("Computing player ${universeData3DAtPlayer.id} with DefaultAI")
-        return listOf()
+        return RootReasoner(universeData3DAtPlayer).getCommandList()
     }
+}
+
+class RootReasoner(universeData3DAtPlayer: UniverseData3DAtPlayer) : SequenceReasoner() {
+
+    val decisionData = DecisionData(universeData3DAtPlayer)
+
+    override val optionList: List<Option> = listOf()
 }
--- universe/src/main/kotlin/relativitization/universe/ai/default/utils/DecisionData.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/default/utils/DecisionData.kt
@@ -11,6 +11,22 @@ data class DecisionData(
     val commandList: MutableList<Command> = mutableListOf(),
     var mutableUniverseData3DAtPlayer: MutableUniverseData3DAtPlayer = DataSerializer.copy(universeData3DAtPlayer),
 ) {
+    fun addCommands(newCommandList: List<Command>) {
+        commandList.addAll(newCommandList)
+        for (command in newCommandList) {
+            val playerData = mutableUniverseData3DAtPlayer.get(command.toId)
+            if (playerData.id == -1) {
+                logger.error("resetMutableData error: Player id -1")
+            }
+            command.checkAndExecute(playerData, universeData3DAtPlayer.universeSettings)
+        }
+    }
+
+    fun removeCommand(commandListToRemove: List<Command>) {
+        commandList.removeAll { commandListToRemove.contains(it) }
+        resetMutableData()
+    }
+
     fun resetMutableData() {
         mutableUniverseData3DAtPlayer = DataSerializer.copy(universeData3DAtPlayer)
         for (command in commandList) {
@@ -21,8 +37,6 @@ data class DecisionData(
             command.checkAndExecute(playerData, universeData3DAtPlayer.universeSettings)
         }
     }
-
-
 
     companion object {
         private val logger = RelativitizationLogManager.getLogger()
