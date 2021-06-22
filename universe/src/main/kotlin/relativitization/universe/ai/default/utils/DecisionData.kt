2ddac1346160a6cf30586f5eb25a5919b3b954f8
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-22 12:31:34 +0800

    Add DecisionData

--- universe/src/main/kotlin/relativitization/universe/ai/default/utils/DecisionData.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/default/utils/DecisionData.kt
@@ -1,7 +1,30 @@
 package relativitization.universe.ai.default.utils
 
+import relativitization.universe.data.MutableUniverseData3DAtPlayer
 import relativitization.universe.data.UniverseData3DAtPlayer
+import relativitization.universe.data.commands.Command
+import relativitization.universe.data.serializer.DataSerializer
+import relativitization.universe.utils.RelativitizationLogManager
 
 data class DecisionData(
     val universeData3DAtPlayer: UniverseData3DAtPlayer,
-)
+    val commandList: MutableList<Command> = mutableListOf(),
+    var mutableUniverseData3DAtPlayer: MutableUniverseData3DAtPlayer = DataSerializer.copy(universeData3DAtPlayer),
+) {
+    fun resetMutableData() {
+        mutableUniverseData3DAtPlayer = DataSerializer.copy(universeData3DAtPlayer)
+        for (command in commandList) {
+            val playerData = mutableUniverseData3DAtPlayer.get(command.toId)
+            if (playerData.id == -1) {
+                logger.error("resetMutableData error: Player id -1")
+            }
+            command.checkAndExecute(playerData, universeData3DAtPlayer.universeSettings)
+        }
+    }
+
+
+
+    companion object {
+        private val logger = RelativitizationLogManager.getLogger()
+    }
+}
