ecf4c7fad70e57a8f8118005d84bd1af62da10b0
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-25 09:30:34 +0800

    Update dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,7 +3,7 @@ object Versions {
     const val appCodeNumber = 1
     const val appVersion = "0.1"
 
-    const val kotlinVersion = "1.5.10"
+    const val kotlinVersion = "1.5.20"
     const val kotlinxCoroutineVersion = "1.5.0"
     const val kotlinxSerializationVersion = "1.2.1"
     const val ktorVersion = "1.6.0"
@@ -13,8 +13,8 @@ object Versions {
     const val kotlinFakerVersion = "1.7.1"
 
     const val androidGradlePluginVersion = "7.0.0-beta04"
-    const val androidLifeCycleKtxVersion = "2.3.1"
-    const val androidFragmentKtxVersion = "1.3.4"
+    const val androidLifeCycleKtxVersion = "2.4.0-alpha02"
+    const val androidFragmentKtxVersion = "1.3.5"
 
     const val logbackVersion = "1.2.3"
 }
