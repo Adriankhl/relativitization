d00c45ac9801b8d6c20d22fc2e0db46da8af761c
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-06-25 16:48:38 +0800

    Add PopData

--- universe/src/main/kotlin/relativitization/universe/data/popsystem/Carrier.kt
+++ universe/src/main/kotlin/relativitization/universe/data/popsystem/Carrier.kt
@@ -6,12 +6,14 @@ import kotlinx.serialization.Serializable
 data class Carrier(
     val restMass: Double = 1.0,
     val carrierType: CarrierType = CarrierType.SPACESHIP,
+    val allPopData: AllPopData = AllPopData(),
 )
 
 @Serializable
 data class MutableCarrier(
     var restMass: Double = 1.0,
     var carrierType: CarrierType = CarrierType.SPACESHIP,
+    var allPopData: MutableAllPopData = MutableAllPopData(),
 )
 
 enum class CarrierType {
--- universe/src/main/kotlin/relativitization/universe/data/popsystem/PopData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/popsystem/PopData.kt
@@ -3,6 +3,73 @@ package relativitization.universe.data.popsystem
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class AllPopData(
+    val labourerPopData: LabourerPopData = LabourerPopData(),
+    val engineerPopData: EngineerPopData = EngineerPopData(),
+    val scholarPopData: ScholarPopData = ScholarPopData(),
+    val educatorPopData: EducatorPopData = EducatorPopData(),
+)
+
+@Serializable
+data class MutableAllPopData(
+    var labourerPopData: MutableLabourerPopData = MutableLabourerPopData(),
+    var engineerPopData: MutableEngineerPopData = MutableEngineerPopData(),
+    var scholarPopData: MutableScholarPopData = MutableScholarPopData(),
+    var educatorPopData: MutableEducatorPopData = MutableEducatorPopData(),
+)
+
+@Serializable
 data class CommonPopData(
-    val population: Double = 100.0,
+    val childPopulation: Double = 20.0,
+    val adultPopulation: Double = 100.0,
+    val elderlyPopulation: Double = 20.0,
+    val satisfaction: Double = 0.0,
+)
+
+@Serializable
+data class MutableCommonPopData(
+    var childPopulation: Double = 20.0,
+    var adultPopulation: Double = 100.0,
+    var elderlyPopulation: Double = 20.0,
+    var satisfaction: Double = 0.0,
+)
+
+@Serializable
+data class LabourerPopData(
+    val commonPopData: CommonPopData = CommonPopData()
+)
+
+@Serializable
+data class MutableLabourerPopData(
+    var commonPopData: MutableCommonPopData = MutableCommonPopData()
+)
+
+@Serializable
+data class EngineerPopData(
+    val commonPopData: CommonPopData = CommonPopData()
+)
+
+@Serializable
+data class MutableEngineerPopData(
+    var commonPopData: MutableCommonPopData = MutableCommonPopData()
+)
+
+@Serializable
+data class ScholarPopData(
+    val commonPopData: CommonPopData = CommonPopData()
+)
+
+@Serializable
+data class MutableScholarPopData(
+    var commonPopData: MutableCommonPopData = MutableCommonPopData()
+)
+
+@Serializable
+data class EducatorPopData(
+    val commonPopData: CommonPopData = CommonPopData()
+)
+
+@Serializable
+data class MutableEducatorPopData(
+    var commonPopData: MutableCommonPopData = MutableCommonPopData()
 )
