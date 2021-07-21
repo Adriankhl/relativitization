11906560e5538e8c4ae3ac76ee771e006f007838
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-07-21 18:57:33 +0800

    Complete BasicScienceKnowledge

--- universe/src/main/kotlin/relativitization/universe/data/science/knowledge/BasicScienceKnowledge.kt
+++ universe/src/main/kotlin/relativitization/universe/data/science/knowledge/BasicScienceKnowledge.kt
@@ -31,3 +31,123 @@ data class PhysicsKnowledge(
         mutableKnowledgeData.physicsResearchLevel += importance
     }
 }
+
+@Serializable
+data class MaterialKnowledge(
+    override val knowledgeId: Int,
+    val importance: Double,
+    override val xCor: Double,
+    override val yCor: Double,
+    override val difficulty: Double,
+    override val referenceKnowledgeIdList: List<Int>,
+    override val referenceTechnologyIdList: List<Int>,
+) : SingleKnowledgeData() {
+    override fun updateKnowledgeData(mutableKnowledgeData: MutableKnowledgeData) {
+        mutableKnowledgeData.materialResearchLevel += importance
+    }
+}
+
+@Serializable
+data class ComputerKnowledge(
+    override val knowledgeId: Int,
+    val importance: Double,
+    override val xCor: Double,
+    override val yCor: Double,
+    override val difficulty: Double,
+    override val referenceKnowledgeIdList: List<Int>,
+    override val referenceTechnologyIdList: List<Int>,
+) : SingleKnowledgeData() {
+    override fun updateKnowledgeData(mutableKnowledgeData: MutableKnowledgeData) {
+        mutableKnowledgeData.computerResearchLevel += importance
+    }
+}
+
+@Serializable
+data class MechanicsKnowledge(
+    override val knowledgeId: Int,
+    val importance: Double,
+    override val xCor: Double,
+    override val yCor: Double,
+    override val difficulty: Double,
+    override val referenceKnowledgeIdList: List<Int>,
+    override val referenceTechnologyIdList: List<Int>,
+) : SingleKnowledgeData() {
+    override fun updateKnowledgeData(mutableKnowledgeData: MutableKnowledgeData) {
+        mutableKnowledgeData.mechanicsResearchLevel += importance
+    }
+}
+
+@Serializable
+data class MedicineKnowledge(
+    override val knowledgeId: Int,
+    val importance: Double,
+    override val xCor: Double,
+    override val yCor: Double,
+    override val difficulty: Double,
+    override val referenceKnowledgeIdList: List<Int>,
+    override val referenceTechnologyIdList: List<Int>,
+) : SingleKnowledgeData() {
+    override fun updateKnowledgeData(mutableKnowledgeData: MutableKnowledgeData) {
+        mutableKnowledgeData.medicineResearchLevel += importance
+    }
+}
+
+@Serializable
+data class EconomyKnowledge(
+    override val knowledgeId: Int,
+    val importance: Double,
+    override val xCor: Double,
+    override val yCor: Double,
+    override val difficulty: Double,
+    override val referenceKnowledgeIdList: List<Int>,
+    override val referenceTechnologyIdList: List<Int>,
+) : SingleKnowledgeData() {
+    override fun updateKnowledgeData(mutableKnowledgeData: MutableKnowledgeData) {
+        mutableKnowledgeData.economyResearchLevel += importance
+    }
+}
+
+@Serializable
+data class PoliticsKnowledge(
+    override val knowledgeId: Int,
+    val importance: Double,
+    override val xCor: Double,
+    override val yCor: Double,
+    override val difficulty: Double,
+    override val referenceKnowledgeIdList: List<Int>,
+    override val referenceTechnologyIdList: List<Int>,
+) : SingleKnowledgeData() {
+    override fun updateKnowledgeData(mutableKnowledgeData: MutableKnowledgeData) {
+        mutableKnowledgeData.politicsResearchLevel += importance
+    }
+}
+
+@Serializable
+data class SociologyKnowledge(
+    override val knowledgeId: Int,
+    val importance: Double,
+    override val xCor: Double,
+    override val yCor: Double,
+    override val difficulty: Double,
+    override val referenceKnowledgeIdList: List<Int>,
+    override val referenceTechnologyIdList: List<Int>,
+) : SingleKnowledgeData() {
+    override fun updateKnowledgeData(mutableKnowledgeData: MutableKnowledgeData) {
+        mutableKnowledgeData.sociologyResearchLevel += importance
+    }
+}
+
+@Serializable
+data class PsychologyKnowledge(
+    override val knowledgeId: Int,
+    val importance: Double,
+    override val xCor: Double,
+    override val yCor: Double,
+    override val difficulty: Double,
+    override val referenceKnowledgeIdList: List<Int>,
+    override val referenceTechnologyIdList: List<Int>,
+) : SingleKnowledgeData() {
+    override fun updateKnowledgeData(mutableKnowledgeData: MutableKnowledgeData) {
+        mutableKnowledgeData.psychologyResearchLevel += importance
+    }
+}
