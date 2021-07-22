e1466c7425692583ce69897e71f94d85a109ef63
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-07-22 19:38:49 +0800

    Add generateKnowledgeList and generateTechnologyList

--- universe/src/main/kotlin/relativitization/universe/generate/science/GenerateUniverseScienceData.kt
+++ universe/src/main/kotlin/relativitization/universe/generate/science/GenerateUniverseScienceData.kt
@@ -3,6 +3,8 @@ package relativitization.universe.generate.science
 import relativitization.universe.data.UniverseData
 import relativitization.universe.data.science.MutableUniverseScienceData
 import relativitization.universe.data.science.UniverseScienceData
+import relativitization.universe.data.science.knowledge.SingleKnowledgeData
+import relativitization.universe.data.science.technology.SingleTechnologyData
 import relativitization.universe.data.serializer.DataSerializer
 import relativitization.universe.utils.RelativitizationLogManager
 
@@ -15,5 +17,31 @@ object GenerateUniverseScienceData {
             universeScienceData
         )
         return DataSerializer.copy(mutableUniverseScienceData)
+    }
+
+    /**
+     * Generate knowledge list
+     *
+     * @param universeScienceData universe science data
+     * @param numNewSingleKnowledgeData number of new single knowledge data
+     */
+    private fun generateKnowledgeList(
+        universeScienceData: UniverseScienceData,
+        numNewSingleKnowledgeData: Int,
+    ): List<SingleKnowledgeData> {
+        return listOf()
+    }
+
+    /**
+     * Generate technology list
+     *
+     * @param universeScienceData universe science data
+     * @param numNewSingleTechnologyData number of new single knowledge data
+     */
+    private fun generateTechnologyList(
+        universeScienceData: UniverseScienceData,
+        numNewSingleTechnologyData: Int,
+    ): List<SingleTechnologyData> {
+        return listOf()
     }
 }
