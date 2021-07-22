61ce5c7fe32836e80c5bdcb8e2863a0a72ebe791
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-07-22 14:51:51 +0800

    Add computePlayerKnowledgeData and computePlayerTechnologyData

--- universe/src/main/kotlin/relativitization/universe/data/science/PlayerScienceData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/science/PlayerScienceData.kt
@@ -7,6 +7,7 @@ import relativitization.universe.data.science.knowledge.SingleKnowledgeData
 import relativitization.universe.data.science.technology.MutableTechnologyData
 import relativitization.universe.data.science.technology.SingleTechnologyData
 import relativitization.universe.data.science.technology.TechnologyData
+import relativitization.universe.data.serializer.DataSerializer
 
 @Serializable
 data class PlayerScienceData(
@@ -26,4 +27,24 @@ data class MutablePlayerScienceData(
     var commonSenseTechnologyData: MutableTechnologyData = MutableTechnologyData(),
     var playerKnowledgeData: MutableKnowledgeData = MutableKnowledgeData(),
     var playerTechnologyData: MutableTechnologyData = MutableTechnologyData(),
-)
+) {
+    /**
+     * Compute player knowledge data by common sense and knowledge data list
+     */
+    fun computePlayerKnowledgeData() {
+        playerKnowledgeData = DataSerializer.copy(commonSenseKnowledgeData)
+        singleKnowledgeDataList.forEach {
+            it.updateKnowledgeData(playerKnowledgeData)
+        }
+    }
+
+    /**
+     * Compute player technology data by common sense and technology data list
+     */
+    fun computePlayerTechnologyData() {
+        playerTechnologyData = DataSerializer.copy(commonSenseTechnologyData)
+        singleTechnologyDataList.forEach {
+            it.updateTechnologyData(playerTechnologyData)
+        }
+    }
+}
