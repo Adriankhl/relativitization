bcd2a2a4856e04d6eac2f9a3a9af04b873a776ed
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-05 21:47:39 +0800

    Add updatePlayerData for science info

--- gdx-core/src/main/kotlin/relativitization/game/components/info/ScienceInfo.kt
+++ gdx-core/src/main/kotlin/relativitization/game/components/info/ScienceInfo.kt
@@ -3,18 +3,35 @@ package relativitization.game.components.info
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import relativitization.game.RelativitizationGame
 import relativitization.game.utils.ScreenComponent
+import relativitization.universe.data.PlayerData
 
 class ScienceInfo(val game: RelativitizationGame) : ScreenComponent<Table>(game.assets) {
     private val gdxSettings = game.gdxSettings
 
     private val table: Table = Table()
 
+    // the currently viewing player data
+    private var playerData: PlayerData = PlayerData(-1)
+
     init {
         table.background = assets.getBackgroundColor(0.2f, 0.2f, 0.2f, 1.0f)
-    }
 
+        updatePlayerData()
+    }
 
     override fun getScreenComponent(): Table {
         return table
+    }
+
+    override fun onPrimarySelectedPlayerIdChange() {
+        updatePlayerData()
+    }
+
+    private fun updatePlayerData() {
+        playerData = if (game.universeClient.isPrimarySelectedPlayerIdValid()) {
+            game.universeClient.getUniverseData3D().get(game.universeClient.primarySelectedPlayerId)
+        } else {
+            game.universeClient.getUniverseData3D().getCurrentPlayerData()
+        }
     }
 }
