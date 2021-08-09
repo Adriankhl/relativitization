8075bb9662d29a453761779f9cc8d563f692e496
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-09 15:11:58 +0800

    Add DefaultUniverseScienceDataProcess

---
+++ universe/src/main/kotlin/relativitization/universe/science/default/DefaultUniverseScienceDataProcess.kt
@@ -0,0 +1,102 @@
+package relativitization.universe.science.default
+
+import relativitization.universe.data.UniverseSettings
+import relativitization.universe.data.science.UniverseScienceData
+import relativitization.universe.data.science.knowledge.*
+import relativitization.universe.generate.science.DefaultGenerateUniverseScienceData
+import relativitization.universe.science.UniverseScienceDataProcess
+import kotlin.math.max
+
+object DefaultUniverseScienceDataProcess : UniverseScienceDataProcess() {
+    override fun basicResearchProjectFunction(
+        universeSettings: UniverseSettings
+    ): (BasicResearchProjectData, MutableBasicResearchData) -> Unit {
+        return { basicResearchProjectData, mutableBasicResearchData ->
+            when (basicResearchProjectData.basicResearchField) {
+                BasicResearchField.MATHEMATICS -> {
+                    mutableBasicResearchData.mathematicsLevel += basicResearchProjectData.significance
+                }
+                BasicResearchField.PHYSICS -> {
+                    mutableBasicResearchData.physicsLevel += basicResearchProjectData.significance
+                }
+                BasicResearchField.COMPUTER_SCIENCE -> {
+                    mutableBasicResearchData.computerScienceLevel += basicResearchProjectData.significance
+                }
+                BasicResearchField.LIFE_SCIENCE -> {
+                    mutableBasicResearchData.lifeScienceLevel += basicResearchProjectData.significance
+                }
+                BasicResearchField.SOCIAL_SCIENCE -> {
+                    mutableBasicResearchData.socialScienceLevel += basicResearchProjectData.significance
+                }
+                BasicResearchField.HUMANITY -> {
+                    mutableBasicResearchData.humanityLevel += basicResearchProjectData.significance
+                }
+            }
+        }
+    }
+
+    override fun appliedResearchProjectFunction(
+        universeSettings: UniverseSettings
+    ): (AppliedResearchProjectData, MutableAppliedResearchData) -> Unit {
+        return { appliedResearchProjectData, mutableAppliedResearchData ->
+            when (appliedResearchProjectData.appliedResearchField) {
+                AppliedResearchField.ENERGY_TECHNOLOGY -> {
+                    mutableAppliedResearchData.energyTechnologyLevel += appliedResearchProjectData.significance
+                }
+                AppliedResearchField.FOOD_TECHNOLOGY -> {
+                    mutableAppliedResearchData.foodTechnologyLevel += appliedResearchProjectData.significance
+                }
+                AppliedResearchField.BIOMEDICAL_TECHNOLOGY -> {
+                    mutableAppliedResearchData.biomedicalTechnologyLevel += appliedResearchProjectData.significance
+                }
+                AppliedResearchField.CHEMICAL_TECHNOLOGY -> {
+                    mutableAppliedResearchData.chemicalTechnologyLevel += appliedResearchProjectData.significance
+                }
+                AppliedResearchField.ENVIRONMENTAL_TECHNOLOGY -> {
+                    mutableAppliedResearchData.energyTechnologyLevel += appliedResearchProjectData.significance
+                }
+                AppliedResearchField.ARCHITECTURE_TECHNOLOGY -> {
+                    mutableAppliedResearchData.architectureTechnologyLevel += appliedResearchProjectData.significance
+                }
+                AppliedResearchField.MACHINERY_TECHNOLOGY -> {
+                    mutableAppliedResearchData.machineryTechnologyLevel += appliedResearchProjectData.significance
+                }
+                AppliedResearchField.MATERIAL_TECHNOLOGY -> {
+                    mutableAppliedResearchData.machineryTechnologyLevel += appliedResearchProjectData.significance
+                }
+                AppliedResearchField.INFORMATION_TECHNOLOGY -> {
+                    mutableAppliedResearchData.informationTechnologyLevel += appliedResearchProjectData.significance
+                }
+                AppliedResearchField.ART_TECHNOLOGY -> {
+                    mutableAppliedResearchData.artTechnologyLevel += appliedResearchProjectData.significance
+                }
+                AppliedResearchField.MILITARY_TECHNOLOGY -> {
+                    mutableAppliedResearchData.militaryTechnologyLevel += appliedResearchProjectData.significance
+                }
+            }
+        }
+    }
+
+    override fun newUniverseScienceData(
+        universeScienceData: UniverseScienceData,
+        universeSettings: UniverseSettings
+    ): UniverseScienceData {
+
+        val numBasicResearchProject: Int = universeScienceData.basicResearchProjectDataMap.size
+        val numAppliedResearchProject: Int = universeScienceData.appliedResearchProjectDataMap.size
+
+        // Generate new projects only if there are too few remaining projects
+        val shouldGenerate: Boolean =
+            (numBasicResearchProject < 10) || (numAppliedResearchProject < 10)
+
+        return if (shouldGenerate) {
+            DefaultGenerateUniverseScienceData.generate(
+                universeScienceData,
+                max(30 - numBasicResearchProject, 0),
+                max(30 - numAppliedResearchProject, 0),
+            )
+        } else {
+            universeScienceData
+        }
+    }
+}
