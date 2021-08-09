9f593ad3f6246f0e784e16e0840644e2c97ea24d
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-09 15:40:47 +0800

    Add UniverseScienceDataProcessCollection

--- universe/src/main/kotlin/relativitization/universe/science/UniverseScienceDataProcess.kt
+++ universe/src/main/kotlin/relativitization/universe/science/UniverseScienceDataProcess.kt
@@ -1,11 +1,15 @@
 package relativitization.universe.science
 
+import relativitization.universe.ai.AICollection
 import relativitization.universe.data.UniverseSettings
 import relativitization.universe.data.science.UniverseScienceData
 import relativitization.universe.data.science.knowledge.AppliedResearchProjectData
 import relativitization.universe.data.science.knowledge.BasicResearchProjectData
 import relativitization.universe.data.science.knowledge.MutableAppliedResearchData
 import relativitization.universe.data.science.knowledge.MutableBasicResearchData
+import relativitization.universe.science.default.DefaultUniverseScienceDataProcess
+import relativitization.universe.science.empty.EmptyUniverseScienceDataProcess
+import relativitization.universe.utils.RelativitizationLogManager
 
 abstract class UniverseScienceDataProcess {
     /**
@@ -44,3 +48,21 @@ abstract class UniverseScienceDataProcess {
 
 }
 
+object UniverseScienceDataProcessCollection {
+    private val logger = RelativitizationLogManager.getLogger()
+
+    val universeScienceDataProcessNameMap: Map<String, UniverseScienceDataProcess> = mapOf(
+        "DefaultScience" to DefaultUniverseScienceDataProcess,
+        "EmptyScience" to EmptyUniverseScienceDataProcess,
+    )
+
+    fun getProcess(universeSettings: UniverseSettings): UniverseScienceDataProcess {
+        return universeScienceDataProcessNameMap.getOrElse(
+            universeSettings.universeScienceDataProcessName
+        ) {
+            logger.error("No universe science process name: ${universeSettings.universeScienceDataProcessName}," +
+                    " using default universe science data process")
+            DefaultUniverseScienceDataProcess
+        }
+    }
+}
