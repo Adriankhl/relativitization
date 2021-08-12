b7b86d2d8cbac9378a9ad95db9043bda160bb0fe
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-12 16:58:55 +0800

    Add getTariff

--- universe/src/main/kotlin/relativitization/universe/data/economy/TaxData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/economy/TaxData.kt
@@ -1,22 +1,25 @@
 package relativitization.universe.data.economy
 
 import kotlinx.serialization.Serializable
+import relativitization.universe.data.economy.resource.ResourceType
 
 /**
  * Tax rate data
  */
 @Serializable
 data class TaxRateData(
-    val tariffRate: Double = 0.0,
+    val tariffRateMap: Map<ResourceType, Double> = mapOf(),
     val minIncomeTaxRate: Double = 0.0,
     val maxIncomeTaxRate: Double = 0.0,
     val minIncomeTaxRateBoundary: Double = 0.0,
     val maxIncomeTaxRateBoundary: Double = 0.0,
-)
+) {
+    fun getTariff(resourceType: ResourceType): Double = tariffRateMap.getOrDefault(resourceType, 0.0)
+}
 
 @Serializable
 data class MutableTaxRateData(
-    var tariffRate: Double = 0.0,
+    var tariffRateMap: MutableMap<ResourceType, Double> = mutableMapOf(),
     var minIncomeTaxRate: Double = 0.0,
     var maxIncomeTaxRate: Double = 0.0,
     var minIncomeTaxRateBoundary: Double = 0.0,
