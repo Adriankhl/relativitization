6ed9c50d0aec564ff47943db9d44bd3ac9231a87
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-13 12:53:33 +0800

    Add ai task

--- universe/src/main/kotlin/relativitization/universe/data/ai/AIData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/ai/AIData.kt
@@ -4,10 +4,22 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AIData(
-    val aiName: String = "DefaultAI"
+    val aiName: String = "DefaultAI",
+    val aiTask: AITask = AITask.DEFAULT,
 )
 
 @Serializable
 data class MutableAIData(
-    var aiName: String = "DefaultAI"
+    var aiName: String = "DefaultAI",
+    var aiTask: AITask = AITask.DEFAULT,
 )
+
+enum class AITask(val value: String) {
+    DEFAULT("Default"),
+    LOGISTIC("Logistic"),
+    ;
+
+    override fun toString(): String {
+        return value
+    }
+}
