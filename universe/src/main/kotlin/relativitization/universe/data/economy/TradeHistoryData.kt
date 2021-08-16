0231f8dcb80819454347902630ac27e35b5450f2
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-16 13:14:03 +0800

    Add TradeRecordData

--- universe/src/main/kotlin/relativitization/universe/data/economy/TradeHistoryData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/economy/TradeHistoryData.kt
@@ -7,12 +7,12 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 data class TradeHistoryData(
-    val historyMap: Map<Int, Map<ResourceType, Map<ResourceQualityClass, SellRecordData>>> = mapOf(),
+    val historyMap: Map<Int, Map<ResourceType, Map<ResourceQualityClass, TradeRecordData>>> = mapOf(),
 )
 
 @Serializable
 data class MutableTradeHistoryData(
-    val historyMap: MutableMap<Int, MutableMap<ResourceType, MutableMap<ResourceQualityClass, MutableSellRecordData>>> = mutableMapOf(),
+    val historyMap: MutableMap<Int, MutableMap<ResourceType, MutableMap<ResourceQualityClass, MutableTradeRecordData>>> = mutableMapOf(),
 )
 
 /**
@@ -20,21 +20,31 @@ data class MutableTradeHistoryData(
  *
  * @property time the time of the trade
  * @property resourceType the type of resource
- * @property soldAmount the amount of the resource sold
- * @property fuelRestMassPrice the price of the resource
+ * @property resourceQualityClass the quality class of the resource
+ * @property resourceQualityData the quality data of the resource
+ * @property tradeAmount the amount of the resource sold
+ * @property price the price of the resource in fuel rest mass
  */
 @Serializable
-data class SellRecordData(
+data class TradeRecordData(
     val time: Int = -1,
     val resourceType: ResourceType = ResourceType.PLANT,
-    val soldAmount: Double = 0.0,
-    val fuelRestMassPrice: Double = 0.0,
+    val resourceQualityClass: ResourceQualityClass = ResourceQualityClass.FIRST,
+    val resourceQualityData: ResourceQualityData = ResourceQualityData(),
+    val tradeAmount: Double = 0.0,
+    val price: Double = 0.0,
 )
 
 @Serializable
-data class MutableSellRecordData(
+data class MutableTradeRecordData(
     var time: Int = -1,
     var resourceType: ResourceType = ResourceType.PLANT,
-    var soldAmount: Double = 0.0,
-    var fuelRestMassPrice: Double = 0.0,
-)
+    var resourceQualityClass: ResourceQualityClass = ResourceQualityClass.FIRST,
+    var resourceQualityData: MutableResourceQualityData = MutableResourceQualityData(),
+    var tradeAmount: Double = 0.0,
+    var price: Double = 0.0,
+) {
+    fun addSellRecord(mutableTradeRecordData: MutableTradeRecordData) {
+
+    }
+}
