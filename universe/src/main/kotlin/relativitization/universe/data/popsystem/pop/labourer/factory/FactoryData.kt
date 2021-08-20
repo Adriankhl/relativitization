b12d25b6ebf3b5e84dadfda9176fc560338b94d4
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-20 16:33:08 +0800

    Add kdoc for factory

--- universe/src/main/kotlin/relativitization/universe/data/popsystem/pop/labourer/factory/FactoryData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/popsystem/pop/labourer/factory/FactoryData.kt
@@ -1,9 +1,20 @@
 package relativitization.universe.data.popsystem.pop.labourer.factory
 
 import kotlinx.serialization.Serializable
+import relativitization.universe.data.economy.MutableResourceQualityData
 import relativitization.universe.data.economy.ResourceQualityData
 import relativitization.universe.data.economy.ResourceType
 
+/**
+ * Data for a factory of labour pop
+ *
+ * @property outputResource the output resource type
+ * @property maxOutputResourceQualityData maximum output resource quality
+ * @property maxOutputAmount maximum output resource amount
+ * @property lastOutputAmount the output amount in the latest turn
+ * @property inputResourceMap map the input resource type to the input-related data
+ * @property fuelRestMassConsumptionRate fuel consumption rate
+ */
 @Serializable
 data class FactoryData(
     val outputResource: ResourceType = ResourceType.PLANT,
@@ -34,14 +45,24 @@ data class MutableFactoryData(
     }
 }
 
+/**
+ * Input resource related data
+ *
+ * @property maxInputResourceQualityData maximum input resource quality, quality exceeding this
+ * won't improve the output quality
+ * @property amountPerOutputUnit amount of resource required to produce one unit of output resource
+ * @property lastInputAmount the input amount in the latest turn
+ */
 @Serializable
 data class InputResourceData(
+    val maxInputResourceQualityData: ResourceQualityData = ResourceQualityData(),
     val amountPerOutputUnit: Double = 1.0,
     val lastInputAmount: Double = 0.0,
 )
 
 @Serializable
 data class MutableInputResourceData(
+    var maxInputResourceQualityData: MutableResourceQualityData = MutableResourceQualityData(),
     var amountPerOutputUnit: Double = 1.0,
     var lastInputAmount: Double = 0.0,
 )
