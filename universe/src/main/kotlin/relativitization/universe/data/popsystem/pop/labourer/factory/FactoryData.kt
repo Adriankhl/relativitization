1be73f01c01e72507a684cf21e9ff2ad7c65f176
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-20 15:54:13 +0800

    Update factory data

--- universe/src/main/kotlin/relativitization/universe/data/popsystem/pop/labourer/factory/FactoryData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/popsystem/pop/labourer/factory/FactoryData.kt
@@ -8,24 +8,40 @@ import relativitization.universe.data.economy.ResourceType
 data class FactoryData(
     val outputResource: ResourceType = ResourceType.PLANT,
     val maxOutputResourceQualityData: ResourceQualityData = ResourceQualityData(),
+    val maxOutputAmount: Double = 0.0,
+    val lastOutputAmount: Double = 0.0,
     val inputResourceMap: Map<ResourceType, InputResourceData> = mapOf(),
     val fuelRestMassConsumptionRate: Double = 0.0,
-)
+) {
+    fun maxInputAmount(resourceType: ResourceType): Double {
+        val amountPerUnit: Double = inputResourceMap[resourceType]?.amountPerOutputUnit ?: 0.0
+        return amountPerUnit * maxOutputAmount
+    }
+}
 
 @Serializable
 data class MutableFactoryData(
     var outputResource: ResourceType = ResourceType.PLANT,
     var maxOutputResourceQualityData: ResourceQualityData = ResourceQualityData(),
+    var maxOutputAmount: Double = 0.0,
+    var lastOutputAmount: Double = 0.0,
     var inputResourceMap: MutableMap<ResourceType, InputResourceData> = mutableMapOf(),
     var fuelRestMassConsumptionRate: Double = 0.0,
-)
+) {
+    fun maxInputAmount(resourceType: ResourceType): Double {
+        val amountPerUnit: Double = inputResourceMap[resourceType]?.amountPerOutputUnit ?: 0.0
+        return amountPerUnit * maxOutputAmount
+    }
+}
 
 @Serializable
 data class InputResourceData(
-    val amount: Double = 1.0,
+    val amountPerOutputUnit: Double = 1.0,
+    val lastInputAmount: Double = 0.0,
 )
 
 @Serializable
 data class MutableInputResourceData(
-    var amount: Double = 1.0,
+    var amountPerOutputUnit: Double = 1.0,
+    var lastInputAmount: Double = 0.0,
 )
