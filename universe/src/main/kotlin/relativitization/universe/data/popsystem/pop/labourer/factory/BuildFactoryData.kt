793e70606b40cf292a334cc9369cd5031a0c1b78
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-20 19:13:20 +0800

    Change scale to targetQualityData for factory building

--- universe/src/main/kotlin/relativitization/universe/data/popsystem/pop/labourer/factory/BuildFactoryData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/popsystem/pop/labourer/factory/BuildFactoryData.kt
@@ -1,6 +1,7 @@
 package relativitization.universe.data.popsystem.pop.labourer.factory
 
 import kotlinx.serialization.Serializable
+import relativitization.universe.data.economy.MutableResourceQualityData
 import relativitization.universe.data.economy.ResourceQualityData
 import relativitization.universe.data.economy.ResourceType
 
@@ -8,19 +9,19 @@ import relativitization.universe.data.economy.ResourceType
  * A factory waiting to be built
  *
  * @property outputResource the output resource of the factory
- * @property qualityScale how good is the output resource quality in a scale between 0 and 1
+ * @property targetQualityData the target output resource quality
  * @property progress the progress of building, from 0 and 1
  */
 @Serializable
 data class BuildFactoryData(
     val outputResource: ResourceType = ResourceType.PLANT,
-    val qualityScale: Double = 0.0,
+    val targetQualityData: ResourceQualityData = ResourceQualityData(),
     val progress: Double = 0.0,
 )
 
 @Serializable
 data class MutableBuildFactoryData(
     var outputResource: ResourceType = ResourceType.PLANT,
-    var qualityScale: Double = 0.0,
+    var targetQualityData: MutableResourceQualityData = MutableResourceQualityData()
     var progress: Double = 0.0,
 )
