d5ac3c16d887121603f6d71e9d0abf53cbe1154d
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-24 20:56:56 +0800

    Add DataComponentMapTest

--- universe/src/main/kotlin/relativitization/universe/data/component/PlayerDataComponent.kt
+++ universe/src/main/kotlin/relativitization/universe/data/component/PlayerDataComponent.kt
@@ -1,9 +1,36 @@
 package relativitization.universe.data.component
 
 import kotlinx.serialization.Serializable
+import relativitization.universe.utils.RelativitizationLogManager
+import kotlin.reflect.KClass
 
 @Serializable
 sealed class PlayerDataComponent
 
 @Serializable
 sealed class MutablePlayerDataComponent
+
+@Serializable
+data class DataComponentMap(
+    val dataMap: Map<String, PlayerDataComponent> = mapOf(),
+) {
+    constructor(dataList: List<PlayerDataComponent>) : this(
+        dataMap = dataList.map {
+            (it::class.simpleName ?: "") to it
+        }.toMap()
+    )
+
+    inline fun <reified T : PlayerDataComponent> getOrDefault(key: KClass<T>, defaultValue: T): T {
+        val data: PlayerDataComponent? = dataMap[key.simpleName]
+        return if(data is T) {
+            data
+        } else {
+            defaultValue
+        }
+    }
+
+
+    companion object {
+        private val logger = RelativitizationLogManager.getLogger()
+    }
+}
