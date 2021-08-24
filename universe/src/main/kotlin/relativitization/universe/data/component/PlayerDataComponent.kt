8230d438323dcf595fa917f32117ca490a84a46b
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-24 21:04:42 +0800

    Add MutableDataComponentMap

--- universe/src/main/kotlin/relativitization/universe/data/component/PlayerDataComponent.kt
+++ universe/src/main/kotlin/relativitization/universe/data/component/PlayerDataComponent.kt
@@ -20,13 +20,52 @@ data class DataComponentMap(
         }.toMap()
     )
 
-    inline fun <reified T : PlayerDataComponent> getOrDefault(key: KClass<T>, defaultValue: T): T {
+    inline fun <reified T : PlayerDataComponent> getOrDefault(
+        key: KClass<T>,
+        defaultValue: T
+    ): T {
         val data: PlayerDataComponent? = dataMap[key.simpleName]
-        return if(data is T) {
+        return if (data is T) {
             data
         } else {
             defaultValue
         }
+    }
+
+
+    companion object {
+        private val logger = RelativitizationLogManager.getLogger()
+    }
+}
+
+@Serializable
+data class MutableDataComponentMap(
+    val dataMap: MutableMap<String, MutablePlayerDataComponent> = mutableMapOf(),
+) {
+    constructor(dataList: List<MutablePlayerDataComponent>) : this(
+        dataMap = dataList.map {
+            (it::class.simpleName ?: "") to it
+        }.toMap().toMutableMap()
+    )
+
+    inline fun <reified T : MutablePlayerDataComponent> getOrDefault(
+        key: KClass<T>,
+        defaultValue: T
+    ): T {
+        val data: MutablePlayerDataComponent? = dataMap[key.simpleName]
+        return if (data is T) {
+            data
+        } else {
+            defaultValue
+        }
+    }
+
+    fun <T : MutablePlayerDataComponent> put(dataComponent: T) {
+        dataMap[(dataComponent::class.simpleName ?: "")] = dataComponent
+    }
+
+    fun <T : MutablePlayerDataComponent> remove(key: KClass<T>) {
+        dataMap.remove(key::class.simpleName)
     }
 
 
