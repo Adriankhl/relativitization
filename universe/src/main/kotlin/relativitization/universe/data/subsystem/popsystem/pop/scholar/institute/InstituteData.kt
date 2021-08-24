a5c3d1576c0b9f10ca4be61df61c5113caa4aaa3
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-08-24 13:43:52 +0800

    Update institute data

--- universe/src/main/kotlin/relativitization/universe/data/subsystem/popsystem/pop/scholar/institute/InstituteData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/subsystem/popsystem/pop/scholar/institute/InstituteData.kt
@@ -6,4 +6,8 @@ import kotlinx.serialization.Serializable
 data class InstituteData(
     val xCor: Double = 0.0,
     val yCor: Double = 0.0,
+    val maxNumEmployee: Double = 0.0,
+    val lastNumEmployee: Double = 0.0,
+    val size: Double = 0.0,
+    val reputation: Double = 0.0,
 )
