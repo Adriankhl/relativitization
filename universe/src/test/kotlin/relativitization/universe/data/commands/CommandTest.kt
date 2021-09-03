13ce1775636921315ad2738cbf32e9c934a59231
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-03 21:09:23 +0800

    Add reflection test

---
+++ universe/src/test/kotlin/relativitization/universe/data/commands/CommandTest.kt
@@ -0,0 +1,21 @@
+package relativitization.universe.data.commands
+
+import relativitization.universe.data.component.physics.Int4D
+import relativitization.universe.data.component.physics.Velocity
+import kotlin.test.Test
+
+internal class CommandTest {
+    @Test
+    fun reflectionTest() {
+        val c1: ChangeVelocityCommand = ChangeVelocityCommand(
+            targetVelocity = Velocity(0.1, 0.0, 0.0),
+            toId = 2,
+            fromId = 1,
+            fromInt4D = Int4D(7, 0, 0, 0),
+        )
+
+        val c2: Command = c1
+
+        assert(c1::class.simpleName == c2::class.simpleName)
+    }
+}
