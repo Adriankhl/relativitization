0bdbe29d1f8bdbb252e33107d8b90cd0226b8484
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-06 19:31:28 +0800

    Complete productionQualityClass

--- universe/src/main/kotlin/relativitization/universe/mechanisms/production/FactoryProduction.kt
+++ universe/src/main/kotlin/relativitization/universe/mechanisms/production/FactoryProduction.kt
@@ -5,6 +5,7 @@ import relativitization.universe.data.UniverseData3DAtPlayer
 import relativitization.universe.data.UniverseSettings
 import relativitization.universe.data.commands.Command
 import relativitization.universe.data.component.economy.*
+import relativitization.universe.data.component.popsystem.pop.labourer.factory.MutableFactoryData
 import relativitization.universe.data.component.popsystem.pop.labourer.factory.MutableInputResourceData
 import relativitization.universe.data.component.science.UniverseScienceData
 import relativitization.universe.mechanisms.Mechanism
@@ -28,16 +29,22 @@ object FactoryProduction : Mechanism() {
     /**
      * How much productivity is used due to the limitation of resource
      *
-     * @param inputResourceMap the input resources required
+     * @param mutableFactoryData the factory producing this resource
      * @param resourceData the amount of resource owned by the player
      */
     fun productivityFraction(
-        inputResourceMap: MutableMap<ResourceType, MutableInputResourceData>,
+        mutableFactoryData: MutableFactoryData,
         resourceData: MutableResourceData,
     ): Double {
-        val fractionList: List<Double> = inputResourceMap.map { (type, inputResourceData) ->
-            0.0
+        val fractionList: List<Double> = mutableFactoryData.inputResourceMap.map { (type, inputResourceData) ->
+            val requiredAmount: Double = inputResourceData.amountPerOutputUnit * mutableFactoryData.maxOutputAmount
+            val qualityClass: ResourceQualityClass = resourceData.productionQualityClass(
+                type,
+                requiredAmount,
+                inputResourceData.maxInputResourceQualityData
+            )
+            resourceData.getProductionResourceAmount(type, qualityClass) / requiredAmount
         }
-        return 0.0
+        return fractionList.maxOrNull() ?: 0.0
     }
 }
