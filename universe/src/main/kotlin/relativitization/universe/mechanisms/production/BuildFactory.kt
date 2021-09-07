8d49d9025718a42f11b017c04d861c65b896abb4
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-07 16:21:35 +0800

    Add BuildFactory mechanism

---
+++ universe/src/main/kotlin/relativitization/universe/mechanisms/production/BuildFactory.kt
@@ -0,0 +1,19 @@
+package relativitization.universe.mechanisms.production
+
+import relativitization.universe.data.MutablePlayerData
+import relativitization.universe.data.UniverseData3DAtPlayer
+import relativitization.universe.data.UniverseSettings
+import relativitization.universe.data.commands.Command
+import relativitization.universe.data.component.science.UniverseScienceData
+import relativitization.universe.mechanisms.Mechanism
+
+object BuildFactory : Mechanism() {
+    override fun process(
+        mutablePlayerData: MutablePlayerData,
+        universeData3DAtPlayer: UniverseData3DAtPlayer,
+        universeSettings: UniverseSettings,
+        universeScienceData: UniverseScienceData
+    ): List<Command> {
+        TODO("Not yet implemented")
+    }
+}
