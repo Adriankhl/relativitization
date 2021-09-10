b4cb8cae736f7fa9a57861bc1087ef1a19a75412
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-10 14:31:44 +0800

    Add FactoryProductionTest

---
+++ universe/src/test/kotlin/relativitization/universe/mechanisms/production/FactoryProductionTest.kt
@@ -0,0 +1,33 @@
+package relativitization.universe.mechanisms.production
+
+import kotlinx.coroutines.runBlocking
+import relativitization.universe.Universe
+import relativitization.universe.data.UniverseData3DAtPlayer
+import relativitization.universe.generate.GenerateSettings
+import relativitization.universe.generate.UniverseGenerationCollection
+import kotlin.test.Test
+
+internal class FactoryProductionTest {
+    @Test
+    fun fixedMinimalTest() {
+        val universe = Universe(UniverseGenerationCollection.generate(GenerateSettings()), ".")
+
+
+        runBlocking {
+            universe.postProcessUniverse(mapOf(
+                1 to listOf()),
+                mapOf(
+                    2 to listOf(),
+                    3 to listOf(),
+                    4 to listOf()
+                )
+            )
+            universe.preProcessUniverse()
+        }
+
+
+        val view8: UniverseData3DAtPlayer = universe.getUniverse3DViewAtPlayer(1)
+
+        assert(view8.get(1).playerInternalData.physicsData().fuelRestMass == 102.0)
+    }
+}
