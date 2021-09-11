eb5094ff6aaa1f3d06edb287a5e74f2435ebc1f9
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-11 20:51:00 +0800

    Add I18NString toMessageFormat()

--- universe/src/main/kotlin/relativitization/universe/utils/I18NString.kt
+++ universe/src/main/kotlin/relativitization/universe/utils/I18NString.kt
@@ -29,6 +29,22 @@ data class I18NString(
         }
     }.reduce { acc, s ->  acc + s }
 
+    fun toMessageFormat(): List<String> {
+        val s1: String = message.map {
+            when (it) {
+                is RealString -> it.str
+                is IntString -> try {
+                    "{${it.int}}"
+                } catch (e: Throwable) {
+                    logger.error("Problematic I18NString: $this")
+                    ""
+                }
+            }
+        }.reduce { acc, s ->  acc + s }
+
+        return listOf(s1) + arg
+    }
+
     companion object {
         private val logger = RelativitizationLogManager.getLogger()
     }
