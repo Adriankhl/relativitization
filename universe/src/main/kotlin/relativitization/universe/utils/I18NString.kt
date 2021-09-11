c3381fcadd148933bd0f5647597f8b9fbda69b18
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-11 22:05:49 +0800

    Add plus for I18NString

--- universe/src/main/kotlin/relativitization/universe/utils/I18NString.kt
+++ universe/src/main/kotlin/relativitization/universe/utils/I18NString.kt
@@ -24,6 +24,19 @@ data class I18NString(
     val arg: List<String>
 ) {
     /**
+     * Combine 2 I18NString, increase the index of IntString in the other I18NString
+     */
+    operator fun plus(other: I18NString): I18NString {
+        val newMessage: List<TString> = message + other.message.map {
+            when (it) {
+                is RealString -> it
+                is IntString -> IntString(it.int + arg.size)
+            }
+        }
+        return I18NString(newMessage, arg + other.arg)
+    }
+
+    /**
      * Convert to a string
      */
     fun toNormalString(): String = message.map {
