eef2914c4704536f4f3010de9b22100b706672c8
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-14 20:20:20 +0800

    Add fixedMinimalStrangeCaseTest

--- universe/src/test/kotlin/relativitization/universe/data/events/MoveToDouble3DEventTest.kt
+++ universe/src/test/kotlin/relativitization/universe/data/events/MoveToDouble3DEventTest.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.runBlocking
 import relativitization.universe.Universe
 import relativitization.universe.data.UniverseData3DAtPlayer
 import relativitization.universe.data.commands.AddEventCommand
+import relativitization.universe.data.component.physics.Double4D
 import relativitization.universe.data.component.physics.Int4D
 import relativitization.universe.generate.GenerateSettings
 import relativitization.universe.generate.GenerateUniverse
@@ -64,5 +65,62 @@ internal class MoveToDouble3DEventTest {
 
         val view: UniverseData3DAtPlayer = universe.getUniverse3DViewAtPlayer(1)
         assert(view.get(2).double4D.toDouble3D() == view.get(3).groupCenterDouble3D(view.universeSettings.groupEdgeLength))
+    }
+
+    @Test
+    fun fixedMinimalStrangeCaseTest() {
+        val universe = Universe(UniverseGenerationCollection.generate(GenerateSettings()), ".")
+        val view7: UniverseData3DAtPlayer = universe.getUniverse3DViewAtPlayer(1)
+        assert(view7.get(1).int4D == Int4D(7, 0, 0, 0))
+
+        val originalDouble4D: Double4D = view7.get(1).double4D
+
+        val event = MoveToDouble3DEvent(
+            toId = 1,
+            fromId = 1,
+            targetDouble3D = view7.get(3)
+                .groupCenterDouble3D(view7.universeSettings.groupEdgeLength),
+            maxSpeed = 0.2,
+            stayTime = 999
+
+        )
+
+        val command = AddEventCommand(
+            event,
+            Int4D(7, 0, 0, 0),
+        )
+
+        runBlocking {
+            universe.postProcessUniverse(
+                mapOf(
+                    1 to listOf(command)
+                ),
+                mapOf(
+                    2 to listOf(),
+                    3 to listOf(),
+                    4 to listOf()
+                )
+            )
+            universe.preProcessUniverse()
+        }
+
+
+        runBlocking {
+            for (i in 1..20) {
+                universe.postProcessUniverse(
+                    mapOf(
+                        1 to listOf()
+                    ),
+                    mapOf(
+                        2 to listOf(),
+                        3 to listOf(),
+                    )
+                )
+                universe.preProcessUniverse()
+            }
+        }
+
+        val view: UniverseData3DAtPlayer = universe.getUniverse3DViewAtPlayer(1)
+        assert(view.get(1).double4D.toDouble3D() == originalDouble4D.toDouble3D())
     }
 }
