b1758bc6c3b7bf75acf6a84c9f4c255f3b7b2fd9
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-18 19:19:16 +0800

    Add properties for BuildFactoryCommand

--- universe/src/main/kotlin/relativitization/universe/data/commands/FactoryCommands.kt
+++ universe/src/main/kotlin/relativitization/universe/data/commands/FactoryCommands.kt
@@ -3,15 +3,25 @@ package relativitization.universe.data.commands
 import kotlinx.serialization.Serializable
 import relativitization.universe.data.MutablePlayerData
 import relativitization.universe.data.UniverseSettings
+import relativitization.universe.data.component.economy.ResourceQualityData
+import relativitization.universe.data.component.economy.ResourceType
 import relativitization.universe.data.component.physics.Int4D
 import relativitization.universe.utils.I18NString
 
+/**
+ * Build a factory on player
+ *
+ * @property topLeaderId the player id of the top leader of the sender
+ */
 @Serializable
 data class BuildFactoryCommand(
     override val toId: Int,
     override val fromId: Int,
     override val fromInt4D: Int4D,
     val topLeaderId: Int,
+    val resourceType: ResourceType,
+    val qualityLevel: Double,
+    val senderMaxFactoryQuality: ResourceQualityData,
 ) : Command() {
     override val description: I18NString = I18NString(
         listOf(),
@@ -30,7 +40,13 @@ data class BuildFactoryCommand(
         return sameTopLeader || playerData.playerInternalData.politicsData().allowForeignInvestor
     }
 
+    override fun selfExecuteBeforeSend(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ) {
+    }
+
     override fun execute(playerData: MutablePlayerData, universeSettings: UniverseSettings) {
-        TODO("Not yet implemented")
+        playerData
     }
 }
