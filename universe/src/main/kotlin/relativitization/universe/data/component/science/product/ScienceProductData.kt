cc0f2e7f2a83de92393805276caa2533349507a0
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-18 19:38:43 +0800

    Add maxFactoryQualityMap

--- universe/src/main/kotlin/relativitization/universe/data/component/science/product/ScienceProductData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/component/science/product/ScienceProductData.kt
@@ -1,15 +1,20 @@
 package relativitization.universe.data.component.science.product
 
 import kotlinx.serialization.Serializable
+import relativitization.universe.data.component.economy.MutableResourceQualityData
+import relativitization.universe.data.component.economy.ResourceQualityData
+import relativitization.universe.data.component.economy.ResourceType
 
 @Serializable
 data class ScienceProductData(
     val maxShipRestMass: Double = 10000.0,
     val maxShipEnginePowerByRestMass: Double = 1E-6,
+    val maxFactoryQualityMap: Map<ResourceType, ResourceQualityData> = mapOf(),
 )
 
 @Serializable
 data class MutableScienceProductData(
     var maxShipRestMass: Double = 10000.0,
     var maxShipEnginePowerByRestMass: Double = 1E-6,
+    var maxFactoryQualityMap: MutableMap<ResourceType, MutableResourceQualityData> = mutableMapOf(),
 )
