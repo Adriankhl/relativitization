c2e86c7d3ae87993ff755c338d5ad777f6bf9a06
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-19 17:15:57 +0800

    Complete description for SendResourceCommand

--- universe/src/main/kotlin/relativitization/universe/data/commands/LogisticsCommands.kt
+++ universe/src/main/kotlin/relativitization/universe/data/commands/LogisticsCommands.kt
@@ -3,29 +3,60 @@ package relativitization.universe.data.commands
 import kotlinx.serialization.Serializable
 import relativitization.universe.data.MutablePlayerData
 import relativitization.universe.data.UniverseSettings
+import relativitization.universe.data.component.economy.ResourceQualityClass
+import relativitization.universe.data.component.economy.ResourceType
 import relativitization.universe.data.component.physics.Int4D
 import relativitization.universe.utils.I18NString
+import relativitization.universe.utils.IntString
+import relativitization.universe.utils.RealString
 
 @Serializable
 data class SendResourceCommand(
     override val toId: Int,
     override val fromId: Int,
     override val fromInt4D: Int4D,
+    val targetPlayerId: Int,
+    val resourceType: ResourceType,
+    val resourceQualityClass: ResourceQualityClass,
+    val amount: Double,
 ) : Command() {
     override val description: I18NString = I18NString(
-        listOf(),
-        listOf(),
+        listOf(
+            RealString("Send "),
+            IntString(0),
+            RealString(" "),
+            IntString(1),
+            RealString(" (class: "),
+            IntString(2),
+            RealString(") from player "),
+            IntString(3),
+            RealString(" to "),
+            IntString(4),
+        ),
+        listOf(
+            amount.toString(),
+            resourceType.toString(),
+            resourceQualityClass.toString(),
+            toId.toString(),
+            targetPlayerId.toString(),
+        ),
     )
 
     override fun canSend(playerData: MutablePlayerData, universeSettings: UniverseSettings): Boolean {
-        TODO("Not yet implemented")
+        return playerData.isSubOrdinateOrSelf(toId)
     }
 
     override fun canExecute(
         playerData: MutablePlayerData,
         universeSettings: UniverseSettings
     ): Boolean {
-        TODO("Not yet implemented")
+        return playerData.isLeaderOrSelf(fromId)
+    }
+
+    override fun selfExecuteBeforeSend(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ) {
     }
 
     override fun execute(playerData: MutablePlayerData, universeSettings: UniverseSettings) {
