853709a0085abfb8af7daaba0ebf9fd68d853c2f
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-26 20:04:53 +0800

    Remove build factory data

--- universe/src/main/kotlin/relativitization/universe/data/component/popsystem/pop/labourer/LabourerPopData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/component/popsystem/pop/labourer/LabourerPopData.kt
@@ -3,9 +3,7 @@ package relativitization.universe.data.component.popsystem.pop.labourer
 import kotlinx.serialization.Serializable
 import relativitization.universe.data.component.popsystem.pop.CommonPopData
 import relativitization.universe.data.component.popsystem.pop.MutableCommonPopData
-import relativitization.universe.data.component.popsystem.pop.labourer.factory.BuildFactoryData
 import relativitization.universe.data.component.popsystem.pop.labourer.factory.FactoryData
-import relativitization.universe.data.component.popsystem.pop.labourer.factory.MutableBuildFactoryData
 import relativitization.universe.data.component.popsystem.pop.labourer.factory.MutableFactoryData
 import relativitization.universe.maths.collection.ListFind
 
@@ -13,24 +11,16 @@ import relativitization.universe.maths.collection.ListFind
 data class LabourerPopData(
     val commonPopData: CommonPopData = CommonPopData(),
     val factoryMap: Map<Int, FactoryData> = mapOf(),
-    val buildingFactoryMap: Map<Int, BuildFactoryData> = mapOf(),
 )
 
 @Serializable
 data class MutableLabourerPopData(
     var commonPopData: MutableCommonPopData = MutableCommonPopData(),
     var factoryMap: MutableMap<Int, MutableFactoryData> = mutableMapOf(),
-    var buildingFactoryMap: MutableMap<Int, MutableBuildFactoryData> = mutableMapOf(),
 ) {
     fun addFactory(mutableFactoryData: MutableFactoryData) {
-        val allIdList: List<Int> = factoryMap.keys.toList() + buildingFactoryMap.keys.toList()
+        val allIdList: List<Int> = factoryMap.keys.toList()
         val newId: Int = ListFind.minMissing(allIdList, 0)
         factoryMap[newId] = mutableFactoryData
-    }
-
-    fun addBuildFactory(mutableBuildFactoryData: MutableBuildFactoryData) {
-        val allIdList: List<Int> = factoryMap.keys.toList() + buildingFactoryMap.keys.toList()
-        val newId: Int = ListFind.minMissing(allIdList, 0)
-        buildingFactoryMap[newId] = mutableBuildFactoryData
     }
 }
