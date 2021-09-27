4c1c23bc210150e8d279bf3dc1e6d60c1d0049c7
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-27 13:42:54 +0800

    Remove ideal factory

--- universe/src/main/kotlin/relativitization/universe/data/component/science/product/ScienceProductData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/component/science/product/ScienceProductData.kt
@@ -4,12 +4,14 @@ import kotlinx.serialization.Serializable
 import relativitization.universe.data.component.economy.MutableResourceQualityData
 import relativitization.universe.data.component.economy.ResourceQualityData
 import relativitization.universe.data.component.economy.ResourceType
+import relativitization.universe.data.component.popsystem.pop.labourer.factory.FactoryInternalData
+import relativitization.universe.data.component.popsystem.pop.labourer.factory.MutableFactoryInternalData
 
 @Serializable
 data class ScienceProductData(
     val maxShipRestMass: Double = 10000.0,
     val maxShipEnginePowerByRestMass: Double = 1E-6,
-    val idealFactoryMap: Map<ResourceType, IdealFactoryData> = mapOf(),
+    val idealFactoryMap: Map<ResourceType, FactoryInternalData> = mapOf(),
     val fuelLogisticsLossFractionPerDistance: Double = 0.9,
     val resourceLogisticsLossFractionPerDistance: Double = 0.9,
 )
@@ -18,7 +20,7 @@ data class ScienceProductData(
 data class MutableScienceProductData(
     var maxShipRestMass: Double = 10000.0,
     var maxShipEnginePowerByRestMass: Double = 1E-6,
-    var idealFactoryMap: MutableMap<ResourceType, MutableIdealFactoryData> = mutableMapOf(),
+    var idealFactoryMap: MutableMap<ResourceType, MutableFactoryInternalData> = mutableMapOf(),
     var fuelLogisticsLossFractionPerDistance: Double = 0.9,
     var resourceLogisticsLossFractionPerDistance: Double = 0.9,
 )
