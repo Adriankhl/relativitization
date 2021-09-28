da00bb2948887089f66aea8cfd73940bdad4e04d
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-28 12:52:56 +0800

    Add for MutableScienceProductData

--- universe/src/main/kotlin/relativitization/universe/data/component/science/product/ScienceProductData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/component/science/product/ScienceProductData.kt
@@ -1,11 +1,13 @@
 package relativitization.universe.data.component.science.product
 
 import kotlinx.serialization.Serializable
+import relativitization.universe.data.component.economy.MutableResourceQualityData
 import relativitization.universe.data.component.economy.ResourceQualityData
 import relativitization.universe.data.component.economy.ResourceType
 import relativitization.universe.data.component.popsystem.pop.labourer.factory.FactoryInternalData
 import relativitization.universe.data.component.popsystem.pop.labourer.factory.InputResourceData
 import relativitization.universe.data.component.popsystem.pop.labourer.factory.MutableFactoryInternalData
+import relativitization.universe.data.component.popsystem.pop.labourer.factory.MutableInputResourceData
 import relativitization.universe.maths.algebra.Quadratic
 import relativitization.universe.utils.RelativitizationLogManager
 
@@ -143,4 +145,122 @@ data class MutableScienceProductData(
     var idealFactoryMap: MutableMap<ResourceType, MutableFactoryInternalData> = mutableMapOf(),
     var fuelLogisticsLossFractionPerDistance: Double = 0.9,
     var resourceLogisticsLossFractionPerDistance: Double = 0.9,
-)
+) {
+    fun getIdealFactory(resourceType: ResourceType): MutableFactoryInternalData {
+        return idealFactoryMap.getOrPut(resourceType) {
+            logger.debug("No ideal factory with type $resourceType")
+            MutableFactoryInternalData(outputResource = resourceType)
+        }
+    }
+
+    fun newFactoryInternalData(
+        outputResourceType: ResourceType,
+        qualityLevel: Double
+    ): MutableFactoryInternalData {
+        val actualQualityLevel: Double = when {
+            qualityLevel > 1.0 -> {
+                logger.error("quality level greater than 1.0")
+                1.0
+            }
+            qualityLevel < 0.0 -> {
+                logger.error("quality level smaller than 0.0")
+                0.0
+            }
+            else -> {
+                qualityLevel
+            }
+        }
+
+        val idealFactory: MutableFactoryInternalData = getIdealFactory(outputResourceType)
+
+        val maxOutputResourceQualityData: MutableResourceQualityData = idealFactory.maxOutputResourceQualityData * actualQualityLevel
+
+        // Max increase to 5 times
+        val maxOutputAmount: Double = idealFactory.maxOutputAmount * Quadratic.standard(
+            x = actualQualityLevel,
+            xMin = 0.0,
+            xMax = 1.0,
+            yMin = 1.0,
+            yMax = 5.0,
+            increasing = false,
+            accelerate = true
+        )
+
+        // Reduce the required input resource quality and amount
+        val inputResourceMap: MutableMap<ResourceType, MutableInputResourceData> = idealFactory.inputResourceMap.mapValues {
+            val inputResourceData: MutableInputResourceData = it.value
+
+            val qualityFactor: Double = Quadratic.standard(
+                x = actualQualityLevel,
+                xMin = 0.0,
+                xMax = 1.0,
+                yMin = 0.0,
+                yMax = 1.0,
+                increasing = true,
+                accelerate = true
+            )
+
+            val amountFactor: Double = Quadratic.standard(
+                x = actualQualityLevel,
+                xMin = 0.0,
+                xMax = 1.0,
+                yMin = 0.2,
+                yMax = 1.0,
+                increasing = true,
+                accelerate = true
+            )
+
+            MutableInputResourceData(
+                maxInputResourceQualityData = inputResourceData.maxInputResourceQualityData * qualityFactor,
+                amountPerOutputUnit = inputResourceData.amountPerOutputUnit * amountFactor
+            )
+        }.toMutableMap()
+
+        // Reduce the fuel rest mass consumption rate
+        val fuelRestMassConsumptionRate: Double = idealFactory.fuelRestMassConsumptionRate * Quadratic.standard(
+            x = actualQualityLevel,
+            xMin = 0.0,
+            xMax = 1.0,
+            yMin = 0.2,
+            yMax = 1.0,
+            increasing = true,
+            accelerate = true
+        )
+
+        // Reduce the number of employee needed
+        val maxNumEmployee: Double = idealFactory.maxNumEmployee * Quadratic.standard(
+            x = actualQualityLevel,
+            xMin = 0.0,
+            xMax = 1.0,
+            yMin = 0.2,
+            yMax = 1.0,
+            increasing = true,
+            accelerate = true
+        )
+
+        // Reduce size
+        val size: Double = idealFactory.size * Quadratic.standard(
+            x = actualQualityLevel,
+            xMin = 0.0,
+            xMax = 1.0,
+            yMin = 0.2,
+            yMax = 1.0,
+            increasing = true,
+            accelerate = true
+        )
+
+        return MutableFactoryInternalData(
+            outputResource = outputResourceType,
+            maxOutputResourceQualityData = maxOutputResourceQualityData,
+            maxOutputAmount = maxOutputAmount,
+            inputResourceMap = inputResourceMap,
+            fuelRestMassConsumptionRate = fuelRestMassConsumptionRate,
+            maxNumEmployee = maxNumEmployee,
+            size = size
+        )
+    }
+
+    companion object {
+        private val logger = RelativitizationLogManager.getLogger()
+    }
+}
