4c28ba2fc91cf00c9128f49fbff277fcc5626a98
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-09-30 15:43:57 +0800

    Complete BuildFactoryCommand

--- universe/src/main/kotlin/relativitization/universe/data/commands/FactoryCommands.kt
+++ universe/src/main/kotlin/relativitization/universe/data/commands/FactoryCommands.kt
@@ -4,22 +4,28 @@ import kotlinx.serialization.Serializable
 import relativitization.universe.data.MutablePlayerData
 import relativitization.universe.data.UniverseSettings
 import relativitization.universe.data.component.physics.Int4D
+import relativitization.universe.data.component.popsystem.MutableCarrierData
 import relativitization.universe.data.component.popsystem.pop.labourer.factory.FactoryInternalData
+import relativitization.universe.data.component.popsystem.pop.labourer.factory.MutableFactoryData
+import relativitization.universe.data.component.popsystem.pop.labourer.factory.MutableFactoryInternalData
+import relativitization.universe.data.serializer.DataSerializer
 import relativitization.universe.utils.I18NString
 import relativitization.universe.utils.IntString
 import relativitization.universe.utils.RealString
+import relativitization.universe.utils.RelativitizationLogManager
 
 /**
  * Build a factory on player
  *
- * @property topLeaderId the player id of the top leader of the sender
+ * @property senderTopLeaderId the player id of the top leader of the sender
  */
 @Serializable
 data class BuildFactoryCommand(
     override val toId: Int,
     override val fromId: Int,
     override val fromInt4D: Int4D,
-    val topLeaderId: Int,
+    val senderTopLeaderId: Int,
+    val targetCarrierId: Int,
     val factoryInternalData: FactoryInternalData,
     val qualityLevel: Double,
     val storedFuelRestMass: Double,
@@ -33,7 +39,7 @@ data class BuildFactoryCommand(
         playerData: MutablePlayerData,
         universeSettings: UniverseSettings
     ): CanSendWithMessage {
-        val sameTopLeaderId: Boolean = playerData.topLeaderId() == topLeaderId
+        val sameTopLeaderId: Boolean = playerData.topLeaderId() == senderTopLeaderId
         val sameTopLeaderIdI18NString: I18NString = if (sameTopLeaderId) {
             I18NString("")
         } else {
@@ -46,7 +52,7 @@ data class BuildFactoryCommand(
                     RealString(". ")
                 ),
                 listOf(
-                    topLeaderId.toString(),
+                    senderTopLeaderId.toString(),
                     playerData.topLeaderId().toString(),
                 ),
             )
@@ -94,8 +100,13 @@ data class BuildFactoryCommand(
         playerData: MutablePlayerData,
         universeSettings: UniverseSettings
     ): Boolean {
-        val sameTopLeader: Boolean = playerData.topLeaderId() == topLeaderId
-        return sameTopLeader || playerData.playerInternalData.politicsData().allowForeignInvestor
+        val sameTopLeader: Boolean = playerData.topLeaderId() == senderTopLeaderId
+        val allowConstruction: Boolean =
+            (sameTopLeader || playerData.playerInternalData.politicsData().allowForeignInvestor)
+
+        val hasCarrier: Boolean = playerData.playerInternalData.popSystemData().carrierDataMap.containsKey(targetCarrierId)
+
+        return allowConstruction
     }
 
     override fun selfExecuteBeforeSend(
@@ -108,9 +119,34 @@ data class BuildFactoryCommand(
                 qualityLevel
             )
         playerData.playerInternalData.physicsData().fuelRestMassData.production -= fuelNeeded
+
     }
 
     override fun execute(playerData: MutablePlayerData, universeSettings: UniverseSettings) {
-        playerData.playerInternalData
+
+        val hasCarrier: Boolean = playerData.playerInternalData.popSystemData().carrierDataMap.containsKey(targetCarrierId)
+
+        if (hasCarrier) {
+            val carrier: MutableCarrierData = playerData.playerInternalData.popSystemData().carrierDataMap.getValue(targetCarrierId)
+            carrier.allPopData.labourerPopData.addFactory(
+                MutableFactoryData(
+                    ownerPlayerId = fromId,
+                    factoryInternalData = DataSerializer.copy(factoryInternalData),
+                    numBuilding = 1,
+                    isOpened = true,
+                    lastOutputAmount = 0.0,
+                    lastInputAmountMap = mutableMapOf(),
+                    storedFuelRestMass = storedFuelRestMass,
+                    lastNumEmployee = 0.0
+                )
+            )
+        } else {
+            logger.error("No carrier $targetCarrierId")
+        }
+    }
+
+
+    companion object {
+        private val logger = RelativitizationLogManager.getLogger()
     }
 }
