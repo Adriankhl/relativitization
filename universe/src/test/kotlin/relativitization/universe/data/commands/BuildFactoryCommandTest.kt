828647da0130bce3415707e4bbc3061951ba1708
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-10-01 18:18:01 +0800

    Add BuildFactoryCommandTest

---
+++ universe/src/test/kotlin/relativitization/universe/data/commands/BuildFactoryCommandTest.kt
@@ -0,0 +1,21 @@
+package relativitization.universe.data.commands
+
+import relativitization.universe.Universe
+import relativitization.universe.data.UniverseData3DAtPlayer
+import relativitization.universe.data.component.economy.ResourceQualityClass
+import relativitization.universe.data.component.economy.ResourceType
+import relativitization.universe.generate.GenerateSettings
+import relativitization.universe.generate.UniverseGenerationCollection
+import kotlin.test.Test
+
+internal class BuildFactoryCommandTest {
+    @Test
+    fun fixedMinimalTest() {
+        val universe = Universe(UniverseGenerationCollection.generate(GenerateSettings()), ".")
+        val view7: UniverseData3DAtPlayer = universe.getUniverse3DViewAtPlayer(1)
+        view7.get(2).playerInternalData.economyData().resourceData.getTradeResourceAmount(
+            ResourceType.PLANT,
+            ResourceQualityClass.FIRST,
+        )
+    }
+}
