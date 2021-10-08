35a69c41c1596dce76b4f2b503fa4f10c222cadc
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-10-08 15:33:44 +0800

    Add FuelFactoryData

---
+++ universe/src/main/kotlin/relativitization/universe/data/component/popsystem/pop/labourer/factory/FuelFactoryData.kt
@@ -0,0 +1,104 @@
+package relativitization.universe.data.component.popsystem.pop.labourer.factory
+
+import kotlinx.serialization.Serializable
+import relativitization.universe.data.component.economy.MutableResourceQualityData
+import relativitization.universe.data.component.economy.ResourceQualityData
+import relativitization.universe.data.component.economy.ResourceType
+import kotlin.math.pow
+
+/**
+ * Data for a factory of labour pop
+ *
+ * @property ownerPlayerId the owner of this factory
+ * @property resourceFactoryInternalData the data describing this factory
+ * @property numBuilding how large is this factory, affect the throughput of the factory
+ * @property isOpened whether this factory is opened
+ * @property lastOutputAmount the output amount in the latest turn
+ * @property lastNumEmployee number of employee in the last turn
+ */
+@Serializable
+data class FuelFactoryData(
+    val ownerPlayerId: Int = -1,
+    val resourceFactoryInternalData: FuelFactoryInternalData = FuelFactoryInternalData(),
+    val numBuilding: Int = 1,
+    val isOpened: Boolean = true,
+    val lastOutputAmount: Double = 0.0,
+    val lastNumEmployee: Double = 0.0,
+)
+
+@Serializable
+data class FuelResourceFactoryData(
+    var ownerPlayerId: Int = -1,
+    var resourceFactoryInternalData: MutableFuelFactoryInternalData = MutableFuelFactoryInternalData(),
+    var numBuilding: Int = 1,
+    var isOpened: Boolean = true,
+    var lastOutputAmount: Double = 0.0,
+    var lastNumEmployee: Double = 0.0,
+) {
+    fun employeeFraction(): Double = lastNumEmployee / (resourceFactoryInternalData.maxNumEmployee * numBuilding)
+}
+
+/**
+ * Internal Data for a factory of labour pop
+ *
+ * @property maxOutputAmount maximum output fuel amount
+ * @property maxNumEmployee max number of employee
+ * @property size the size of this factory
+ */
+@Serializable
+data class FuelFactoryInternalData(
+    val maxOutputAmount: Double = 0.0,
+    val maxNumEmployee: Double = 0.0,
+    val size: Double = 0.0,
+) {
+    fun squareDiff(other: ResourceFactoryInternalData): Double {
+        val outputAmountDiff: Double = (maxOutputAmount - other.maxOutputAmount).pow(2)
+
+        val employeeDiff: Double = (maxNumEmployee - other.maxNumEmployee).pow(2)
+
+        val sizeDiff: Double = (size - other.size).pow(2)
+
+        return (outputAmountDiff + employeeDiff + sizeDiff)
+    }
+
+    fun squareDiff(other: MutableResourceFactoryInternalData): Double {
+        val outputAmountDiff: Double = (maxOutputAmount - other.maxOutputAmount).pow(2)
+
+        val employeeDiff: Double = (maxNumEmployee - other.maxNumEmployee).pow(2)
+
+        val sizeDiff: Double = (size - other.size).pow(2)
+
+        return (outputAmountDiff + employeeDiff + sizeDiff)
+    }
+}
+
+@Serializable
+data class MutableFuelFactoryInternalData(
+    var outputResource: ResourceType = ResourceType.FUEL,
+    var maxOutputResourceQualityData: MutableResourceQualityData = MutableResourceQualityData(),
+    var maxOutputAmount: Double = 0.0,
+    var inputResourceMap: MutableMap<ResourceType, MutableInputResourceData> = mutableMapOf(),
+    var fuelRestMassConsumptionRate: Double = 0.0,
+    var maxNumEmployee: Double = 0.0,
+    var size: Double = 0.0,
+) {
+    fun squareDiff(other: ResourceFactoryInternalData): Double {
+        val outputAmountDiff: Double = (maxOutputAmount - other.maxOutputAmount).pow(2)
+
+        val employeeDiff: Double = (maxNumEmployee - other.maxNumEmployee).pow(2)
+
+        val sizeDiff: Double = (size - other.size).pow(2)
+
+        return (outputAmountDiff + employeeDiff + sizeDiff)
+    }
+
+    fun squareDiff(other: MutableResourceFactoryInternalData): Double {
+        val outputAmountDiff: Double = (maxOutputAmount - other.maxOutputAmount).pow(2)
+
+        val employeeDiff: Double = (maxNumEmployee - other.maxNumEmployee).pow(2)
+
+        val sizeDiff: Double = (size - other.size).pow(2)
+
+        return (outputAmountDiff + employeeDiff + sizeDiff)
+    }
+}
