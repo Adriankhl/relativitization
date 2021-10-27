fe695297d19832a00af50cd562ece2014a3bbfc3
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-10-27 22:51:49 +0800

    Fix remainFraction

--- universe/src/main/kotlin/relativitization/universe/data/commands/LogisticsCommands.kt
+++ universe/src/main/kotlin/relativitization/universe/data/commands/LogisticsCommands.kt
@@ -472,7 +472,7 @@ data class SendResourceToPopCommand(
                 popType = targetPopType,
                 resourceType = resourceType,
                 resourceQualityData = resourceQualityData,
-                resourceAmount = amount,
+                resourceAmount = amount * remainFraction,
             )
         }
     }
