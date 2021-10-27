1bc09a9b3c46ba272fb5c7f73da536c7722e5865
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-10-27 22:22:58 +0800

    Refactor export fraction

--- universe/src/main/kotlin/relativitization/universe/mechanisms/logistics/ExportResource.kt
+++ universe/src/main/kotlin/relativitization/universe/mechanisms/logistics/ExportResource.kt
@@ -8,10 +8,9 @@ import relativitization.universe.data.commands.SendResourceCommand
 import relativitization.universe.data.commands.SendResourceToPopCommand
 import relativitization.universe.data.component.economy.MutableResourceData
 import relativitization.universe.data.component.economy.ResourceQualityData
-import relativitization.universe.data.component.physics.Int3D
-import relativitization.universe.data.component.physics.Int4D
-import relativitization.universe.data.component.popsystem.pop.MutableAllPopData
 import relativitization.universe.data.component.popsystem.pop.service.MutableServicePopData
+import relativitization.universe.data.component.popsystem.pop.service.export.MutablePlayerSingleExportData
+import relativitization.universe.data.component.popsystem.pop.service.export.MutablePopSingleExportData
 import relativitization.universe.data.global.UniverseGlobalData
 import relativitization.universe.mechanisms.Mechanism
 
@@ -26,12 +25,9 @@ object ExportResource : Mechanism() {
         val exportToPlayerCommandList: List<Command> =
             mutablePlayerData.playerInternalData.popSystemData().carrierDataMap.values.map {
                 val mutableServicePopData: MutableServicePopData = it.allPopData.servicePopData
-                val exportFraction: Double = computeExportFraction(mutableServicePopData)
                 computeExportToPlayerCommands(
                     mutableServicePopData = mutableServicePopData,
                     mutablePlayerData = mutablePlayerData,
-                    exportFraction = exportFraction
-
                 )
             }.flatten()
 
@@ -41,8 +37,37 @@ object ExportResource : Mechanism() {
     /**
      * Compute the fraction of the efficiency of export centers
      */
-    fun computeExportFraction(
-        mutableServicePopData: MutableServicePopData
+    fun computePlayerExportFraction(
+        mutableServicePopData: MutableServicePopData,
+        mutableResourceData: MutableResourceData,
+        mutablePlayerSingleExportData: MutablePlayerSingleExportData,
+    ) : Double {
+        val numEmployee: Double = mutableServicePopData.commonPopData.numEmployee()
+
+        val educationLevelMultiplier: Double = (mutableServicePopData.commonPopData.educationLevel * 9.0) + 1.0
+
+        val totalExportAmount: Double = mutableServicePopData.exportData.totalExportAmount()
+
+        val fraction: Double = if (totalExportAmount > 0.0) {
+            numEmployee * educationLevelMultiplier / totalExportAmount
+        } else {
+            1.0
+        }
+
+        return when {
+            fraction > 1.0 -> 1.0
+            fraction < 0.0 -> 0.0
+            else -> fraction
+        }
+    }
+
+    /**
+     * Compute the fraction of the efficiency of export centers
+     */
+    fun computePopExportFraction(
+        mutableServicePopData: MutableServicePopData,
+        mutableResourceData: MutableResourceData,
+        mutablePopSingleExportData: MutablePopSingleExportData
     ) : Double {
         val numEmployee: Double = mutableServicePopData.commonPopData.numEmployee()
 
@@ -69,7 +94,6 @@ object ExportResource : Mechanism() {
     fun computeExportToPlayerCommands(
         mutableServicePopData: MutableServicePopData,
         mutablePlayerData: MutablePlayerData,
-        exportFraction: Double,
     ): List<Command> {
 
         return mutableServicePopData.exportData.playerExportCenterMap.map { (_, exportCenterData) ->
@@ -77,26 +101,31 @@ object ExportResource : Mechanism() {
 
                 // Compute the quality and amount
                 val resourceData: MutableResourceData = mutablePlayerData.playerInternalData.economyData().resourceData
-                val totalAmount: Double = resourceData.getTradeResourceAmount(
-                    resourceType = it.resourceType,
-                    resourceQualityClass = it.resourceQualityClass
+
+                val exportFraction: Double = computePlayerExportFraction(
+                    mutableServicePopData = mutableServicePopData,
+                    mutableResourceData = resourceData,
+                    mutablePlayerSingleExportData = it
                 )
-                val amount: Double = if (totalAmount > (it.amountPerTime * exportFraction)) {
-                    it.amountPerTime * exportFraction
-                } else {
-                    totalAmount
-                }
+
+                val amount: Double = it.amountPerTime * exportFraction
 
                 val resourceQualityData: ResourceQualityData = resourceData.getResourceQuality(
                     resourceType = it.resourceType,
                     resourceQualityClass = it.resourceQualityClass
                 ).toResourceQualityData()
 
+                val price: Double = resourceData.getResourcePrice(
+                    resourceType = it.resourceType,
+                    resourceQualityClass = it.resourceQualityClass
+                )
+
                 // Consume resource
                 resourceData.getResourceAmountData(
                     resourceType = it.resourceType,
                     resourceQualityClass = it.resourceQualityClass
                 ).trade -= amount
+                it.storedFuelRestMass -= price * amount
 
                 SendResourceCommand(
                     toId = it.targetPlayerId,
@@ -112,7 +141,7 @@ object ExportResource : Mechanism() {
     }
 
     /**
-     * Compute export to player command
+     * Compute export to pop command
      */
     fun computeExportToPopCommands(
         mutableServicePopData: MutableServicePopData,
@@ -128,28 +157,31 @@ object ExportResource : Mechanism() {
                         // Compute the quality and amount
                         val resourceData: MutableResourceData =
                             mutablePlayerData.playerInternalData.economyData().resourceData
-                        val totalAmount: Double = resourceData.getTradeResourceAmount(
+
+                        val exportFraction: Double = computePopExportFraction(
+                            mutableServicePopData = mutableServicePopData,
+                            mutableResourceData = resourceData,
+                            mutablePopSingleExportData = it
+                        )
+
+                        val amount: Double = it.amountPerTime * exportFraction
+
+                        val resourceQualityData: ResourceQualityData = resourceData.getResourceQuality(
+                            resourceType = it.resourceType,
+                            resourceQualityClass = it.resourceQualityClass
+                        ).toResourceQualityData()
+
+                        val price: Double = resourceData.getResourcePrice(
                             resourceType = it.resourceType,
                             resourceQualityClass = it.resourceQualityClass
                         )
-                        val amount: Double =
-                            if (totalAmount > (it.amountPerTime * exportFraction)) {
-                                it.amountPerTime * exportFraction
-                            } else {
-                                totalAmount
-                            }
-
-                        val resourceQualityData: ResourceQualityData =
-                            resourceData.getResourceQuality(
-                                resourceType = it.resourceType,
-                                resourceQualityClass = it.resourceQualityClass
-                            ).toResourceQualityData()
 
                         // Consume resource
                         resourceData.getResourceAmountData(
                             resourceType = it.resourceType,
                             resourceQualityClass = it.resourceQualityClass
                         ).trade -= amount
+                        it.storedFuelRestMass -= price * amount
 
                         SendResourceToPopCommand(
                             toId = ownerPlayerId,
