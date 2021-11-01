b92b2aaa712673f5429d76363c3441695162230f
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-01 10:44:12 +0800

    Add availablePortTest()

---
+++ gdx-desktop/src/test/kotlin/relativitization/utils/ServerPortTest.kt
@@ -0,0 +1,31 @@
+package relativitization.utils
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import relativitization.server.UniverseServer
+import relativitization.universe.UniverseServerSettings
+import kotlin.test.Test
+
+internal class ServerPortTest {
+    @Test
+    fun availablePortTest() {
+        assert(ServerPort.findAvailablePort() == 29979)
+
+        val universeServer = UniverseServer(UniverseServerSettings(""))
+
+        assert(ServerPort.findAvailablePort() == 29979)
+
+        runBlocking {
+            launch {
+                universeServer.start()
+            }
+
+            delay(1000)
+
+            assert(ServerPort.findAvailablePort() == 29980)
+
+            universeServer.stop()
+        }
+    }
+}
--- universe/src/main/kotlin/relativitization/universe/UniverseServerSettings.kt
+++ universe/src/main/kotlin/relativitization/universe/UniverseServerSettings.kt
@@ -5,15 +5,15 @@ import kotlinx.serialization.Serializable
 /**
  * Settings of server, should be accessed with mutex lock to prevent setting and getting at the same time
  *
- * @property programDir the location of the program directories, "." for desktop and context.filesDir for android
  * @property adminPassword the password for admin access
+ * @property programDir the location of the program directories, "." for desktop and context.filesDir for android
  * @property clearInactivePerTurn clear the human player id and password and turn them to ai
  * @property waitTimeLimit time limit (in seconds) to wait for human input
  */
 @Serializable
 data class UniverseServerSettings(
-    val programDir: String = ".",
     var adminPassword: String,
+    val programDir: String = ".",
     var clearInactivePerTurn: Boolean = false,
     var waitTimeLimit: Int = 60,
 )
