4c1e2d9ae8f22314cb3fc30ec5e0ec7e05f4e625
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-05 12:17:55 +0800

    Add computeDesireResourceType

---
+++ universe/src/main/kotlin/relativitization/universe/mechanisms/pop/UpdateDesire.kt
@@ -0,0 +1,112 @@
+package relativitization.universe.mechanisms.pop
+
+import relativitization.universe.data.MutablePlayerData
+import relativitization.universe.data.UniverseData3DAtPlayer
+import relativitization.universe.data.UniverseSettings
+import relativitization.universe.data.commands.Command
+import relativitization.universe.data.components.economy.MutableResourceQualityData
+import relativitization.universe.data.components.economy.ResourceQualityData
+import relativitization.universe.data.components.economy.ResourceType
+import relativitization.universe.data.components.popsystem.pop.MutableCommonPopData
+import relativitization.universe.data.components.popsystem.pop.MutableResourceDesireData
+import relativitization.universe.data.components.popsystem.pop.PopType
+import relativitization.universe.data.global.UniverseGlobalData
+import relativitization.universe.mechanisms.Mechanism
+
+object UpdateDesire : Mechanism() {
+    override fun process(
+        mutablePlayerData: MutablePlayerData,
+        universeData3DAtPlayer: UniverseData3DAtPlayer,
+        universeSettings: UniverseSettings,
+        universeGlobalData: UniverseGlobalData
+    ): List<Command> {
+        return listOf()
+    }
+
+    /**
+     * Compute desire resource type of pop
+     *
+     * @param popType the desire of this pop
+     */
+    fun computeDesireResourceType(
+        popType: PopType
+    ): List<ResourceType> {
+        return when (popType) {
+            PopType.LABOURER -> listOf(
+                ResourceType.FOOD,
+                ResourceType.CLOTH,
+                ResourceType.HOUSEHOLD_GOOD,
+                ResourceType.ENTERTAINMENT,
+            )
+            PopType.ENGINEER -> listOf(
+                ResourceType.FOOD,
+                ResourceType.CLOTH,
+                ResourceType.HOUSEHOLD_GOOD,
+                ResourceType.ENTERTAINMENT,
+            )
+            PopType.SCHOLAR -> listOf(
+                ResourceType.FOOD,
+                ResourceType.CLOTH,
+                ResourceType.HOUSEHOLD_GOOD,
+                ResourceType.ENTERTAINMENT,
+            )
+            PopType.EDUCATOR -> listOf(
+                ResourceType.FOOD,
+                ResourceType.CLOTH,
+                ResourceType.HOUSEHOLD_GOOD,
+                ResourceType.ENTERTAINMENT,
+            )
+            PopType.MEDIC -> listOf(
+                ResourceType.FOOD,
+                ResourceType.CLOTH,
+                ResourceType.HOUSEHOLD_GOOD,
+                ResourceType.ENTERTAINMENT,
+            )
+            PopType.SERVICE_WORKER -> listOf(
+                ResourceType.FOOD,
+                ResourceType.CLOTH,
+                ResourceType.HOUSEHOLD_GOOD,
+                ResourceType.ENTERTAINMENT,
+            )
+            PopType.ENTERTAINER -> listOf(
+                ResourceType.FOOD,
+                ResourceType.CLOTH,
+                ResourceType.HOUSEHOLD_GOOD,
+                ResourceType.ENTERTAINMENT,
+            )
+            PopType.SOLDIER -> listOf(
+                ResourceType.FOOD,
+                ResourceType.CLOTH,
+                ResourceType.HOUSEHOLD_GOOD,
+                ResourceType.ENTERTAINMENT,
+            )
+        }
+    }
+
+    fun computeDesireResourceAmount(
+        mutableCommonPopData: MutableCommonPopData
+    ): Double {
+        return mutableCommonPopData.adultPopulation
+    }
+
+    /**
+     * Compute the new desire resource quality
+     */
+    fun computeDesireResourceQuality(
+        mutableCommonPopData: MutableCommonPopData,
+        resourceType: ResourceType,
+        changeFactor: Double,
+    ): MutableResourceQualityData {
+        return if (mutableCommonPopData.desireResourceMap.containsKey(resourceType)) {
+            val newDesire: MutableResourceDesireData = mutableCommonPopData.lastDesireResourceMap.getValue(resourceType)
+
+            MutableResourceQualityData()
+        } else {
+            if (mutableCommonPopData.lastDesireResourceMap.containsKey(resourceType)) {
+                mutableCommonPopData.lastDesireResourceMap.getValue(resourceType).desireQuality * changeFactor
+            } else {
+                MutableResourceQualityData()
+            }
+        }
+    }
+}
