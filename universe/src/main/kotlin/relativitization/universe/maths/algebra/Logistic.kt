c45fa152280592fc4bc266719e4fd7e6872f2bec
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-05 22:53:34 +0800

    Add scaledLogistic

--- universe/src/main/kotlin/relativitization/universe/maths/algebra/Logistic.kt
+++ universe/src/main/kotlin/relativitization/universe/maths/algebra/Logistic.kt
@@ -8,4 +8,17 @@ object Logistic {
     fun logit(p: Double) = ln(p / (1.0 - p))
 
     fun standardLogistic(x: Double) = 1.0 / (1.0 + exp(-x))
+
+    /**
+     * A scaled logistic function where it has a specific slope at x = 0 and the y scale
+     *
+     * @param x x value
+     * @param slope0 slope at x = 0
+     * @param yScale the scaling of y
+     */
+    fun scaledLogistic(
+        x: Double,
+        slope0: Double,
+        yScale: Double,
+    ): Double = yScale * standardLogistic(x * slope0 / (0.25 * yScale))
 }
