15efd684fd70f6223038e209a0d7a0198dd4dac1
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-10 12:30:49 +0800

    Add comment

--- universe/src/main/kotlin/relativitization/universe/mechanisms/pop/BuyResource.kt
+++ universe/src/main/kotlin/relativitization/universe/mechanisms/pop/BuyResource.kt
@@ -7,6 +7,11 @@ import relativitization.universe.data.commands.Command
 import relativitization.universe.data.global.UniverseGlobalData
 import relativitization.universe.mechanisms.Mechanism
 
+/**
+ * Pop buy resource to fulfill their desire
+ * Can buy from the player which the pop belongs to only
+ * Will implement buying from other player in the future
+ */
 object BuyResource : Mechanism() {
     override fun process(
         mutablePlayerData: MutablePlayerData,
