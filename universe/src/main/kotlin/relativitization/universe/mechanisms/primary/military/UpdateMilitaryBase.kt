ac860043e6f2bc03a295ea2315d1bf12ed43e0e2
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-12 19:19:44 +0800

    Add UpdateMilitaryBase

---
+++ universe/src/main/kotlin/relativitization/universe/mechanisms/primary/military/UpdateMilitaryBase.kt
@@ -0,0 +1,19 @@
+package relativitization.universe.mechanisms.primary.military
+
+import relativitization.universe.data.MutablePlayerData
+import relativitization.universe.data.UniverseData3DAtPlayer
+import relativitization.universe.data.UniverseSettings
+import relativitization.universe.data.commands.Command
+import relativitization.universe.data.global.UniverseGlobalData
+import relativitization.universe.mechanisms.Mechanism
+
+object UpdateMilitaryBase : Mechanism() {
+    override fun process(
+        mutablePlayerData: MutablePlayerData,
+        universeData3DAtPlayer: UniverseData3DAtPlayer,
+        universeSettings: UniverseSettings,
+        universeGlobalData: UniverseGlobalData
+    ): List<Command> {
+        return listOf()
+    }
+}
