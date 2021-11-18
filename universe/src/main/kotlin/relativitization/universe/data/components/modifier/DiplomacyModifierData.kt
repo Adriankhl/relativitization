b6aacf1cb877b9bcf5b13ddb74554ab0ee284dcb
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-18 15:31:12 +0800

    Add RelationModifier

--- universe/src/main/kotlin/relativitization/universe/data/components/modifier/DiplomacyModifierData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/components/modifier/DiplomacyModifierData.kt
@@ -7,15 +7,18 @@ import kotlin.math.max
  * Modifier about diplomacy
  *
  * @property peaceTreaty store peace treaty, map from other player id to (universe) time left
+ * @property relationModifierMap store relation modifier, map from other player id to relation modifier
  */
 @Serializable
 data class DiplomacyModifierData(
-    val peaceTreaty: Map<Int, Int> = mapOf()
+    val peaceTreaty: Map<Int, Int> = mapOf(),
+    val relationModifierMap: Map<Int, RelationModifier> = mapOf(),
 )
 
 @Serializable
 data class MutableDiplomacyModifierData(
-    var peaceTreaty: MutableMap<Int, Int> = mutableMapOf()
+    var peaceTreaty: MutableMap<Int, Int> = mutableMapOf(),
+    var relationModifierMap: MutableMap<Int, MutableRelationModifier> = mutableMapOf(),
 ) {
 
 
@@ -64,3 +67,35 @@ data class MutableDiplomacyModifierData(
         }
     }
 }
+
+/**
+ * Modifier the relation
+ */
+@Serializable
+data class RelationModifier(
+    val receiveFuelList: List<SingleRelationModifier> = listOf(),
+)
+
+@Serializable
+data class MutableRelationModifier(
+    var receiveFuelList: MutableList<MutableSingleRelationModifier> = mutableListOf(),
+)
+
+
+/**
+ * Effect from a single improve relation act
+ *
+ * @property change the amount of relation change
+ * @property durationLeft the duration of this effect left, in player time, affected by time dilation
+ */
+@Serializable
+data class SingleRelationModifier(
+    val change: Double = 0.0,
+    val durationLeft: Double = 0.0,
+)
+
+@Serializable
+data class MutableSingleRelationModifier(
+    var change: Double = 0.0,
+    var durationLeft: Double = 0.0,
+)
