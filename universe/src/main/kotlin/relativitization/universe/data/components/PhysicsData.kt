684a94c9753abe04fac608a79ef74d2c02c2e61f
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-19 14:32:42 +0800

    Add otherRestMass

--- universe/src/main/kotlin/relativitization/universe/data/components/PhysicsData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/components/PhysicsData.kt
@@ -9,6 +9,7 @@ import relativitization.universe.data.components.physics.MutableFuelRestMassData
  * Player data related to physics
  *
  * @property coreRestMass the core rest mass of the player, cannot be converted to energy
+ * @property otherRestMass the other rest mass of the player, e.g., fuel stored in pop and factory
  * @property fuelRestMassData various data of fuel rest mass
  * @property targetFuelRestMassData target fuel rest mass storage
  */
@@ -16,20 +17,22 @@ import relativitization.universe.data.components.physics.MutableFuelRestMassData
 @SerialName("PhysicsData")
 data class PhysicsData(
     val coreRestMass: Double = 1.0,
+    val otherRestMass: Double = 0.0,
     val fuelRestMassData: FuelRestMassData = FuelRestMassData(),
     val targetFuelRestMassData: FuelRestMassData = FuelRestMassData(),
 ) : PlayerDataComponent() {
-    fun totalRestMass() = coreRestMass + fuelRestMassData.total()
+    fun totalRestMass() = coreRestMass + otherRestMass + fuelRestMassData.total()
 }
 
 @Serializable
 @SerialName("PhysicsData")
 data class MutablePhysicsData(
     var coreRestMass: Double = 1.0,
+    var otherRestMass: Double = 0.0,
     var fuelRestMassData: MutableFuelRestMassData = MutableFuelRestMassData(),
     var targetFuelRestMassData: MutableFuelRestMassData = MutableFuelRestMassData(),
 ) : MutablePlayerDataComponent() {
-    fun totalRestMass() = coreRestMass + fuelRestMassData.total()
+    fun totalRestMass() = coreRestMass + otherRestMass + fuelRestMassData.total()
 
     fun addFuel(restMass: Double) {
         when {
