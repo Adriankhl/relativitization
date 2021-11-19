bcc1bafbd7c9c3bee4a18229f5f55f27ca41d81c
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-19 16:23:20 +0800

    Remove ai task

--- universe/src/main/kotlin/relativitization/universe/data/components/AIData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/components/AIData.kt
@@ -4,18 +4,12 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import relativitization.universe.ai.default.DefaultAI
 import relativitization.universe.ai.name
-import relativitization.universe.data.components.ai.task.BuyResourceTask
-import relativitization.universe.data.components.ai.task.LogisticsTaskData
-import relativitization.universe.data.components.ai.task.MutableBuyResourceTask
-import relativitization.universe.data.components.ai.task.MutableLogisticsTaskData
 
 @Serializable
 @SerialName("AIData")
 data class AIData(
     val aiName: String = DefaultAI.name(),
     val aiTask: AITask = AITask.DEFAULT,
-    val logisticsTaskData: LogisticsTaskData = LogisticsTaskData(),
-    val buyResourceTask: BuyResourceTask = BuyResourceTask(),
 ) : PlayerDataComponent()
 
 @Serializable
@@ -23,15 +17,11 @@ data class AIData(
 data class MutableAIData(
     var aiName: String = DefaultAI.name(),
     var aiTask: AITask = AITask.DEFAULT,
-    var logisticsTaskData: MutableLogisticsTaskData = MutableLogisticsTaskData(),
-    var buyResourceTask: MutableBuyResourceTask = MutableBuyResourceTask(),
 ) : MutablePlayerDataComponent()
 
 enum class AITask(val value: String) {
     DEFAULT("Default"),
     EMPTY("Empty"),
-    LOGISTICS("Logistics"),
-    BUY_RESOURCE("Buy resource"),
     ;
 
     override fun toString(): String {
