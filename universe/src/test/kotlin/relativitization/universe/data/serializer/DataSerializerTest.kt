5824a5c8f3592f48df6dcb4a5b9a82f9a1e2e5e4
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-20 10:07:32 +0800

    Add DataSerializerTest

--- universe/src/test/kotlin/relativitization/universe/data/serializer/DataSerializerTest.kt
+++ universe/src/test/kotlin/relativitization/universe/data/serializer/DataSerializerTest.kt
@@ -1,19 +1,44 @@
 package relativitization.universe.data.serializer
 
+import kotlinx.serialization.Polymorphic
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.*
 import kotlin.test.Test
 
-abstract class TestAbstract
-
-sealed class TestSeal : TestAbstract()
-
-data class TestData(
-    val testNum: Int = 1
-) : TestSeal()
 
 internal class DataSerializerTest {
+    val module = SerializersModule {
+        polymorphic(GrandProject::class) {
+            subclass(OwnedProject::class)
+        }
+    }
+
+    val format = Json { serializersModule = module }
+
+    @Serializable
+    abstract class GrandProject
+
+    @Serializable
+    sealed class Project : GrandProject() {
+        abstract val name: String
+    }
+
+    @Serializable
+    @SerialName("owned")
+    class OwnedProject(override val name: String, val owner: String) : Project()
+
 
     @Test
     fun sealSubclassTest() {
 
+        val data1: Project = OwnedProject("kotlinx.coroutines", "kotlin")
+        println(format.encodeToString(data1))
+
+
+        val data2: GrandProject = OwnedProject("kotlinx.coroutines", "kotlin")
+        println(format.encodeToString(data2))
     }
 }
