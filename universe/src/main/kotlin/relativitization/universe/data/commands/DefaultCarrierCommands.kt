ff7ab0a1bdfc144cb65567eb8d6bedf1268b195f
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-23 20:45:46 +0800

    Add BuildCarrierCommands

---
+++ universe/src/main/kotlin/relativitization/universe/data/commands/DefaultCarrierCommands.kt
@@ -0,0 +1,35 @@
+package relativitization.universe.data.commands
+
+import kotlinx.serialization.Serializable
+import relativitization.universe.data.MutablePlayerData
+import relativitization.universe.data.UniverseSettings
+import relativitization.universe.data.components.default.physics.Int4D
+import relativitization.universe.utils.I18NString
+
+@Serializable
+data class BuildCarrierCommands(
+    override val toId: Int,
+    override val fromId: Int,
+    override val fromInt4D: Int4D
+) : DefaultCommand() {
+    override val description: I18NString
+        get() = TODO("Not yet implemented")
+
+    override fun canSend(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ): CanSendCheckMessage {
+        TODO("Not yet implemented")
+    }
+
+    override fun canExecute(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun execute(playerData: MutablePlayerData, universeSettings: UniverseSettings) {
+        TODO("Not yet implemented")
+    }
+}
