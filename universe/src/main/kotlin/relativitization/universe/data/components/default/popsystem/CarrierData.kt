0b51af6f55347e169c2777432ddfd9646657ca2e
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-23 13:56:37 +0800

    Add CarrierInternalData

--- universe/src/main/kotlin/relativitization/universe/data/components/default/popsystem/CarrierData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/components/default/popsystem/CarrierData.kt
@@ -4,28 +4,46 @@ import kotlinx.serialization.Serializable
 import relativitization.universe.data.components.default.popsystem.pop.AllPopData
 import relativitization.universe.data.components.default.popsystem.pop.MutableAllPopData
 
-@Serializable
-data class CarrierData(
-    val coreRestMass: Double = 1.0,
-    val maxMovementDeltaFuelRestMass: Double = 0.0,
-    val size: Double = 100.0,
-    val idealPopulation: Double = 100.0,
-    val carrierType: CarrierType = CarrierType.SPACESHIP,
-    val allPopData: AllPopData = AllPopData(),
-)
-
-@Serializable
-data class MutableCarrierData(
-    var coreRestMass: Double = 1.0,
-    var maxMovementDeltaFuelRestMass: Double = 0.0,
-    var size: Double = 100.0,
-    var idealPopulation: Double = 100.0,
-    var carrierType: CarrierType = CarrierType.SPACESHIP,
-    var allPopData: MutableAllPopData = MutableAllPopData(),
-)
-
 enum class CarrierType {
     STELLAR,
     PLANETARY,
     SPACESHIP
 }
+
+@Serializable
+data class CarrierData(
+    val carrierType: CarrierType = CarrierType.SPACESHIP,
+    val carrierInternalData: CarrierInternalData = CarrierInternalData(),
+    val coreRestMass: Double = 1.0,
+    val maxMovementDeltaFuelRestMass: Double = 0.0,
+    val size: Double = 100.0,
+    val idealPopulation: Double = 100.0,
+    val allPopData: AllPopData = AllPopData(),
+)
+
+@Serializable
+data class MutableCarrierData(
+    var carrierType: CarrierType = CarrierType.SPACESHIP,
+    var carrierInternalData: MutableCarrierInternalData = MutableCarrierInternalData(),
+    var coreRestMass: Double = 1.0,
+    var maxMovementDeltaFuelRestMass: Double = 0.0,
+    var size: Double = 100.0,
+    var idealPopulation: Double = 100.0,
+    var allPopData: MutableAllPopData = MutableAllPopData(),
+)
+
+@Serializable
+data class CarrierInternalData(
+    val coreRestMass: Double = 1.0,
+    val maxMovementDeltaFuelRestMass: Double = 0.0,
+    val size: Double = 100.0,
+    val idealPopulation: Double = 100.0,
+)
+
+@Serializable
+data class MutableCarrierInternalData(
+    var coreRestMass: Double = 1.0,
+    var maxMovementDeltaFuelRestMass: Double = 0.0,
+    var size: Double = 100.0,
+    var idealPopulation: Double = 100.0,
+)
