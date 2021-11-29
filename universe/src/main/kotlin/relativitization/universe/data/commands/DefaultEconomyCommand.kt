a075f873f647a5ba05a284ad5cda5a6c6f72bf55
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-29 21:08:19 +0800

    Complete ChangeDefaultExportTariffComman

--- universe/src/main/kotlin/relativitization/universe/data/commands/DefaultEconomyCommand.kt
+++ universe/src/main/kotlin/relativitization/universe/data/commands/DefaultEconomyCommand.kt
@@ -80,3 +80,75 @@ data class ChangeDefaultImportTariffCommand(
     }
 
 }
+
+/**
+ * Change export tariff rate of self
+ *
+ * @property resourceType the tariff of this resource
+ * @property rate the new tariff rate
+ */
+@Serializable
+data class ChangeDefaultExportTariffCommand(
+    override val toId: Int,
+    override val fromId: Int,
+    override val fromInt4D: Int4D,
+    val resourceType: ResourceType,
+    val rate: Double,
+) : DefaultCommand() {
+    override val description: I18NString = I18NString(
+        listOf(
+            RealString("Change default export tariff rate of "),
+            IntString(0),
+            RealString(" to "),
+            IntString(1),
+            RealString(". ")
+        ),
+        listOf(
+            resourceType.toString(),
+            rate.toString(),
+        )
+    )
+
+    override fun canSend(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ): CanSendCheckMessage {
+
+        val isSelf: Boolean = playerData.playerId == toId
+        val isSelfI18NString: I18NString = if (isSelf) {
+            I18NString("")
+        } else {
+            CanSendCheckMessageI18NStringFactory.isNotToSelf(fromId, toId)
+        }
+
+        val isTopLeader: Boolean = playerData.isTopLeader()
+        val isTopLeaderI18NString: I18NString = if (isTopLeader) {
+            I18NString("")
+        } else {
+            CanSendCheckMessageI18NStringFactory.isNotTopLeader(playerData.playerId)
+        }
+
+        return CanSendCheckMessage(
+            isSelf && isTopLeader,
+            I18NString.combine(
+                listOf(
+                    isSelfI18NString,
+                    isTopLeaderI18NString,
+                )
+            )
+        )
+    }
+
+    override fun canExecute(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ): Boolean {
+        return (fromId == playerData.playerId)
+    }
+
+    override fun execute(playerData: MutablePlayerData, universeSettings: UniverseSettings) {
+        playerData.playerInternalData.economyData().taxData.taxRateData
+            .exportTariff.defaultTariffRate.resourceTariffRateMap[resourceType] = rate
+    }
+
+}
