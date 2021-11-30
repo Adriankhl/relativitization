885f24ee3eab27cc7d6bc2b62297bfdffec32d6f
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-30 15:21:52 +0800

    Complete ChangeProductionFuelTargetCommand

--- universe/src/main/kotlin/relativitization/universe/data/commands/DefaultPhysicsCommands.kt
+++ universe/src/main/kotlin/relativitization/universe/data/commands/DefaultPhysicsCommands.kt
@@ -422,3 +422,61 @@ data class ChangeMovementFuelTargetCommand(
         targetFuelData.movement = targetAmount
     }
 }
+
+/**
+ * Change the production fuel target amount
+ *
+ * @property targetAmount the target amount of fuel
+ */
+@Serializable
+data class ChangeProductionFuelTargetCommand(
+    override val toId: Int,
+    override val fromId: Int,
+    override val fromInt4D: Int4D,
+    val targetAmount: Double,
+) : DefaultCommand() {
+    override val description: I18NString = I18NString(
+        listOf(
+            NormalString("Change the target amount of fuel for production to "),
+            IntString(0),
+            NormalString(". "),
+        ),
+        listOf(
+            targetAmount.toString(),
+        ),
+    )
+
+    override fun canSend(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ): CanSendCheckMessage {
+        val isSelf: Boolean = playerData.playerId == toId
+        val isSelfI18NString: I18NString = if (isSelf) {
+            I18NString("")
+        } else {
+            CommandI18NStringFactory.isNotToSelf(fromId, toId)
+        }
+
+        return CanSendCheckMessage(
+            isSelf,
+            listOf(
+                isSelfI18NString,
+            )
+        )
+    }
+
+    override fun canExecute(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ): Boolean {
+
+        return playerData.playerId == toId
+    }
+
+    override fun execute(playerData: MutablePlayerData, universeSettings: UniverseSettings) {
+        val targetFuelData: MutableFuelRestMassData =
+            playerData.playerInternalData.physicsData().targetFuelRestMassData
+
+        targetFuelData.production = targetAmount
+    }
+}
