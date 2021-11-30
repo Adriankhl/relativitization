4b010707d7fafa2f5566be54c3d3d3132a386dd4
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-30 19:04:04 +0800

    Add InstituteInternalData

--- universe/src/main/kotlin/relativitization/universe/data/components/defaults/popsystem/pop/scholar/institute/InstituteData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/components/defaults/popsystem/pop/scholar/institute/InstituteData.kt
@@ -5,18 +5,15 @@ import kotlinx.serialization.Serializable
 /**
  * Data of a research institute
  *
- * @property xCor x coordinate of the center of the institute in the knowledge plane
- * @property yCor y coordinate of the center of the institute in the knowledge plane
- * @property range how far the institute cover the knowledge plane
+ * @property instituteInternalData the internal defining this institute
  * @property strength how good is this institute
  * @property reputation reputation of this institute
- * @property researchEquipmentPerTime amount of research equipment provided per time
- * @property maxNumEmployee maximum number of employee
  * @property lastNumEmployee number of employee in the last round
  * @property size the size of this institute
  */
 @Serializable
 data class InstituteData(
+    val instituteInternalData: InstituteInternalData = InstituteInternalData(),
     val xCor: Double = 0.0,
     val yCor: Double = 0.0,
     val range: Double = 1.0,
@@ -30,6 +27,7 @@ data class InstituteData(
 
 @Serializable
 data class MutableInstituteData(
+    var instituteInternalData: MutableInstituteInternalData = MutableInstituteInternalData(),
     var xCor: Double = 0.0,
     var yCor: Double = 0.0,
     var range: Double = 1.0,
@@ -39,4 +37,31 @@ data class MutableInstituteData(
     var maxNumEmployee: Double = 0.0,
     var lastNumEmployee: Double = 0.0,
     var size: Double = 0.0,
+)
+
+/**
+ * Internal data of a research institute
+ *
+ * @property xCor x coordinate of the center of the institute in the knowledge plane
+ * @property yCor y coordinate of the center of the institute in the knowledge plane
+ * @property range how far the institute cover the knowledge plane
+ * @property researchEquipmentPerTime amount of research equipment provided per time
+ * @property maxNumEmployee maximum number ofBuildInstituteCommand employee
+ */
+@Serializable
+data class InstituteInternalData(
+    val xCor: Double = 0.0,
+    val yCor: Double = 0.0,
+    val range: Double = 1.0,
+    val researchEquipmentPerTime: Double = 0.0,
+    val maxNumEmployee: Double = 0.0,
+)
+
+@Serializable
+data class MutableInstituteInternalData(
+    var xCor: Double = 0.0,
+    var yCor: Double = 0.0,
+    var range: Double = 1.0,
+    var researchEquipmentPerTime: Double = 0.0,
+    var maxNumEmployee: Double = 0.0,
 )
