54883e16a0f735c47750f5418fdfb0a1f65ebf91
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-30 16:22:49 +0800

    Add ChangeFactoryPolicyCommand

---
+++ universe/src/main/kotlin/relativitization/universe/data/commands/DefaultPoliticsCommands.kt
@@ -0,0 +1,39 @@
+package relativitization.universe.data.commands
+
+import kotlinx.serialization.Serializable
+import relativitization.universe.data.MutablePlayerData
+import relativitization.universe.data.UniverseSettings
+import relativitization.universe.data.components.defaults.physics.Int4D
+import relativitization.universe.utils.I18NString
+
+/**
+ * Change the factory policy of this player
+ *
+ */
+@Serializable
+data class ChangeFactoryPolicyCommand(
+    override val toId: Int,
+    override val fromId: Int,
+    override val fromInt4D: Int4D
+) : DefaultCommand() {
+    override val description: I18NString
+        get() = TODO("Not yet implemented")
+
+    override fun canSend(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ): CanSendCheckMessage {
+        TODO("Not yet implemented")
+    }
+
+    override fun canExecute(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun execute(playerData: MutablePlayerData, universeSettings: UniverseSettings) {
+        TODO("Not yet implemented")
+    }
+}
