9dd97734c09298f30975b39c22155223f4039bc9
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-11-30 10:23:35 +0800

    Complete ChangeMiddleIncomeTaxCommand

--- universe/src/main/kotlin/relativitization/universe/data/commands/DefaultEconomyCommands.kt
+++ universe/src/main/kotlin/relativitization/universe/data/commands/DefaultEconomyCommands.kt
@@ -218,3 +218,75 @@ data class ChangeLowIncomeTaxCommand(
             .taxData.taxRateData.incomeTax.lowIncomeTaxRate = rate
     }
 }
+
+/**
+ * Change the middle income tax
+ *
+ * @property rate the new tax rate
+ */
+@Serializable
+data class ChangeMiddleIncomeTaxCommand(
+    override val toId: Int,
+    override val fromId: Int,
+    override val fromInt4D: Int4D,
+    val rate: Double,
+) : DefaultCommand() {
+    override val description: I18NString = I18NString(
+        listOf(
+            RealString("Change middle income tax rate to "),
+            IntString(0),
+            RealString(". ")
+        ),
+        listOf(
+            rate.toString(),
+        )
+    )
+
+    override fun canSend(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ): CanSendCheckMessage {
+
+        val isSelf: Boolean = playerData.playerId == toId
+        val isSelfI18NString: I18NString = if (isSelf) {
+            I18NString("")
+        } else {
+            CommandI18NStringFactory.isNotToSelf(fromId, toId)
+        }
+
+        val isTopLeader: Boolean = playerData.isTopLeader()
+        val isTopLeaderI18NString: I18NString = if (isTopLeader) {
+            I18NString("")
+        } else {
+            CommandI18NStringFactory.isNotTopLeader(playerData.playerId)
+        }
+
+        val isRateValid: Boolean = (rate >= 0.0) && (rate <= 1.0)
+        val isRateValidI18NString: I18NString = if (isRateValid) {
+            I18NString("")
+        } else {
+            I18NString("Rate should be between 0 and 1. ")
+        }
+
+        return CanSendCheckMessage(
+            isSelf && isTopLeader && isRateValid,
+            listOf(
+                isSelfI18NString,
+                isTopLeaderI18NString,
+                isRateValidI18NString
+            )
+        )
+    }
+
+    override fun canExecute(
+        playerData: MutablePlayerData,
+        universeSettings: UniverseSettings
+    ): Boolean {
+        return (playerData.playerId == fromId)
+    }
+
+    override fun execute(playerData: MutablePlayerData, universeSettings: UniverseSettings) {
+        playerData.playerInternalData.economyData()
+            .taxData.taxRateData.incomeTax.middleIncomeTaxRate = rate
+    }
+}
