cb20a2ac08ffc449b1b6ab737d084cab44e7185b
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-12-05 07:49:13 +0800

    Move dead button in show() in DeadScreen

--- gdx-core/src/main/kotlin/relativitization/game/screens/DeadScreen.kt
+++ gdx-core/src/main/kotlin/relativitization/game/screens/DeadScreen.kt
@@ -7,21 +7,21 @@ import relativitization.game.RelativitizationGame
 import relativitization.game.utils.TableScreen
 
 class DeadScreen(val game: RelativitizationGame) : TableScreen(game.assets) {
-    private val background: Image = assets.getImage("background/universe-background")
-    val gdxSettings = game.gdxSettings
 
-    private val deadButton = createTextButton(
-        "You are dead, quit game",
-        gdxSettings.normalFontSize,
-        gdxSettings.soundEffectsVolume
-    ) {
-        runBlocking {
-            game.universeClient.addToOnServerStatusChangeFunctionList { Gdx.app.exit() }
-        }
-    }
+    val gdxSettings = game.gdxSettings
 
     override fun show() {
         super.show()
+
+        val deadButton = createTextButton(
+            "You are dead, quit game",
+            gdxSettings.normalFontSize,
+            gdxSettings.soundEffectsVolume
+        ) {
+            runBlocking {
+                game.universeClient.addToOnServerStatusChangeFunctionList { Gdx.app.exit() }
+            }
+        }
 
         root.add(deadButton)
     }
