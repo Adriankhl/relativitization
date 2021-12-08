e188c022351bd2ee358c1a462bbdc3eded12b56d
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-12-08 13:57:37 +0800

    Fix Test

--- universe/src/test/kotlin/relativitization/universe/maths/number/NotationTest.kt
+++ universe/src/test/kotlin/relativitization/universe/maths/number/NotationTest.kt
@@ -7,29 +7,27 @@ internal class NotationTest {
     @Test
     fun scientificNotationTest() {
         val num1 = 20.0
-        val sf1 = Notation.toScientificNotation(num1)
+        val sf1 = num1.toScientificNotation()
         assert(abs(num1 - sf1.toDouble()) < 0.0001 * sf1.toDouble())
 
         val num2 = 0.0
-        val sf2 = Notation.toScientificNotation(num2)
+        val sf2 = num2.toScientificNotation()
         assert(abs(num2 - sf2.toDouble()) < 0.0001)
 
         val num3 = Double.MAX_VALUE * 0.0001
-        val sf3 = Notation.toScientificNotation(num3)
+        val sf3 = num3.toScientificNotation()
         assert(abs(num3 - sf3.toDouble()) < 0.0001 * sf3.toDouble())
 
         val num4 = Double.MIN_VALUE * 10000.0
-        val sf4 = Notation.toScientificNotation(num4)
+        val sf4 = num4.toScientificNotation()
         assert(abs(num4 - sf4.toDouble()) < 0.0001 * sf4.toDouble())
 
         val num5 = -Double.MAX_VALUE * 0.0001
-        val sf5 = Notation.toScientificNotation(num5)
-        println(sf5)
+        val sf5 = num5.toScientificNotation()
         assert(abs(num5 - sf5.toDouble()) < 0.0001 * abs(sf5.toDouble()))
 
         val num6 = -Double.MIN_VALUE * 10000.0
-        val sf6 = Notation.toScientificNotation(num6)
-        println(sf6)
+        val sf6 = num6.toScientificNotation()
         assert(abs(num6 - sf6.toDouble()) < 0.0001 * abs(sf6.toDouble()))
     }
 }
