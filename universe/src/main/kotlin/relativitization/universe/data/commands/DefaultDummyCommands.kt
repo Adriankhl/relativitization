ab97c4984aed1df5be48c0117141af51883c6a75
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-12-13 08:55:28 +0800

    Simplify ExecuteWarningCommand description

--- universe/src/main/kotlin/relativitization/universe/data/commands/DefaultDummyCommands.kt
+++ universe/src/main/kotlin/relativitization/universe/data/commands/DefaultDummyCommands.kt
@@ -78,12 +78,7 @@ data class ExecuteWarningCommand(
     val reason: I18NString,
 ) : DefaultCommand() {
 
-    override val description: I18NString = I18NString.combine(
-        listOf(
-            I18NString("Execute fail based on the current plan data. "),
-            reason,
-        )
-    )
+    override val description: I18NString = reason
 
     override fun canSend(
         playerData: MutablePlayerData,
