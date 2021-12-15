86e96041384a796350fb0c703a7b8b4956b687b9
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-12-15 09:58:46 +0800

    Add jvm version in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,3 +5,11 @@ plugins {
 repositories {
     gradlePluginPortal()
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "11"
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(11)
+}
