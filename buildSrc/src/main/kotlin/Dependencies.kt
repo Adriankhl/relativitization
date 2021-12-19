0705a9b786465eecdb25dee2aedca5b3ca843121
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-12-19 21:55:26 +0800

    Add comments in buildSrc

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,22 +3,27 @@ object Versions {
     const val appCodeNumber = 1
     const val appVersion = "0.1"
 
+    // Kotlin multiplatform
     const val kotlinVersion = "1.6.10"
     const val kotlinxCoroutineVersion = "1.5.2"
     const val kotlinxSerializationVersion = "1.3.1"
     const val kotlinxDateTimeVersion = "0.3.1"
     const val ktorVersion = "1.6.7"
 
+    // Documentation
     const val dokkaVersion = "1.6.0"
 
+    // JVM
     const val gdxVersion = "1.10.0"
     const val log4jVersion = "2.17.0"
     const val kotlinFakerVersion = "1.9.0"
 
+    // Android
     const val androidGradlePluginVersion = "7.0.4"
     const val androidAppCompatVersion = "1.4.0"
     const val androidLifeCycleKtxVersion = "2.4.0"
     const val androidFragmentKtxVersion = "1.4.0"
 
+    // Unused in relativitization, for ktor
     const val logbackVersion = "1.2.9"
 }
