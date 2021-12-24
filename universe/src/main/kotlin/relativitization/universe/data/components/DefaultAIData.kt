cb293a8e1c86a251f7f29b718456c623567c559e
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-12-25 07:58:48 +0800

    Add recentCommandTimeMap

--- universe/src/main/kotlin/relativitization/universe/data/components/DefaultAIData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/components/DefaultAIData.kt
@@ -5,16 +5,26 @@ import kotlinx.serialization.Serializable
 import relativitization.universe.ai.DefaultAI
 import relativitization.universe.ai.name
 
+/**
+ * Contain data for ai computation, not necessarily to be read by human
+ *
+ * @property aiTask determine the current task of ai
+ * @property recentCommandTimeMap a map from player id to time, store the latest time this player
+ * sent a command to other player with that id, prevent repeatedly sending command due to
+ * observation delay
+ */
 @Serializable
 @SerialName("AIData")
 data class AIData(
     val aiTask: AITask = AITask.DEFAULT,
+    val recentCommandTimeMap: Map<Int, Int> = mapOf(),
 ) : DefaultPlayerDataComponent()
 
 @Serializable
 @SerialName("AIData")
 data class MutableAIData(
     var aiTask: AITask = AITask.DEFAULT,
+    val recentCommandTimeMap: MutableMap<Int, Int> = mutableMapOf(),
 ) : MutableDefaultPlayerDataComponent()
 
 enum class AITask(val value: String) {
