3937820e8342ba455191b011bdd31f142d807af7
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-12-25 20:41:16 +0800

    Add NewResourceFactoryReasoner

--- universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/construction/FactoryReasoner.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/construction/FactoryReasoner.kt
@@ -1,15 +1,61 @@
 package relativitization.universe.ai.defaults.node.self.construction
 
-import relativitization.universe.ai.defaults.utils.AINode
-import relativitization.universe.ai.defaults.utils.PlanState
-import relativitization.universe.ai.defaults.utils.SequenceReasoner
+import relativitization.universe.ai.defaults.utils.*
 import relativitization.universe.data.PlanDataAtPlayer
+import relativitization.universe.data.commands.Command
+import relativitization.universe.data.components.defaults.economy.ResourceType
 
 class FactoryReasoner : SequenceReasoner() {
     override fun getSubNodeList(
         planDataAtPlayer: PlanDataAtPlayer,
         planState: PlanState
-    ): List<AINode> = listOf(
+    ): List<AINode> = ResourceType.values().map {
+        NewResourceFactoryReasoner(it)
+    }
+}
 
+class NewResourceFactoryReasoner(
+    val resourceType: ResourceType
+) : DualUtilityReasoner() {
+    override fun getOptionList(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): List<DualUtilityOption> = listOf(
+        BuildNewResourceFactoryOption(resourceType),
+        NotBuildNewResourceFactoryOption(resourceType),
     )
+}
+
+class BuildNewResourceFactoryOption(
+    val resourceType: ResourceType
+) : DualUtilityOption() {
+    override fun getConsiderationList(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): List<DualUtilityConsideration> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCommandList(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): List<Command> {
+        TODO("Not yet implemented")
+    }
+}
+
+class NotBuildNewResourceFactoryOption(
+    val resourceType: ResourceType
+) : DualUtilityOption() {
+    override fun getConsiderationList(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): List<DualUtilityConsideration> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCommandList(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): List<Command> = listOf()
 }
