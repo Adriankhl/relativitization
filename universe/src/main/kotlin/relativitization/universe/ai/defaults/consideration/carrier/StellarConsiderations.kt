5a1196ebd0b4542bd19e6e68eb9c6d9f3bd4dea2
Adriankhl <dlshcbmuipmam@hotmail.com>
2021-12-28 12:53:44 +0800

    Add HasStellarSystemConsideration

---
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/carrier/StellarConsiderations.kt
@@ -0,0 +1,15 @@
+package relativitization.universe.ai.defaults.consideration.carrier
+
+import relativitization.universe.ai.defaults.utils.DualUtilityConsideration
+import relativitization.universe.ai.defaults.utils.DualUtilityData
+import relativitization.universe.ai.defaults.utils.PlanState
+import relativitization.universe.data.PlanDataAtPlayer
+
+class HasStellarSystemConsideration : DualUtilityConsideration {
+    override fun getDualUtilityData(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): DualUtilityData {
+        TODO("Not yet implemented")
+    }
+}
