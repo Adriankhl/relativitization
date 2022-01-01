6975f0d86d46779cad4b57d8635cf3d5fffd1187
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-01 19:16:04 +0800

    Complete BuildNewFuelFactoryOption

--- universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/construction/FactoryReasoner.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/construction/FactoryReasoner.kt
@@ -5,8 +5,10 @@ import relativitization.universe.ai.defaults.consideration.building.NoResourceFa
 import relativitization.universe.ai.defaults.consideration.building.NoResourceFactoryConsideration
 import relativitization.universe.ai.defaults.utils.*
 import relativitization.universe.data.PlanDataAtPlayer
+import relativitization.universe.data.commands.BuildForeignFuelFactoryCommand
 import relativitization.universe.data.commands.BuildForeignResourceFactoryCommand
 import relativitization.universe.data.components.defaults.economy.ResourceType
+import relativitization.universe.data.components.defaults.popsystem.pop.labourer.factory.MutableFuelFactoryInternalData
 import relativitization.universe.data.components.defaults.popsystem.pop.labourer.factory.MutableResourceFactoryInternalData
 import relativitization.universe.data.serializer.DataSerializer
 import kotlin.math.min
@@ -92,7 +94,52 @@ class BuildNewFuelFactoryOption(
     }
 
     override fun updatePlan(planDataAtPlayer: PlanDataAtPlayer, planState: PlanState) {
+        val idealFactory: MutableFuelFactoryInternalData = planDataAtPlayer
+            .getCurrentMutablePlayerData()
+            .playerInternalData
+            .playerScienceData()
+            .playerScienceApplicationData
+            .idealFuelFactory
 
+        val fuelNeededPerBuilding: Double = planDataAtPlayer
+            .getCurrentMutablePlayerData()
+            .playerInternalData
+            .playerScienceData()
+            .playerScienceApplicationData
+            .newFuelFactoryFuelNeededByConstruction()
+
+        val fuelAvailable: Double = planDataAtPlayer.getCurrentMutablePlayerData()
+            .playerInternalData.physicsData().fuelRestMassData.production
+
+
+        // Don't use all the fuel
+        val maxUsableFuel: Double = fuelAvailable * 0.1
+
+        val numLabourer: Double = planDataAtPlayer.getCurrentMutablePlayerData()
+            .playerInternalData.popSystemData().carrierDataMap.getValue(carrierId).allPopData
+            .labourerPopData.commonPopData.adultPopulation
+
+        // Multiply by 5 to consider pop growth
+        val targetNumLabourer: Double = numLabourer * 5.0
+
+        // Compute the numBuilding by considering the available fuel and number of labourer
+        val fuelFraction: Double = maxUsableFuel / fuelNeededPerBuilding
+        val labourerFraction: Double = targetNumLabourer / idealFactory.maxNumEmployee
+        val numBuilding: Double = min(fuelFraction, labourerFraction)
+
+        planDataAtPlayer.addCommand(
+            BuildForeignFuelFactoryCommand(
+                toId = planDataAtPlayer.getCurrentMutablePlayerData().playerId,
+                fromId = planDataAtPlayer.getCurrentMutablePlayerData().playerId,
+                fromInt4D = planDataAtPlayer.getCurrentMutablePlayerData().int4D.toInt4D(),
+                senderTopLeaderId = planDataAtPlayer.getCurrentMutablePlayerData().topLeaderId(),
+                targetCarrierId = carrierId,
+                ownerId = planDataAtPlayer.getCurrentMutablePlayerData().playerId,
+                fuelFactoryInternalData = DataSerializer.copy(idealFactory),
+                storedFuelRestMass = 0.0,
+                numBuilding = numBuilding,
+            )
+        )
     }
 }
 
