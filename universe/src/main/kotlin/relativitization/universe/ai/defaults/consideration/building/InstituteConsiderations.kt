7109fccf905a6adbba3615899520d0f9d222e8cc
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-04 07:50:26 +0800

    Add NoInstituteAtCarrierConsideration

---
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/building/InstituteConsiderations.kt
@@ -0,0 +1,38 @@
+package relativitization.universe.ai.defaults.consideration.building
+
+import relativitization.universe.ai.defaults.utils.DualUtilityConsideration
+import relativitization.universe.ai.defaults.utils.DualUtilityData
+import relativitization.universe.ai.defaults.utils.DualUtilityDataFactory
+import relativitization.universe.ai.defaults.utils.PlanState
+import relativitization.universe.data.PlanDataAtPlayer
+
+
+/**
+ * Check if there is no institute at a carrier
+ *
+ * @property carrierId the id of the carrier
+ * @property rankIfTrue rank of dual utility if this is true
+ * @property multiplierIfTrue multiplier of dual utility if this is true
+ * @property bonusIfTrue bonus of dual utility if this is true
+ */
+class NoInstituteAtCarrierConsideration(
+    private val carrierId: Int,
+    private val rankIfTrue: Int,
+    private val multiplierIfTrue: Double,
+    private val bonusIfTrue: Double,
+) : DualUtilityConsideration {
+    override fun getDualUtilityData(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): DualUtilityData {
+        val hasInstitute: Boolean = planDataAtPlayer.getCurrentMutablePlayerData()
+            .playerInternalData.popSystemData().carrierDataMap.getValue(carrierId).allPopData
+            .scholarPopData.instituteMap.isNotEmpty()
+
+        return if (hasInstitute) {
+            DualUtilityDataFactory.noImpact()
+        } else {
+            DualUtilityData(rank = rankIfTrue, multiplier = multiplierIfTrue, bonus = bonusIfTrue)
+        }
+    }
+}
