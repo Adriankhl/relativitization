e4c6129e7eba00e0a5627ab5c50e8a91d6760fb0
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-04 08:36:26 +0800

    Add NewInstituteAtCarrierReasoner

--- universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/construction/InstituteReasoner.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/construction/InstituteReasoner.kt
@@ -1,8 +1,6 @@
 package relativitization.universe.ai.defaults.node.self.construction
 
-import relativitization.universe.ai.defaults.utils.AINode
-import relativitization.universe.ai.defaults.utils.PlanState
-import relativitization.universe.ai.defaults.utils.SequenceReasoner
+import relativitization.universe.ai.defaults.utils.*
 import relativitization.universe.data.PlanDataAtPlayer
 
 class InstituteReasoner : SequenceReasoner() {
@@ -10,16 +8,35 @@ class InstituteReasoner : SequenceReasoner() {
         planDataAtPlayer: PlanDataAtPlayer,
         planState: PlanState
     ): List<AINode> {
-        return listOf()
+        return listOf(NewInstituteReasoner())
     }
 }
 
+/**
+ * Consider building new institutes at all carrier
+ */
 class NewInstituteReasoner : SequenceReasoner() {
     override fun getSubNodeList(
         planDataAtPlayer: PlanDataAtPlayer,
         planState: PlanState
-    ): List<AINode> {
-       return listOf()
-    }
+    ): List<AINode> = planDataAtPlayer.getCurrentMutablePlayerData().playerInternalData
+        .popSystemData().carrierDataMap.keys.map {
+            NewInstituteAtCarrierReasoner(it)
+        }
+}
 
+/**
+ * Consider building a new institute at a carrier
+ */
+class NewInstituteAtCarrierReasoner(
+    private val carrierId: Int,
+) : DualUtilityReasoner() {
+    override fun getOptionList(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): List<DualUtilityOption> {
+        return listOf(
+            DoNothingDualUtilityOption(1, 1.0, 1.0)
+        )
+    }
 }
