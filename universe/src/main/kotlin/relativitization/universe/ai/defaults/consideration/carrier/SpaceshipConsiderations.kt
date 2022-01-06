0e4439614030f2b455571c8c0ee3bb173ad09572
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-06 21:57:48 +0800

    Add NoSpaceShipConsideration

--- universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/carrier/SpaceshipConsiderations.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/carrier/SpaceshipConsiderations.kt
@@ -2,17 +2,36 @@ package relativitization.universe.ai.defaults.consideration.carrier
 
 import relativitization.universe.ai.defaults.utils.DualUtilityConsideration
 import relativitization.universe.ai.defaults.utils.DualUtilityData
+import relativitization.universe.ai.defaults.utils.DualUtilityDataFactory
 import relativitization.universe.ai.defaults.utils.PlanState
 import relativitization.universe.data.PlanDataAtPlayer
+import relativitization.universe.data.components.defaults.popsystem.CarrierType
 
 /**
- * Check if the player has enough fuel to build a new spaceship
+ * Check if there is no spaceship
+ *
+ * @property rankIfTrue rank of dual utility if this is true
+ * @property multiplierIfTrue multiplier of dual utility if this is true
+ * @property bonusIfTrue bonus of dual utility if this is true
  */
-class NewSpaceShipFuelConsideration : DualUtilityConsideration {
+class NoSpaceShipConsideration(
+    private val rankIfTrue: Int,
+    private val multiplierIfTrue: Double,
+    private val bonusIfTrue: Double,
+) : DualUtilityConsideration {
     override fun getDualUtilityData(
         planDataAtPlayer: PlanDataAtPlayer,
         planState: PlanState
     ): DualUtilityData {
-        TODO("Not yet implemented")
+        val hasSpaceship: Boolean = planDataAtPlayer.getCurrentMutablePlayerData()
+            .playerInternalData.popSystemData().carrierDataMap.values.any {
+                it.carrierType == CarrierType.SPACESHIP
+            }
+
+        return if (hasSpaceship) {
+            DualUtilityDataFactory.noImpact()
+        } else {
+            DualUtilityData(rank = rankIfTrue, multiplier = multiplierIfTrue, bonus = bonusIfTrue)
+        }
     }
 }
--- universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/carrier/StellarConsiderations.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/carrier/StellarConsiderations.kt
@@ -2,14 +2,36 @@ package relativitization.universe.ai.defaults.consideration.carrier
 
 import relativitization.universe.ai.defaults.utils.DualUtilityConsideration
 import relativitization.universe.ai.defaults.utils.DualUtilityData
+import relativitization.universe.ai.defaults.utils.DualUtilityDataFactory
 import relativitization.universe.ai.defaults.utils.PlanState
 import relativitization.universe.data.PlanDataAtPlayer
+import relativitization.universe.data.components.defaults.popsystem.CarrierType
 
-class HasStellarSystemConsideration : DualUtilityConsideration {
+/**
+ * Check if there is no star
+ *
+ * @property rankIfTrue rank of dual utility if this is true
+ * @property multiplierIfTrue multiplier of dual utility if this is true
+ * @property bonusIfTrue bonus of dual utility if this is true
+ */
+class NoStellarSystemConsideration(
+    private val rankIfTrue: Int,
+    private val multiplierIfTrue: Double,
+    private val bonusIfTrue: Double,
+) : DualUtilityConsideration {
     override fun getDualUtilityData(
         planDataAtPlayer: PlanDataAtPlayer,
         planState: PlanState
     ): DualUtilityData {
-        TODO("Not yet implemented")
+        val hasStar: Boolean = planDataAtPlayer.getCurrentMutablePlayerData()
+            .playerInternalData.popSystemData().carrierDataMap.values.any {
+                it.carrierType == CarrierType.STELLAR
+            }
+
+        return if (hasStar) {
+            DualUtilityDataFactory.noImpact()
+        } else {
+            DualUtilityData(rank = rankIfTrue, multiplier = multiplierIfTrue, bonus = bonusIfTrue)
+        }
     }
 }
