f814003effb4197486ed152cc71d78f0a3e5a525
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-07 12:31:45 +0800

    Add StoreFuelRestMassHistory

---
+++ universe/src/main/kotlin/relativitization/universe/mechanisms/defaults/dilated/history/StoreFuelRestMassHistory.kt
@@ -0,0 +1,19 @@
+package relativitization.universe.mechanisms.defaults.dilated.history
+
+import relativitization.universe.data.MutablePlayerData
+import relativitization.universe.data.UniverseData3DAtPlayer
+import relativitization.universe.data.UniverseSettings
+import relativitization.universe.data.commands.Command
+import relativitization.universe.data.global.UniverseGlobalData
+import relativitization.universe.mechanisms.Mechanism
+
+object StoreFuelRestMassHistory : Mechanism() {
+    override fun process(
+        mutablePlayerData: MutablePlayerData,
+        universeData3DAtPlayer: UniverseData3DAtPlayer,
+        universeSettings: UniverseSettings,
+        universeGlobalData: UniverseGlobalData
+    ): List<Command> {
+        return listOf()
+    }
+}
