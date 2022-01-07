69b72b9db3ef2beb0f3c8f5bea4da153bda02ac1
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-07 21:03:46 +0800

    Improve IncreasingProductionFuelConsideration

--- universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/fuel/ProductionFuelConsiderations.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/fuel/ProductionFuelConsiderations.kt
@@ -13,11 +13,17 @@ import relativitization.universe.data.components.defaults.ai.MutableFuelRestMass
  * @property rankIfTrue rank of dual utility if this is true
  * @property multiplierIfTrue multiplier of dual utility if this is true
  * @property bonusIfTrue bonus of dual utility if this is true
+ * @property rankIfFalse rank of dual utility if this is false
+ * @property multiplierIfFalse multiplier of dual utility if this is false
+ * @property bonusIfFalse bonus of dual utility if this is false
  */
 class IncreasingProductionFuelConsideration(
     private val rankIfTrue: Int,
     private val multiplierIfTrue: Double,
     private val bonusIfTrue: Double,
+    private val rankIfFalse: Int,
+    private val multiplierIfFalse: Double,
+    private val bonusIfFalse: Double,
 ) : DualUtilityConsideration {
     override fun getDualUtilityData(
         planDataAtPlayer: PlanDataAtPlayer,
@@ -34,7 +40,11 @@ class IncreasingProductionFuelConsideration(
         return if (isIncreasing) {
             DualUtilityData(rank = rankIfTrue, multiplier = multiplierIfTrue, bonus = bonusIfTrue)
         } else {
-            DualUtilityDataFactory.noImpact()
+            DualUtilityData(
+                rank = rankIfFalse,
+                multiplier = multiplierIfFalse,
+                bonus = bonusIfFalse
+            )
         }
     }
 }
