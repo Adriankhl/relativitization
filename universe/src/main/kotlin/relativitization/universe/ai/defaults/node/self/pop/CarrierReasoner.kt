615ad306c7e08dded2b2220c7cabe079a5efc7c9
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-09 11:45:33 +0800

    Add considerations for CreateCarrierOption

--- universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/pop/CarrierReasoner.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/pop/CarrierReasoner.kt
@@ -1,5 +1,8 @@
 package relativitization.universe.ai.defaults.node.self.pop
 
+import relativitization.universe.ai.defaults.consideration.building.SufficientLabourerEmploymentConsideration
+import relativitization.universe.ai.defaults.consideration.carrier.SufficientPopulationRatioConsideration
+import relativitization.universe.ai.defaults.consideration.fuel.SufficientProductionFuelConsideration
 import relativitization.universe.ai.defaults.utils.*
 import relativitization.universe.data.PlanDataAtPlayer
 import relativitization.universe.data.components.defaults.economy.ResourceType
@@ -14,12 +17,45 @@ class CarrierReasoner : DualUtilityReasoner() {
     )
 }
 
+/**
+ * Create a carrier for this player
+ */
 class CreateCarrierOption : DualUtilityOption() {
     override fun getConsiderationList(
         planDataAtPlayer: PlanDataAtPlayer,
         planState: PlanState
     ): List<DualUtilityConsideration> {
-        return listOf()
+        // Fuel needed to build a low quality carrier2
+        val fuelNeeded: Double = planDataAtPlayer.getCurrentMutablePlayerData().playerInternalData
+            .playerScienceData().playerScienceApplicationData.newSpaceshipFuelNeededByConstruction(
+                0.1
+            )
+
+        return listOf(
+            SufficientPopulationRatioConsideration(
+                ratio = 0.1,
+                rankIfTrue = 1,
+                multiplierIfTrue = 1.0,
+                bonusIfTrue = 1.0
+            ),
+            SufficientLabourerEmploymentConsideration(
+                rankIfTrue = 1,
+                multiplierIfTrue = 1.0,
+                bonusIfTrue = 0.1,
+                rankIfFalse = 0,
+                multiplierIfFalse = 0.0,
+                bonusIfFalse = 0.0
+            ),
+            SufficientProductionFuelConsideration(
+                requiredProductionFuelRestMass = fuelNeeded,
+                rankIfTrue = 1,
+                multiplierIfTrue = 10.0,
+                bonusIfTrue = 1.0,
+                rankIfFalse = 1,
+                multiplierIfFalse = 0.1,
+                bonusIfFalse = 0.0
+            )
+        )
     }
 
     override fun updatePlan(planDataAtPlayer: PlanDataAtPlayer, planState: PlanState) {
