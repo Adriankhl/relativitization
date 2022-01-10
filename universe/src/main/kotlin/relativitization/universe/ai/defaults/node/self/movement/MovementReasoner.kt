11e24da4776cdb822a7150f68aeb4590b6b518ce
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-10 21:53:28 +0800

    Add MovementReasoner

---
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/movement/MovementReasoner.kt
@@ -0,0 +1,12 @@
+package relativitization.universe.ai.defaults.node.self.movement
+
+import relativitization.universe.ai.defaults.utils.DualUtilityOption
+import relativitization.universe.ai.defaults.utils.DualUtilityReasoner
+import relativitization.universe.ai.defaults.utils.PlanState
+import relativitization.universe.data.PlanDataAtPlayer
+
+class MovementReasoner : DualUtilityReasoner() {
+    override fun getOptionList(planDataAtPlayer: PlanDataAtPlayer, planState: PlanState): List<DualUtilityOption> {
+        TODO("Not yet implemented")
+    }
+}
