f832a2d0214e3c0b41c04e511a3ce12fed543aa7
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-12 19:22:49 +0800

    Add surfaceTest()

---
+++ universe/src/test/kotlin/relativitization/universe/data/components/defaults/physics/Int3DTest.kt
@@ -0,0 +1,26 @@
+package relativitization.universe.data.components.defaults.physics
+
+import relativitization.universe.maths.grid.Grids
+import kotlin.test.Test
+
+internal class Int3DTest {
+    @Test
+    fun surfaceTest() {
+        val i1 = Int3D(1, 1, 1)
+        val l1: List<Int3D> = Grids.create3DGrid(3, 3, 3) { x, y, z ->
+            Int3D(x, y, z)
+        }.flatten().flatten() - i1
+
+        val il1 = i1.getInt3DSurfaceList(
+            halfEdgeLength = 1,
+            minX = 0,
+            maxX = 2,
+            minY = 0,
+            maxY = 2,
+            minZ = 0,
+            maxZ = 2
+        )
+
+        assert(l1.all { il1.contains(it) } && il1.all { l1.contains(it) })
+    }
+}
