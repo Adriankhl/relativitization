4e3c13cad901cde4ffb5de035eb39afbc8bcbc29
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-12 12:50:57 +0800

    Add MoveToLowerDensityCubeOption

--- universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/movement/MovementReasoner.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/node/self/movement/MovementReasoner.kt
@@ -1,9 +1,6 @@
 package relativitization.universe.ai.defaults.node.self.movement
 
-import relativitization.universe.ai.defaults.utils.DoNothingDualUtilityOption
-import relativitization.universe.ai.defaults.utils.DualUtilityOption
-import relativitization.universe.ai.defaults.utils.DualUtilityReasoner
-import relativitization.universe.ai.defaults.utils.PlanState
+import relativitization.universe.ai.defaults.utils.*
 import relativitization.universe.data.PlanDataAtPlayer
 
 class MovementReasoner : DualUtilityReasoner() {
@@ -11,6 +8,20 @@ class MovementReasoner : DualUtilityReasoner() {
         planDataAtPlayer: PlanDataAtPlayer,
         planState: PlanState
     ): List<DualUtilityOption> = listOf(
+        MoveToLowerDensityCubeOption(),
         DoNothingDualUtilityOption(rank = 1, multiplier = 1.0, bonus = 1.0)
     )
+}
+
+class MoveToLowerDensityCubeOption : DualUtilityOption() {
+    override fun getConsiderationList(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): List<DualUtilityConsideration> {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePlan(planDataAtPlayer: PlanDataAtPlayer, planState: PlanState) {
+        TODO("Not yet implemented")
+    }
 }
