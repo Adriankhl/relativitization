db3be11fc556679f4fa410bd32f2e1c57cc77f4e
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-19 12:28:58 +0800

    Add LargerMilitaryStrengthConsideration

---
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/military/MilitaryStrengthConsiderations.kt
@@ -0,0 +1,34 @@
+package relativitization.universe.ai.defaults.consideration.military
+
+import relativitization.universe.ai.defaults.utils.DualUtilityConsideration
+import relativitization.universe.ai.defaults.utils.DualUtilityData
+import relativitization.universe.ai.defaults.utils.PlanState
+import relativitization.universe.data.PlanDataAtPlayer
+
+/**
+ * Compare the military strength of this player to target player
+ *
+ * @property targetPlayerId compare with the player with this id
+ * @property rankIfTrue rank of dual utility if this is true
+ * @property multiplierIfTrue multiplier of dual utility if this is true
+ * @property bonusIfTrue bonus of dual utility if this is true
+ * @property rankIfFalse rank of dual utility if this is false
+ * @property multiplierIfFalse multiplier of dual utility if this is false
+ * @property bonusIfFalse bonus of dual utility if this is false
+ */
+class LargerMilitaryStrengthConsideration(
+    private val targetPlayerId: Int,
+    private val rankIfTrue: Int,
+    private val multiplierIfTrue: Double,
+    private val bonusIfTrue: Double,
+    private val rankIfFalse: Int,
+    private val multiplierIfFalse: Double,
+    private val bonusIfFalse: Double,
+) : DualUtilityConsideration {
+    override fun getDualUtilityData(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): DualUtilityData {
+        TODO("Not yet implemented")
+    }
+}
