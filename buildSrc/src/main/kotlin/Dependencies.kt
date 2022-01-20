7efcdb278e6fa2eca505560b8f2e598651d23f1e
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-20 14:07:53 +0800

    Update dependency

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val kotlinVersion = "1.6.10"
     const val kotlinxCoroutineVersion = "1.6.0"
     const val kotlinxSerializationVersion = "1.3.2"
-    const val kotlinxDateTimeVersion = "0.3.1"
+    const val kotlinxDateTimeVersion = "0.3.2"
     const val ktorVersion = "1.6.7"
 
     // Documentation
