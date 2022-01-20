2f290dbeebef956b2c6a70e5078c9e159083776a
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-20 19:18:38 +0800

    Add ProposePeaceToPlayerOption

--- universe/src/main/kotlin/relativitization/universe/ai/defaults/node/other/diplomacy/ProposePeaceReasoner.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/node/other/diplomacy/ProposePeaceReasoner.kt
@@ -2,6 +2,7 @@ package relativitization.universe.ai.defaults.node.other.diplomacy
 
 import relativitization.universe.ai.defaults.utils.*
 import relativitization.universe.data.PlanDataAtPlayer
+import relativitization.universe.data.commands.ProposePeaceCommand
 
 class ProposePeaceReasoner : SequenceReasoner() {
     override fun getSubNodeList(
@@ -20,6 +21,29 @@ class ProposePeaceToPlayerReasoner(
         planDataAtPlayer: PlanDataAtPlayer,
         planState: PlanState
     ): List<DualUtilityOption> = listOf(
+        ProposePeaceToPlayerOption(targetPlayerId),
         DoNothingDualUtilityOption(rank = 1, multiplier = 1.0, bonus = 1.0)
     )
+}
+
+class ProposePeaceToPlayerOption(
+    private val targetPlayerId: Int
+) : DualUtilityOption() {
+    override fun getConsiderationList(
+        planDataAtPlayer: PlanDataAtPlayer,
+        planState: PlanState
+    ): List<DualUtilityConsideration> {
+        return listOf()
+    }
+
+    override fun updatePlan(planDataAtPlayer: PlanDataAtPlayer, planState: PlanState) {
+        planDataAtPlayer.addCommand(
+            ProposePeaceCommand(
+                toId = planDataAtPlayer.getCurrentMutablePlayerData().playerId,
+                fromId = planDataAtPlayer.getCurrentMutablePlayerData().playerId,
+                fromInt4D = planDataAtPlayer.getCurrentMutablePlayerData().int4D.toInt4D(),
+                targetPlayerId = targetPlayerId
+            )
+        )
+    }
 }
