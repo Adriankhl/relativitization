835aadc2c30febf95c14e626eaf38d5dd038be3e
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-20 16:33:58 +0800

    Add IsTopLeaderConsideration

--- universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/hierarchy/HierarchyConsiderations.kt
+++ universe/src/main/kotlin/relativitization/universe/ai/defaults/consideration/hierarchy/HierarchyConsiderations.kt
@@ -70,3 +70,38 @@ class HierarchyRelationConsideration(
         }
     }
 }
+
+/**
+ * Check whether this player is a top leader
+ *
+ * @property rankIfTrue rank of dual utility if this is true
+ * @property multiplierIfTrue multiplier of dual utility if this is true
+ * @property bonusIfTrue bonus of dual utility if this is true
+ * @property rankIfFalse rank of dual utility if this is false
+ * @property multiplierIfFalse multiplier of dual utility if this is false
+ * @property bonusIfFalse bonus of dual utility if this is false
+ */
+class IsTopLeaderConsideration(
+    private val rankIfTrue: Int,
+    private val multiplierIfTrue: Double,
+    private val bonusIfTrue: Double,
+    private val rankIfFalse: Int,
+    private val multiplierIfFalse: Double,
+    private val bonusIfFalse: Double,
+) : DualUtilityConsideration() {
+    override fun getDualUtilityData(planDataAtPlayer: PlanDataAtPlayer, planState: PlanState): DualUtilityData {
+        return if (planDataAtPlayer.getCurrentMutablePlayerData().isTopLeader()) {
+            DualUtilityData(
+                rank = rankIfTrue,
+                multiplier = multiplierIfTrue,
+                bonus = bonusIfTrue
+            )
+        } else {
+            DualUtilityData(
+                rank = rankIfFalse,
+                multiplier = multiplierIfFalse,
+                bonus = bonusIfFalse
+            )
+        }
+    }
+}
