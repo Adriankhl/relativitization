8d26ed856eb5a0d4cbf90ccdfcf9c4a65e28c8d3
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-26 15:28:16 +0800

    Add total attack and shield in summary

--- gdx-core/src/main/kotlin/relativitization/game/utils/Summary.kt
+++ gdx-core/src/main/kotlin/relativitization/game/utils/Summary.kt
@@ -1,18 +1,37 @@
 package relativitization.game.utils
 
+import relativitization.universe.data.PlayerData
 import relativitization.universe.data.components.defaults.popsystem.CarrierData
 
 object Summary {
-    fun computeCarrierListSummary(
-        carrierList: List<CarrierData>
+    fun compute(
+        thisPlayer: PlayerData,
+        otherPlayerList: List<PlayerData>,
     ): CarrierListSummary {
 
+        val carrierList: List<CarrierData> = thisPlayer.playerInternalData.popSystemData().carrierDataMap.values +
+                otherPlayerList.flatMap { it.playerInternalData.popSystemData().carrierDataMap.values }
+
+        val totalAttack: Double = carrierList.sumOf {
+            it.allPopData.soldierPopData.militaryBaseData.attack
+        }
+
+        val totalShield: Double = carrierList.sumOf {
+            it.allPopData.soldierPopData.militaryBaseData.shield
+        }
+
         return CarrierListSummary(
+            playerId = thisPlayer.playerId,
             numCarrier = carrierList.size,
+            totalAttack = totalAttack,
+            totalShield = totalShield,
         )
     }
 }
 
 data class CarrierListSummary(
+    val playerId: Int,
     val numCarrier: Int,
+    val totalAttack: Double,
+    val totalShield: Double,
 )
