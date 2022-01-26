1e6e09a3c801026dd36ea079d0a3cd1462232efe
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-01-26 16:43:14 +0800

    Add totalFuelSupply in summary

--- gdx-core/src/main/kotlin/relativitization/game/utils/Summary.kt
+++ gdx-core/src/main/kotlin/relativitization/game/utils/Summary.kt
@@ -12,6 +12,7 @@ object Summary {
         thisPlayer: PlayerData,
         otherPlayerList: List<PlayerData> = listOf(),
     ): PlayerSummary {
+        val allPlayerId: List<Int> = listOf(thisPlayer.playerId) + otherPlayerList.map { it.playerId }
 
         val carrierList: List<CarrierData> = thisPlayer.playerInternalData.popSystemData().carrierDataMap.values +
                 otherPlayerList.flatMap { it.playerInternalData.popSystemData().carrierDataMap.values }
@@ -24,17 +25,41 @@ object Summary {
             it.allPopData.soldierPopData.militaryBaseData.shield
         }
 
-        val totalFuelConsumption: Double = carrierList.sumOf { carrierData ->
+        val totalFuelDemand: Double = carrierList.sumOf { carrierData ->
             val totalSalary: Double = PopType.values().sumOf { popType ->
                 val commonPopData: CommonPopData = carrierData.allPopData.getCommonPopData(popType)
                 commonPopData.salaryPerEmployee * commonPopData.adultPopulation * (1.0 - commonPopData.unemploymentRate)
             }
             val totalResourceFactoryConsumption: Double = carrierData.allPopData.labourerPopData.resourceFactoryMap
                 .values.sumOf { resourceFactory ->
-                    resourceFactory.resourceFactoryInternalData.fuelRestMassConsumptionRate *
-                            resourceFactory.numBuilding * resourceFactory.employeeFraction()
+                    // only consider self resource factory
+                    if (allPlayerId.contains(resourceFactory.ownerPlayerId)) {
+                        resourceFactory.resourceFactoryInternalData.fuelRestMassConsumptionRate *
+                                resourceFactory.numBuilding * resourceFactory.employeeFraction()
+                    } else {
+                        0.0
+                    }
                 }
             totalSalary + totalResourceFactoryConsumption
+        }
+
+        val totalFuelSupply: Double = carrierList.sumOf { carrierData ->
+            val baseProduction: Double = BaseStellarFuelProduction.baseFuelProduction(
+                carrierData.carrierType,
+                carrierData.carrierInternalData.coreRestMass
+            )
+
+            val fuelFactoryProduction: Double = carrierData.allPopData.labourerPopData.fuelFactoryMap
+                .values.sumOf { fuelFactory ->
+                    // Only calculate fuel factory belongs to this group
+                    if (allPlayerId.contains(fuelFactory.ownerPlayerId)) {
+                        fuelFactory.lastOutputAmount
+                    } else {
+                        0.0
+                    }
+                }
+
+            baseProduction + fuelFactoryProduction
         }
 
 
@@ -47,7 +72,8 @@ object Summary {
             numCarrier = carrierList.size,
             totalAttack = totalAttack,
             totalShield = totalShield,
-            totalFuelConsumption = totalFuelConsumption,
+            totalFuelDemand = totalFuelDemand,
+            totalFuelSupply = totalFuelSupply,
         )
     }
 }
@@ -57,5 +83,6 @@ data class PlayerSummary(
     val numCarrier: Int,
     val totalAttack: Double,
     val totalShield: Double,
-    val totalFuelConsumption: Double,
+    val totalFuelDemand: Double,
+    val totalFuelSupply: Double,
 )
