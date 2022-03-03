5b1d0811a1eb057166113da1b2eff2acbecbc31a
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-03-03 14:24:31 +0800

    Update dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -16,11 +16,11 @@ object Versions {
 
     // JVM
     const val gdxVersion = "1.10.0"
-    const val log4jVersion = "2.17.1"
+    const val log4jVersion = "2.17.2"
     const val dataframeVersion = "0.8.0-rc-7"
 
     // Android
-    const val androidGradlePluginVersion = "7.0.4"
+    const val androidGradlePluginVersion = "7.1.2"
     const val androidAppCompatVersion = "1.4.1"
     const val androidLifeCycleKtxVersion = "2.4.1"
     const val androidFragmentKtxVersion = "1.4.1"
