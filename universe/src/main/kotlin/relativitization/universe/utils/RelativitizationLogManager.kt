73a002b96b12222dac551b4ae93c54ee15cc12b2
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-03-04 22:04:59 +0800

    Add RelativitizationLogger

--- universe/src/main/kotlin/relativitization/universe/utils/RelativitizationLogManager.kt
+++ universe/src/main/kotlin/relativitization/universe/utils/RelativitizationLogManager.kt
@@ -5,13 +5,40 @@ import org.apache.logging.log4j.Logger
 import org.apache.logging.log4j.util.StackLocatorUtil
 
 object RelativitizationLogManager {
+    // Android does not support StackLocatorUtil.getCallerClass, use default logger instead
     var useDefaultLoggerName: Boolean = false
 
-    fun getLogger(): Logger = if (useDefaultLoggerName) {
-        LogManager.getLogger("Default")
+    fun getLogger(): RelativitizationLogger = if (useDefaultLoggerName) {
+        RelativitizationLogger("Default")
     } else {
-        LogManager.getLogger(StackLocatorUtil.getCallerClass(2))
+        RelativitizationLogger()
     }
 
-    fun getLogger(name: String): Logger = LogManager.getLogger(name)
+    fun getLogger(name: String): RelativitizationLogger = RelativitizationLogger(name)
+}
+
+class RelativitizationLogger(
+    name: String = ""
+) {
+    val logger: Logger = if (name.isBlank()) {
+        LogManager.getLogger(StackLocatorUtil.getCallerClass(4))
+    } else {
+        LogManager.getLogger(name)
+    }
+
+    fun error(message: String) {
+        logger.error(message)
+    }
+
+    fun warn(message: String) {
+        logger.warn(message)
+    }
+
+    fun debug(message: String) {
+        logger.debug(message)
+    }
+
+    fun info(message: String) {
+        logger.info(message)
+    }
 }
