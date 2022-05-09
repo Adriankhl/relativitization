777a67031f06062780ce294cec5a0c355123c888
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-05-09 08:30:19 +0800

    Add randomSeedTest() for UniverseTest

--- universe/src/test/kotlin/relativitization/universe/UniverseTest.kt
+++ universe/src/test/kotlin/relativitization/universe/UniverseTest.kt
@@ -2,10 +2,17 @@ package relativitization.universe
 
 import relativitization.universe.data.MutableUniverseSettings
 import relativitization.universe.data.UniverseData
+import relativitization.universe.data.commands.DefaultCommandAvailability
+import relativitization.universe.data.commands.name
 import relativitization.universe.generate.GenerateSettings
 import relativitization.universe.generate.GenerateUniverseMethodCollection
 import relativitization.universe.generate.name
+import relativitization.universe.generate.random.RandomOneStarPerPlayerGenerate
 import relativitization.universe.generate.testing.TestingFixedMinimal
+import relativitization.universe.global.DefaultGlobalMechanismList
+import relativitization.universe.global.name
+import relativitization.universe.mechanisms.DefaultMechanismLists
+import relativitization.universe.mechanisms.name
 import kotlin.test.Test
 
 internal class UniverseTest {
@@ -24,5 +31,45 @@ internal class UniverseTest {
         universe.saveAll()
         val universeLoad = Universe.loadUniverseLatest("save-load-test", ".")
         println(universeLoad)
+    }
+
+    @Test
+    fun randomSeedTest() {
+        val generateSetting = GenerateSettings(
+            generateMethod = RandomOneStarPerPlayerGenerate.name(),
+            numPlayer = 100,
+            numHumanPlayer = 8,
+            otherDoubleMap = mutableMapOf("initialPopulation" to 1E6),
+            universeSettings = MutableUniverseSettings(
+                universeName = "Random seed test",
+                commandCollectionName = DefaultCommandAvailability.name(),
+                mechanismCollectionName = DefaultMechanismLists.name(),
+                globalMechanismCollectionName = DefaultGlobalMechanismList.name(),
+                speedOfLight = 1.0,
+                xDim = 10,
+                yDim = 10,
+                zDim = 3,
+                groupEdgeLength = 0.01,
+                randomSeed = 100L,
+            )
+        )
+
+        val universeData1: UniverseData = GenerateUniverseMethodCollection.generate(generateSetting)
+        val universeData2: UniverseData = GenerateUniverseMethodCollection.generate(generateSetting)
+
+        assert(universeData1 == universeData2)
+
+        val universe1 = Universe(universeData1)
+        val universe2 = Universe(universeData2)
+
+        for (turn in 1..20) {
+            universe1.pureAIStep()
+        }
+
+        for (turn in 1..20) {
+            universe2.pureAIStep()
+        }
+
+        assert(universe1.getUniverse3DViewAtPlayer(1) == universe2.getUniverse3DViewAtPlayer(1))
     }
 }
