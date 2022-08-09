3d7d70caab5a1802b26f1cbd0779fd7e1ec2cd03
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-08-09 19:15:40 +0200

    feat(GUi): add quick start guide

--- gdx-core/src/main/kotlin/relativitization/game/screens/HelpScreen.kt
+++ gdx-core/src/main/kotlin/relativitization/game/screens/HelpScreen.kt
@@ -1,6 +1,8 @@
 package relativitization.game.screens
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
+import com.badlogic.gdx.scenes.scene2d.ui.Table
 import relativitization.game.RelativitizationGame
 import relativitization.game.utils.TableScreen
 
@@ -13,26 +15,15 @@ class HelpScreen(
     override fun show() {
         super.show()
 
-        root.add(
-            createLabel(
-                "Guide (Click to open + copy): ",
-                gdxSettings.normalFontSize
-            )
-        ).colspan(2)
+        root.add(createDocScrollPane())
 
-        root.row().space(10f)
+        root.row().space(50f)
 
-        val docTextButton = createTextButton(
-            "https://github.com/Adriankhl/relativitization-game-doc",
-            gdxSettings.normalFontSize,
-            gdxSettings.soundEffectsVolume,
-        ) {
-            Gdx.net.openURI("https://github.com/Adriankhl/relativitization-game-doc")
-            Gdx.app.clipboard.contents = "https://github.com/Adriankhl/relativitization-game-doc"
-        }
-        root.add(docTextButton).colspan(2)
+        root.add(createButtonTable())
+    }
 
-        root.row().space(30f)
+    private fun createButtonTable(): Table {
+        val table = Table()
 
         val returnButton = createTextButton(
             "Return",
@@ -47,7 +38,7 @@ class HelpScreen(
                 dispose()
             }
         }
-        root.add(returnButton)
+        table.add(returnButton).space(20f)
 
         val aboutButton = createTextButton(
             "About",
@@ -56,6 +47,80 @@ class HelpScreen(
         ) {
             game.screen = AboutScreen(game, inGame)
         }
-        root.add(aboutButton)
+        table.add(aboutButton).space(20f)
+
+        return table
+    }
+
+    private fun createDocScrollPane(): ScrollPane {
+        val table = Table()
+
+        table.add(createFullGuideTable())
+
+        table.row().space(50f)
+
+        table.add(createQuickGuideTable())
+
+        val scrollPane: ScrollPane = createScrollPane(table)
+
+        scrollPane.fadeScrollBars = false
+        scrollPane.setClamp(true)
+        scrollPane.setOverscroll(false, false)
+
+        return scrollPane
+    }
+
+    private fun createFullGuideTable(): Table {
+        val nestedTable = Table()
+
+        nestedTable.add(
+            createLabel(
+                "Full Guide (Click to open + copy): ",
+                gdxSettings.bigFontSize
+            )
+        )
+
+        nestedTable.row().space(10f)
+
+        val docTextButton = createTextButton(
+            "https://github.com/Adriankhl/relativitization-game-doc",
+            gdxSettings.normalFontSize,
+            gdxSettings.soundEffectsVolume,
+        ) {
+            Gdx.net.openURI("https://github.com/Adriankhl/relativitization-game-doc")
+            Gdx.app.clipboard.contents = "https://github.com/Adriankhl/relativitization-game-doc"
+        }
+        nestedTable.add(docTextButton)
+
+        return nestedTable
+    }
+
+    private fun createQuickGuideTable(): Table {
+        val nestedTable = Table()
+
+        nestedTable.add(
+            createLabel(
+                "Quick Guide",
+                gdxSettings.bigFontSize,
+            )
+        )
+
+        nestedTable.row().space(30f)
+
+        val aiGuideText: String = """
+            1. Click "AI" in the control bar.
+            2. On the right side, click "Compute".
+            3. Then, click "Use all".
+            This allows the AI to make all the decisions for you in this turn.
+        """.trimIndent()
+
+        nestedTable.add(
+            createLabel(
+                aiGuideText,
+                gdxSettings.normalFontSize
+            )
+        )
+
+        return nestedTable
     }
 }
