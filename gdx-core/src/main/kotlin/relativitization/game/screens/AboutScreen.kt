dfb0bd08aec05abe822b5d9d575f5445adacda99
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-08-19 12:05:38 +0200

    feat(GUI): remove logback license from about screen

--- gdx-core/src/main/kotlin/relativitization/game/screens/AboutScreen.kt
+++ gdx-core/src/main/kotlin/relativitization/game/screens/AboutScreen.kt
@@ -84,15 +84,6 @@ class AboutScreen(
 
         table.add(
             createLabel(
-                logbackLicenseText(),
-                gdxSettings.smallFontSize
-            )
-        )
-
-        table.row().space(30f)
-
-        table.add(
-            createLabel(
                 licensedAssetText(),
                 gdxSettings.smallFontSize
             )
@@ -1114,15 +1105,6 @@ class AboutScreen(
             LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
             OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
             SOFTWARE.          
-        """
-    }
-
-    private fun logbackLicenseText(): String {
-        return """
-            Relativitization depends on logback, licensed under GNU Lesser General Public License version 2.1 
-            
-            Logback: the reliable, generic, fast and flexible logging framework.
-            Copyright (C) 1999-2015, QOS.ch. All rights reserved.
         """
     }
 
