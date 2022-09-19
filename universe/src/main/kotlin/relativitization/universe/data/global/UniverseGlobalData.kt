34fc0d39abbbd44274450b82d7d6193567b22562
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-09-19 11:59:37 +0200

    style: clean unused import

--- universe/src/main/kotlin/relativitization/universe/data/global/UniverseGlobalData.kt
+++ universe/src/main/kotlin/relativitization/universe/data/global/UniverseGlobalData.kt
@@ -2,7 +2,6 @@ package relativitization.universe.data.global
 
 import kotlinx.serialization.Serializable
 import relativitization.universe.data.global.components.*
-import kotlin.reflect.full.createInstance
 
 @Serializable
 data class UniverseGlobalData(
