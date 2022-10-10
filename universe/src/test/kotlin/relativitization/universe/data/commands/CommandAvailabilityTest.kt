beeb6a25f42716466e8b417b7e7672f36fe0e0a2
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-10-10 17:38:06 +0200

    test: add CommandAvailabilityTest

---
+++ universe/src/test/kotlin/relativitization/universe/data/commands/CommandAvailabilityTest.kt
@@ -0,0 +1,30 @@
+package relativitization.universe.data.commands
+
+import relativitization.universe.utils.I18NString
+import kotlin.reflect.KClass
+import kotlin.test.Test
+
+internal class CommandAvailabilityTest {
+    @Test
+    fun kClassTest() {
+        val k1 = DummyCommand::class
+        assert(k1.isInstance(DummyCommand()))
+        assert(!k1.isInstance(CannotSendCommand(reason = I18NString(""))))
+
+        val l1: List<KClass<out Command>> = listOf(
+            DummyCommand::class
+        )
+
+        assert(
+            l1.any {
+                it.isInstance(DummyCommand())
+            }
+        )
+
+        assert(
+            !l1.any {
+                it.isInstance(CannotSendCommand(reason = I18NString("")))
+            }
+        )
+    }
+}
