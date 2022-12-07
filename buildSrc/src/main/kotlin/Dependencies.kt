cc6787352188912503bf6d093cd1330539ec8765
Adriankhl <dlshcbmuipmam@hotmail.com>
2022-12-08 00:55:00 +0100

    build: bump androidFragmentKtxVersion to 1.5.5

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -40,5 +40,5 @@ object Versions {
     const val androidGradlePluginVersion = "7.3.1"
     const val androidAppCompatVersion = "1.5.1"
     const val androidLifeCycleKtxVersion = "2.5.1"
-    const val androidFragmentKtxVersion = "1.5.4"
+    const val androidFragmentKtxVersion = "1.5.5"
 }
