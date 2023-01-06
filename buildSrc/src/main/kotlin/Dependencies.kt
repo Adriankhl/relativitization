800eaf3fa62a11928df469fbcf0ee463342e1ee3
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-01-06 11:27:29 +0100

    build: bump dataframeVersion to 0.9.1

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -34,7 +34,7 @@ object Versions {
     // JVM
     const val gdxVersion = "1.11.0"
     const val log4jVersion = "2.19.0"
-    const val dataframeVersion = "0.8.1"
+    const val dataframeVersion = "0.9.1"
 
     // Android
     const val androidGradlePluginVersion = "7.3.1"
