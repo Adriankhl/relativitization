f0aa0c52b74f0d5709879ddf34eeb7cf95f5128b
Adriankhl <dlshcbmuipmam@hotmail.com>
2023-05-02 14:54:21 +0200

    fix: initialize framework in simulations

--- simulations/src/main/kotlin/relativitization/game/TypicalGame.kt
+++ simulations/src/main/kotlin/relativitization/game/TypicalGame.kt
@@ -1,6 +1,10 @@
 package relativitization.game
 
+import relativitization.universe.game.GameUniverseInitializer
+
 fun main() {
+    GameUniverseInitializer.initialize()
+
     gameSingleRun(
         printStep = true,
         universeName = "Typical game",
